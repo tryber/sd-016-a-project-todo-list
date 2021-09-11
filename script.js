268ebcff2937222c9551cc55063c12677f8b88e1
arctavares <cosilk@hotmail.com>
2021-09-11 14:54:42 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 
-function addListItem(){
+// Add list itens to ol
+function addListItem() {
     let listItem = document.createElement('li')
     listItem.innerText = input.value;
     list.appendChild(listItem);
@@ -11,3 +12,37 @@ function addListItem(){
 }
 
 button.addEventListener('click', addListItem);
+
+// Add id to list itens
+function addId() {
+    for (let i = 0; i < list.children.length; i += 1) {
+        list.children[i].id = i;
+    }
+}
+
+button.addEventListener('click', addId);
+
+// Add background color to selected list item
+
+let selected = -1;
+
+function clean(selectedID){
+    for(let i = 0; i < list.children.length; i += 1){
+        if(selectedID !== i){
+            let id = i.toString();
+            document.getElementById(id).style.backgroundColor = 'white';
+        }
+    }
+}
+
+button.addEventListener('click', () => {
+    for (let i = 0; i < list.children.length; i += 1) {
+        list.children[i].addEventListener('click', () => {
+            let id = i.toString();
+            selected = i;
+            document.getElementById(id).style.backgroundColor = 'rgb(128, 128, 128)';
+            clean(selected);
+        });
+    }
+})
+
