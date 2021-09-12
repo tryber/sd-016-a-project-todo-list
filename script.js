ca495443e3b081dd42743f68537fd97939560739
cdartora <cdartora>
2021-09-11 23:47:45 -0300

    Termina requisitos 9, 10  e eslint

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addBtn = document.querySelector('#criar-tarefa');
 const taskText = document.querySelector('#texto-tarefa');
 const toDoList = document.querySelector('#lista-tarefas');
+const clearBtn = document.querySelector('#apaga-tudo');
+const clearCompletedBtn = document.querySelector('#remover-finalizados');
 
 // Cancela a seleção de todas as tarefas
 function unSelect() {
@@ -20,12 +22,27 @@ function selectTaskHandler(e) {
     e.target.classList.remove('selected');
 
     isSelected = false;
-  } else if (isSelected) {
+  } else if (!isSelected) {
     unSelect();
 
     e.target.classList.add('selected');
 
     isSelected = true;
+  }
+}
+
+// Lida com completar as tarefas
+function completeTaskHandler(e) {
+  let isCompleted = false;
+
+  if (e.target.className.includes('completed')) {
+    e.target.classList.remove('completed');
+
+    isCompleted = false;
+  } else if (!isCompleted) {
+    e.target.classList.add('completed');
+
+    isCompleted = true;
   }
 }
 
@@ -36,7 +53,26 @@ function addTaskHandler() {
   li.innerHTML = taskText.value;
   toDoList.appendChild(li);
   li.addEventListener('click', selectTaskHandler);
+  li.addEventListener('dblclick', completeTaskHandler);
   taskText.value = '';
 }
 
+// Lida com a remoção de todas as tarefas
+function clearTasksHandler() {
+  const tasks = document.querySelectorAll('.task');
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].remove();
+  }
+}
+
+// Lida com a remoção de tarefas completadas
+function clearCompletedTasksHandler() {
+  const completedTasks = document.querySelectorAll('.completed');
+  for (let i = 0; i < completedTasks.length; i += 1) {
+    completedTasks[i].remove();
+  }
+}
+
 addBtn.addEventListener('click', addTaskHandler);
+clearBtn.addEventListener('click', clearTasksHandler);
+clearCompletedBtn.addEventListener('click', clearCompletedTasksHandler);
