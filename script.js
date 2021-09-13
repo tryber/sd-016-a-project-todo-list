7458bd433f0ad24ad3f48d60ae01986d903624ac
lucca-falbo <luccafalbo@gmail.com>
2021-09-13 15:51:19 -0300

    main exercises

--- script.js
+++ script.js
@@ -10,18 +10,51 @@ function selectTask(event) {
   newSelected.classList.toggle('selected');
 }
 
+function completeTask(event) {
+  return event.target.classList.toggle('completed');
+}
+
 function createNewTask() {
   const task = document.getElementById('texto-tarefa').value;
+  document.getElementById('texto-tarefa').value = '';
+  if (task === '') return true;
   const list = document.getElementById('lista-tarefas');
   const newTask = document.createElement('li');
   newTask.innerHTML = task;
   newTask.addEventListener('click', selectTask);
+  newTask.addEventListener('dblclick', completeTask);
   list.appendChild(newTask);
-  document.getElementById('texto-tarefa').value = '';
 }
 
-function addBtnListenr() {
+function addTaskBtnListener() {
   const btn = document.getElementById('criar-tarefa');
   btn.addEventListener('click', createNewTask);
 }
-addBtnListenr();
+addTaskBtnListener();
+
+function clearTasks() {
+  const taskList = document.getElementById('lista-tarefas');
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function addClearBtnListener() {
+  const btn = document.getElementById('apaga-tudo');
+  btn.addEventListener('click', clearTasks);
+}
+addClearBtnListener();
+
+function clearFinished() {
+  const taskList = document.getElementsByClassName('completed');
+  while (taskList.length > 0) {
+    const task = taskList[0];
+    task.parentNode.removeChild(task);
+  }
+}
+
+function addClearFinishedListener() {
+  const btn = document.getElementById('remover-finalizados');
+  btn.addEventListener('click', clearFinished);
+}
+addClearFinishedListener();
