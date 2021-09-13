eddbf8ff05e95183d6889b354518696ba5c00414
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-09-13 18:14:48 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const buttonAdd = document.querySelector('#criar-tarefa');
 buttonAdd.addEventListener('click', add);
 
+
 function add () {
   const inputAdd = document.querySelector('#texto-tarefa');
   const textInput = inputAdd.value;
   const addList = document.querySelector('#lista-tarefas');
   const taskLine = document.createElement('li');
+  taskLine.addEventListener('click', changeBackground);
   taskLine.innerHTML = textInput;
   addList.appendChild(taskLine);
   inputAdd.value = '';
+
+  function changeBackground () {
+    taskLine.classList.remove('background');
+  }
 }
