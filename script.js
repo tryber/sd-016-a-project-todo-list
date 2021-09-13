ccde4fd52588eee96382e5ba2dbc5b24930f232f
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-09-13 20:59:06 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const buttonAdd = document.querySelector('#criar-tarefa');
+const addList = document.querySelector('#lista-tarefas');
 buttonAdd.addEventListener('click', add);
 
 
 function add () {
   const inputAdd = document.querySelector('#texto-tarefa');
   const textInput = inputAdd.value;
-  const addList = document.querySelector('#lista-tarefas');
   const taskLine = document.createElement('li');
-  taskLine.addEventListener('click', changeBackground);
   taskLine.innerHTML = textInput;
   addList.appendChild(taskLine);
   inputAdd.value = '';
+  
+}
 
-  function changeBackground () {
-    taskLine.classList.add('background');
+addList.addEventListener('click', changeBackground);
+
+function changeBackground (event) {
+  const selectLi = document.querySelectorAll('li');
+  for (let index = 0; index < selectLi.length; index += 1) {
+    selectLi[index].style.backgroundColor = 'white';    
   }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)'
 }
