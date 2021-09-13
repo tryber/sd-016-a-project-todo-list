da2c51e4cfa8f7104b3736a288f45bf9900f8988
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-13 17:13:46 -0300

    adiciona backGround-color ao item selecionado

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const list = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
-const input = document.getElementById('texto-tarefa')
+const input = document.getElementById('texto-tarefa');
+
 function createItenList() {
-    const listItem = document.createElement('li');
-    listItem.innerText = input.value;
-    list.appendChild(listItem);
-    input.value = '';
+  const listItem = document.createElement('li');
+  listItem.innerText = input.value;
+  list.appendChild(listItem);
+  input.value = '';
 }
 button.onclick = createItenList;
+let lis = document.getElementsByTagName('li');
+function selectItem(event) {
+   for (let i = 0; i < lis.length; i += 1) {
+       lis[i].classList.remove('selected');
+   }
+   event.target.classList.add('selected')
+}
+list.addEventListener('dblclick', selectItem);
+
