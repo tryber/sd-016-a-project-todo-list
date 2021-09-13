9f08fbff12acafc6c3a2f76ef4a7d92f2e6b2412
Jose <josetoitio@gmail.com>
2021-09-13 14:41:04 -0300

    Exercicio 1 e 2 feitos

---
+++ script.js
@@ -0,0 +1,14 @@
+function addTitle() {
+  const title = document.createElement('h1');
+  title.innerText = 'Minha Lista de Tarefas';
+  document.querySelector('header').appendChild(title);
+}
+addTitle();
+
+function addParagraph() {
+  const paragraph = document.createElement('p');
+  paragraph.id = 'funcionamento';
+  paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+  document.querySelector('main').appendChild(paragraph);
+}
+addParagraph();
