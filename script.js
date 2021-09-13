d288f67dbd0068358322612b59e92cb9b10b8fb8
joao <jgsspinelli92@gmail.com>
2021-09-13 19:34:59 -0300

    entrega dos requisitos 10 e 11

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createListItem() {
   const parentElement = document.getElementById('lista-tarefas')
   listItem.className = 'list-item'
   listItem.innerText = input.value
+  listItem.addEventListener('dblclick', completedTasks)
   parentElement.appendChild(listItem)
   input.value = '';
 }
@@ -14,7 +15,21 @@ function eraseAll() {
   const listItems = document.querySelectorAll('.list-item');
 
   for (let index = 0; index < listItems.length; index += 1) {
-   listItems[index].remove()
+   listItems[index].remove();
   }
 }
 document.getElementById('apaga-tudo').addEventListener('click', eraseAll);
+
+function completedTasks(doubleClick) {
+  doubleClick.target.classList.toggle('completed');
+}
+
+function eraseCompleted() {
+  const listCompletedItems = document.querySelectorAll('.completed')
+
+  for (let index = 0; index < listCompletedItems.length; index += 1) {
+    listCompletedItems[index].remove();
+  }
+}
+document.getElementById('remover-finalizados').addEventListener('click', eraseCompleted);
+
