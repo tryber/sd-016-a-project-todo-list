c3c10cccf64e8ef9c7a43bbbfa24c2f4531838c8
Esau Matias <esaufreirematias@gmail.com>
2021-09-13 16:53:58 -0300

    resoluçao dos requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let input = document.querySelector("#texto-tarefa");
 let botao = document.querySelector("#criar-tarefa");
-let ol = document.querySelector("#lista-tarefas");
+let olPai= document.querySelector("#lista-tarefas");
 
 botao.addEventListener('click', adiciona);
 
@@ -8,5 +8,24 @@ function adiciona() {
   let criaLi = document.createElement('li');
   criaLi.innerHTML = input.value;
   input.value ='';
-  ol.appendChild(criaLi);
+  olPai.appendChild(criaLi);
+
+  mudaCor();
+}
+
+function mudaCor() {
+  const lis = document.querySelectorAll('li');
+  for (let index = 0; index < lis.length; index += 1) {
+    lis[index].addEventListener('click', function() {
+      remove();
+      lis[index].style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
+}
+
+function remove() {
+  const filhosOl = olPai.children;
+  for (let index = 0; index < filhosOl.length; index += 1) {
+    filhosOl[index].style.backgroundColor = 'white';  
+  }
 }
