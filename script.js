8b579f40a902337d8d312d49b7e3c2bcf563fe55
joaogmelo95 <joaogmelo95@gmail.com>
2021-09-13 15:24:14 -0300

    Até o requisito 11

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const olTask = document.querySelector('#lista-tarefas');
 // Localiza o botão de adicionar nova tarefa
 const addTaskBtn = document.querySelector('#criar-tarefa');
 
+// Localiza o botão de apagar tudo
+const deleteAllBtn = document.querySelector('#apaga-tudo');
+
+// Localiza o botão de remover finalizados
+const deleteCompletedBtn = document.querySelector('#remover-finalizados');
+
 // Funções
 // Adicionar cor de fundo ao item de lista quando for clicado e tira dos outros
 function changeListItemColor(event) {
@@ -37,3 +43,26 @@ function addNewTask() {
 
 addTaskBtn.addEventListener('click', addNewTask);
 
+// Apagar todas as tarefas
+function deleteAllTasks() {
+    const itemList = olTask.children;
+    for (let index = itemList.length - 1; index >= 0; index -= 1) {
+        const currentItem = itemList[index];
+        olTask.removeChild(currentItem);
+    }
+}
+
+deleteAllBtn.addEventListener('click', deleteAllTasks);
+
+// Remover tarefas finalizadas
+function deleteCompletedTasks() {
+    const itemList = olTask.children;
+    for (let index = itemList.length - 1; index >= 0; index -= 1) {
+        const currentItem = itemList[index];
+        if (currentItem.classList.contains('completed')) {
+            olTask.removeChild(currentItem);
+        }
+    }
+}
+
+deleteCompletedBtn.addEventListener('click', deleteCompletedTasks);
