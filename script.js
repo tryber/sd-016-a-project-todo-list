04ee49cf727e656ec3e432ccfc997f046430e66a
Your Name <you@example.com>
2021-09-13 17:53:26 -0300

    requisito 10 - concluido

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const localButton = document.querySelector('#criar-tarefa');
 const localOl = document.querySelector('#lista-tarefas');
-let contentInput = document.querySelector('#texto-tarefa');
+const contentInput = document.querySelector('#texto-tarefa');
 
 function createLi() {
   const novaLi = document.createElement('li');
-  novaLi.innerText=contentInput.value;
+  novaLi.innerText = contentInput.value;
   localOl.appendChild(novaLi);
-  contentInput.value="";
-  novaLi.addEventListener('click', function(){
-    novaLi.style.backgroundColor="rgb(128,128,128)";
-  })
+  contentInput.value = '';
+  novaLi.addEventListener('click', function () {
+    novaLi.style.backgroundColor = 'rgb(128,128,128)';
+  });
 }
 
 localButton.addEventListener('click', createLi);
 
+// function color() {
+//   novaLi.style.backgroundColor = 'rgb(128,128,128)';
+// }
 
+const buttonApagar = document.getElementById('apaga-tudo');
+function erasedIten() {
+  localOl.innerHTML = '';
+}
+
+buttonApagar.addEventListener('click',erasedIten);
