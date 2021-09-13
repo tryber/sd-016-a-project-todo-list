9c9f97638d5ba48648108c60eccee35094f098e8
vitor <vitorsbsilva@gmail.com>
2021-09-13 16:36:52 -0300

    Requisito 8 feito após ajuda do Sumo na monitoria

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let button = document.getElementById("criar-tarefa");
 
-button.addEventListener("click", clearInput);
+button.addEventListener("click", addTask);
 
-function clearInput() {
+// OBS: Link consultado para aprender a LIMPAR o conteúdo do Input:
+// https://www.w3schools.com/howto/howto_html_clear_input.asp
+
+function addTask() {
     let inputBox = document.getElementById('texto-tarefa');
     let newElement = document.createElement("li");
     let list = document.getElementById("lista-tarefas");
@@ -11,4 +14,16 @@ function clearInput() {
     list.appendChild(newElement);
 
     inputBox.value = '';
+
+    let listItems = document.body.getElementsByTagName("li");
+
+    for (let index = 0; index < listItems.length; index += 1) {
+        listItems[index].addEventListener("click", paintedBackground);
+    }
+} 
+
+
+function paintedBackground(event) {
+      event.target.style.backgroundColor = "rgb(128, 128, 128)";
 }
+
