42e72f3bef5544f2202fb3f3cef4dee9f733a46a
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-13 16:24:55 -0300

    Requisito 8: Torna possível a seleção de apenas um elemento da lista por vez

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
+let items = [];
+
+function changeBackground(item) {
+  items.forEach((element) => {
+    const value = element;
+    value.style.backgroundColor = '';
+  });
+  const task = item;
+  task.target.style.backgroundColor = 'rgb(128,128,128)';
+}
 
 button.addEventListener('click', () => {
   const liTask = document.createElement('li');
   liTask.innerHTML = input.value;
   lista.appendChild(liTask);
+  liTask.classList.add('items');
   input.value = '';
+  items = document.querySelectorAll('.items');
+  items.forEach((item) => {
+    item.addEventListener('click', changeBackground);
+  });
 });
