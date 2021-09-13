367a28169f755a7de8264b4aeb4551e8898381f9
btex88 <bruno@estagio>
2021-09-13 16:11:13 -0300

    function to set bg color to a task

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function createListElement() {
   const getListParentElement = document.querySelector(taskListClass);
   return getListParentElement.appendChild(createLi);
 }
+
+// Function to set back color shadow of a task list element.
+function cleanAllSelectedItems() {
+  const getListParentElementChildren = document.querySelector(taskListClass).children;
+  for (let index = 0; index < getListParentElementChildren.length; index += 1) {
+    const liIndexPosition = getListParentElementChildren[index];
+    liIndexPosition.style.backgroundColor = 'unset';
+  }
+}
+
