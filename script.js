da2866dd440a445d715825dad5351b283d34083e
Veidz <joaovictorveidz@gmail.com>
2021-09-13 20:34:41 -0300

    4 - Adicionando input

--- script.js
+++ script.js
@@ -10,17 +10,31 @@ function createTitle() {
   header.appendChild(title);
 }
 
+// 2 - Adicionando parágrafo de funcionalidade, embaixo do title
 function createParagraphOperation() {
   const header = document.getElementById('header');
   const operation = document.createElement('p');
-  
+
   operation.id = 'funcionamento';
   operation.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 
   header.appendChild(operation);
 }
 
+// 3 - Adicionando input
+function createTaskInput() {
+  let taskInputSection = document.createElement('section');
+  let taskInput = document.createElement('input');
+
+  taskInputSection.id = 'input-section'
+  taskInput.id = 'texto-tarefa';
+
+  document.body.append(taskInputSection);
+  taskInputSection.appendChild(taskInput);
+}
+
 window.onload = () => {
   createTitle();
   createParagraphOperation();
-}
+  createTaskInput();
+};
