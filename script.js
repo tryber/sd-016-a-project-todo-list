dcef94771fdc0654d061ea7d48be2e08865fcac6
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-13 18:58:44 -0300

    adiciona botao que limpa a lista

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const list = document.getElementById('lista-tarefas');
-const button = document.getElementById('criar-tarefa');
+const buttonAdd = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 
 function createItenList() {
@@ -8,18 +8,22 @@ function createItenList() {
   list.appendChild(listItem);
   input.value = '';
 }
-button.onclick = createItenList;
+buttonAdd.onclick = createItenList;
 let lis = document.getElementsByTagName('li');
 function selectItem(event) {
-   for (let i = 0; i < lis.length; i += 1) {
-       lis[i].classList.remove('selected');
-   }
-   event.target.classList.add('selected')
+  for (let i = 0; i < lis.length; i += 1) {
+    lis[i].classList.remove('selected');
+  }
+  event.target.classList.add('selected')
 }
 list.addEventListener('click', selectItem);
 
 function completeItem(event) {
-    event.target.classList.add('completed')
- }
- list.addEventListener('dblclick', completeItem);
- 
+  event.target.classList.add('completed')
+}
+list.addEventListener('dblclick', completeItem);
+
+function deleteAllLi() {
+  list.innerHTML = '';
+}
+document.getElementById('apaga-tudo').onclick = deleteAllLi;
