8abfa23ec38ca7f2243f8eaaf4ce40ddfc5353de
Christian Lessa <christianclessa@gmail.com>
2021-09-13 18:48:21 -0300

    Requisito 7 concluido

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const listOrder = document.querySelector('#lista-tarefas')
 buttonCreatTarefa.addEventListener('click', inputButton);
 
 function inputButton () {
-    const inputButton = document.querySelector('#texto-tarefa');
-    const creatList = document.createElement('li');
-    const addText = inputButton.value;
-    creatList.innerText = addText
-    listOrder.appendChild(creatList);
-    inputButton.value = "";
+  const inputButton = document.querySelector('#texto-tarefa');
+  const creatList = document.createElement('li');
+  creatList.addEventListener('click', selectBackgroundColor); 
+  const addText = inputButton.value;
+  creatList.innerText = addText
+  listOrder.appendChild(creatList);
+  inputButton.value = "";
+
+
+  function selectBackgroundColor (event) {
+    creatList.classList.remove('background-color');
+    creatList.classList.add('background-color');
+  }
 }
+
