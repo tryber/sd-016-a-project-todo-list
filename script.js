d719227e867a58e63ca7d6caab3f13fbe7b71064
Otavio Cantareli <otavio_cantareli@outlook.com>
2021-09-13 17:39:52 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-/* eslint-disable no-unused-vars */
+function itemStyles() {
+  const items = document.querySelectorAll('.list-item');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].addEventListener('click', () => {
+      items[i].classList.toggle(':active');
+    });
+    items[i].addEventListener('click', () => {
+      items[i].classList.toggle('completed');
+    });
+  }
+}
+
 function createTask() {
   const inputField = document.getElementById('texto-tarefa');
   const input = document.getElementById('texto-tarefa').value;
@@ -6,9 +17,19 @@ function createTask() {
     alert('Insira um texto válido!');
     return;
   }
+  const a = document.createElement('a');
   const ol = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
-  li.innerHTML = input;
+  a.className = 'list-item';
+  a.innerHTML = input;
+  li.appendChild(a);
   inputField.value = '';
   ol.appendChild(li);
+}
+
+function deleteAll() {
+  const elements = document.getElementById('lista-tarefas');
+  while (elements.firstChild) {
+    elements.removeChild(elements.lastChild);
+  }
 }
