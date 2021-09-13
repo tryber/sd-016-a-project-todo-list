47d48f92bd397482ad328917681eb25e49840021
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-09-13 18:26:30 -0300

    5 requisito2

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-//Requisito 5 - inserir texto a lista
-const button = document.getElementById("criar-tarefa");
-button.addEventListener('click',addTarefa);
+    //Requisito 5 - inserir texto a lista
+const button = document.getElementById('criar-tarefa');
 const tarefa = document.getElementById('texto-tarefa');
 
 function addTarefa() {
-    const listIten = document.createElement('li');
-    listIten.innerHTML = tarefa.value;
-    document.getElementById('lista-tarefas').appendChild(listIten);
-    tarefa.value = ' ';
+  const listIten = document.createElement('li');
+  listIten.innerHTML = tarefa.value;
+  document.getElementById('lista-tarefas').appendChild(listIten);
+  tarefa.value = '';
 }
+
+button.addEventListener('click', addTarefa);
