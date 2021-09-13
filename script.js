b5adeafef10cda0371ae176e23cfd0bfe0ee5f38
Mateus Turola <turolamateus@gmail.com>
2021-09-13 19:56:10 -0300

    resolva questoes 7 e 8o

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-function createTask(){
-  let orderList = document.querySelector('#lista-tarefas');
-  let getInput = document.querySelector('#texto-tarefa');
+// CRIA TAREFA
+function createTask() {
+  const orderList = document.querySelector('#lista-tarefas');
+  const getInput = document.querySelector('#texto-tarefa');
 
-  let itemList = document.createElement('li');
+  const itemList = document.createElement('li');
   itemList.innerText = getInput.value;
+  itemList.className = 'task'
   orderList.append(itemList);
   getInput.value = '';
 }
 
-let buttonCreate = document.querySelector('#criar-tarefa');
-buttonCreate.addEventListener('click', createTask)
+const buttonCreate = document.querySelector('#criar-tarefa');
+buttonCreate.addEventListener('click', createTask);
+
+// MUDA COR QUANDO SELECIONADO
+let selectItem = document.getElementsByClassName('selected')
+function removeSelection(){
+  
+  for (let index = 0; index < selectItem.length; index++) {
+    selectItem[index].style.backgroundColor = 'rgb(255, 255, 255)';
+  }
+}
+
+function changeColorBg(event){
+  removeSelection();
+  event.target.className = 'selected'; 
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+
+
+}
+
+let task = document.getElementById('lista-tarefas');
+
+
+task.addEventListener('click', changeColorBg);
+
+
+
