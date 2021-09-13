4938d8df415d4a70ff838389d9574f9f0b2060c3
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-09-13 16:02:40 -0300

    Altera cor do elemento para cinza ao clica-lo

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 let button = document.querySelector('#criar-tarefa')
 let list = document.querySelector('#lista-tarefas')
 let input = document.querySelector('#texto-tarefa')
+let li = document.querySelectorAll('#lista-tarefas li')
 button.addEventListener('click', function(){
 let newTask = input.value;
 let createListItem = document.createElement('li');
+createListItem.addEventListener('click', function(){
+    createListItem.style.backgroundColor = 'rgb(128,128,128)'
+})
 
 createListItem.appendChild(document.createTextNode(newTask));
 list.appendChild(createListItem);
