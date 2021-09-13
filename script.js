7dec9afd225bf14d50727120f84d6fe9bbabead6
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-09-13 16:01:47 -0300

    adicionado requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const header = document.querySelector('header');
 const main = document.querySelector('main');
 
+
 function addTitle() {
   const h1Header = document.createElement('h1');
   h1Header.className = 'title';
@@ -19,14 +20,48 @@ addParagraph();
 
 function addInputField() {
   const inputField = document.createElement('input');
+  const section = document.createElement('section');
   inputField.id = 'texto-tarefa';
-  main.appendChild(inputField);
+  section.id = 'add-task';
+  main.appendChild(section);
+  section.appendChild(inputField);
 }
 addInputField();
 
 function addOrderedList() {
   const orderedList = document.createElement('ol');
+  const section = document.createElement('section');
   orderedList.id = 'lista-tarefas';
-  main.appendChild(orderedList);
+  section.id = 'task-list';
+  main.appendChild(section);
+  section.appendChild(orderedList);
 }
 addOrderedList();
+
+function addTaskButton() {
+  const addButton = document.createElement('button');
+  addButton.id = 'criar-tarefa';
+  addButton.innerText = 'Adicionar';
+  const addtask = document.querySelector('#add-task');
+  addtask.appendChild(addButton);
+}
+addTaskButton();
+
+function addNewTask() {
+  const tasklist = document.querySelector('#lista-tarefas');
+  const addButton = document.querySelector('#criar-tarefa');
+  const inputField = document.querySelector('#texto-tarefa');
+
+  addButton.addEventListener('click', function () {
+    if (inputField.value.length > 0) {
+      const addLi = document.createElement('li');
+      addLi.innerText = inputField.value;
+      addLi.style.order
+      tasklist.appendChild(addLi);
+      inputField.value = '';
+    } else {
+      alert('erro');
+    }
+  });
+}
+addNewTask();
