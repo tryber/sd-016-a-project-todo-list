4ebc57572b598c3fb4f0b55fa47f4655ce57b342
gui <guilhaugusto05@gmail.com>
2021-09-13 17:02:13 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
@@ -10,11 +11,23 @@ function createTask() {
   input.value = '';
 }
 
+function changeTaskColor(e) {
+  const task = e.target;
+  task.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 // Listeners
 button.addEventListener('click', createTask);
 input.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
     e.preventDefault();
     createTask();
+  }
+});
+
+body.addEventListener('click', (e) => {
+  if (e.target.classList.contains('tarefa')) {
+    e.preventDefault();
+    changeTaskColor(e);
   }
 });
