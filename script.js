8d96867d1720b57bd54580f9fc9227c375e92a59
douglas <douglasfmr1@gmail.com>
2021-09-13 16:28:25 -0300

    exercicios do 1 ao 6

--- script.js
+++ script.js
@@ -16,12 +16,3 @@ let list = document.querySelector('#lista-tarefas');
 })
 
 
-// button.addEventListener('click', function() {
-//     if (input.value.length > 0) {
-//       let newLi = document.createElement('li');
-//       newLi.innerText = input.value;
-
-//       list.appendChild(newLi);
-//       input.value = '';
-//     }
-//   })
