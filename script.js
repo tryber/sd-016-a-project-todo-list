63806857fe3f37778f3fde37840c4d8f89864910
Julia Barcelos <julaai@gmail.com>
2021-09-13 16:14:01 -0300

    Aí SIM pai

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 
 // transporta o input para uma funcao de adicionar um lista
 
-const createTask = document.querySelector('#texto-tarefa');
-const taskList = document.querySelector('#lista-tarefas');
+const taskText = document.querySelector('#texto-tarefa'); // input
+const taskList = document.querySelector('#lista-tarefas'); // ol
+const createTask = document.querySelector('#criar-tarefa'); //button
 
-createTask.addEventListener('click', (event) => {
-  const newTask = document.createElement('li');
-  document.querySelector('.selected').classList.remove('selected');
-  event.target.classList.add('selected');
+
+createTask.addEventListener('click', (event) => { // escuta o button
+  const newTask = document.createElement('li'); // cria um l1
+  newTask.classList.add = '.task'; // add classe
+  newTask.innerHTML = taskText.value;
+  taskList.appendChild(newTask);
+  taskText.value = '';
 })
