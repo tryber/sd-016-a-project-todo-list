df18bd147d59dc6de053ed0f466c5f5a62a946d0
Gustavo Meira <gmeira.antonio@gmail.com>
2021-09-13 14:56:35 -0300

    Setimo e Oitavo Requisitos

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ const botaoCriarTarefa = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
 const inputNovaTarefa = document.querySelector('#texto-tarefa');
 
+function adicionarClasseSelecionado(event) {
+  const antigoItem = document.querySelector('.selected');
+  if (antigoItem !== null) {
+    antigoItem.classList.remove('selected');
+  }
+  const itemAtual = event.target;
+  itemAtual.classList.add('selected');
+}
+
+function criandoItemDaLista() {
+  const novaTarefa = document.createElement('li');
+  novaTarefa.addEventListener('click', adicionarClasseSelecionado);
+  novaTarefa.innerText = inputNovaTarefa.value;
+  return novaTarefa;
+}
+
 function adicionarNovaTarefa() {
-  const novaTarefaTexto = document.createElement('li');
-  novaTarefaTexto.innerText = inputNovaTarefa.value;
-  listaTarefas.appendChild(novaTarefaTexto);
+  const novaTarefa = criandoItemDaLista();
+  listaTarefas.appendChild(novaTarefa);
   inputNovaTarefa.value = '';
 }
 
