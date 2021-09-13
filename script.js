40fd18e28b07bfdd747bf93f68467cf23c4f3466
btex88 <bruno@estagio>
2021-09-13 16:09:15 -0300

    function to create li

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Global functions
 const taskListClass = '.task-list';
+
+// Function to create element li and return its creation.
+function createListElement() {
+  const createLi = document.createElement('li');
+  createLi.classList.add('task-list-single-task');
+  const getListParentElement = document.querySelector(taskListClass);
+  return getListParentElement.appendChild(createLi);
+}
