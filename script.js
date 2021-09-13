1ae1da0bae78deab120ae3cce1ae730a693d1f9b
Eric <ericvinicius81@yahoo.com.br>
2021-09-13 19:53:39 -0300

    Correçao do eslint e stylelint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('#criar-tarefa');
-button.addEventListener('click', function () {
-  let input = document.querySelector('#texto-tarefa');
-  let list = document.querySelector('#lista-tarefas');
+button.addEventListener('click', () => {
+  const input = document.querySelector('#texto-tarefa');
+  const list = document.querySelector('#lista-tarefas');
   const task = document.createElement('li');
   task.innerHTML = input.value;
   list.appendChild(task);
