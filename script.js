05e482129891e81924f334350b43c0dd96881057
PatrickR00t <patrick.s.falquetti@gmail.com>
2021-09-13 17:38:51 -0300

    versão2

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function recebeTexto() {
-  let text = document.querySelector("#texto-tarefa")
-}
-
-
-function adicionaLista() {
-  let addList = document.querySelector("#criar-tarefa");
-  addList.addEventListener("click", recebeTexto)
+let add = document.querySelector('#criar-tarefa');
+add.addEventListener('click', addList);
+let texto = document.querySelector('#texto-tarefa');
+function addList() {
+  let olPai = document.querySelector('#lista-tarefas');
+  for(let i = 0; i <= 100; i += 1) {
+    let element = document.createElement('li');
+    olPai.appendChild(element);
+  }
 }
