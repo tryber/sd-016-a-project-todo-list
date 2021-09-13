5445661c877fb7373bddf97dc032890e39162896
rbfeliciano <rb.feliciano@hotmail.com>
2021-09-13 18:30:30 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const taskButton = document.querySelector('#criar-tarefa');
 taskButton.addEventListener('click', addtask);
+const cleanList = document.querySelector('#apaga-tudo');
+cleanList.addEventListener('click', cleanButton);
 
 function selectListColor() {
   const selecList = document.querySelectorAll('li');
   for (let i = 0; i < selecList.length; i += 1) {
-    selecList[i].addEventListener('click', function(event){
+    selecList[i].addEventListener('click', function (event) {
       event.target.style.backgroundColor = 'rgb(128, 128, 128)';
     });
+
   }
+}
+
+function clickColor() {
+  let colorPalette = document.querySelectorAll('li');;
+  colorPalette.addEventListener('click', (event) => {
+    removeColor();
+    event.target.classList.add('selected')
+  })
 }
 
 function addtask() {
@@ -20,3 +31,12 @@ function addtask() {
   taskText.value = '';
   selectListColor();
 }
+
+function cleanButton() {
+  const clean = document.querySelectorAll('li');
+  for (let i = 0; i < clean.length; i += 1) {
+    clean[i].remove();
+  }
+}
+
+cleanButton();
