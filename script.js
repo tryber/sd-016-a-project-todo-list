68cfdd8358e9f2afcd57a6d4932dabbb6765a2bc
joaogmelo95 <joaogmelo95@gmail.com>
2021-09-13 19:36:47 -0300

    Lint - ajustando os espaços para 2 tabs

--- script.js
+++ script.js
@@ -31,61 +31,61 @@ let taskList = [];
 // Funções
 // Adicionar cor de fundo ao item de lista quando for clicado e tira dos outros
 function changeListItemColor(event) {
-    for (let index = 0; index < olTask.children.length; index += 1) {
-        const currentListItem = olTask.children[index];
-        currentListItem.style.backgroundColor = 'white';
-    }
+	for (let index = 0; index < olTask.children.length; index += 1) {
+		const currentListItem = olTask.children[index];
+		currentListItem.style.backgroundColor = 'white';
+	}
 
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+	event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 // Risca a linha caso a tarefa tenha sido realizada + retorna ao estado original
 function completeTask(event) {
-    const currentListItem = event.target;
-    currentListItem.classList.toggle('completed');
+	const currentListItem = event.target;
+	currentListItem.classList.toggle('completed');
 }
 
 // Criar tarefa
 function createTask(content, status) {
-    const newTask = document.createElement('li');
-    newTask.innerHTML = content;
-    if (status) {
-        newTask.classList.add('completed');
-    }
-    newTask.style.backgroundColor = 'white';
-    newTask.addEventListener('click', changeListItemColor);
-    newTask.addEventListener('dblclick', completeTask);
-    olTask.appendChild(newTask);
+	const newTask = document.createElement('li');
+	newTask.innerHTML = content;
+	if (status) {
+		newTask.classList.add('completed');
+	}
+	newTask.style.backgroundColor = 'white';
+	newTask.addEventListener('click', changeListItemColor);
+	newTask.addEventListener('dblclick', completeTask);
+	olTask.appendChild(newTask);
 }
 
 // Adicionar nova tarefa
 function addNewTask() {
-    createTask(taskInput.value, false);
-    taskInput.value = '';
+	createTask(taskInput.value, false);
+	taskInput.value = '';
 }
 
 addTaskBtn.addEventListener('click', addNewTask);
 
 // Apagar todas as tarefas
 function deleteAllTasks() {
-    const itemList = olTask.children;
-    for (let index = itemList.length - 1; index >= 0; index -= 1) {
-        const currentItem = itemList[index];
-        olTask.removeChild(currentItem);
-    }
+	const itemList = olTask.children;
+	for (let index = itemList.length - 1; index >= 0; index -= 1) {
+		const currentItem = itemList[index];
+		olTask.removeChild(currentItem);
+	}
 }
 
 deleteAllBtn.addEventListener('click', deleteAllTasks);
 
 // Remover tarefas finalizadas
 function deleteCompletedTasks() {
-    const itemList = olTask.children;
-    for (let index = itemList.length - 1; index >= 0; index -= 1) {
-        const currentItem = itemList[index];
-        if (currentItem.classList.contains('completed')) {
-            olTask.removeChild(currentItem);
-        }
-    }
+	const itemList = olTask.children;
+	for (let index = itemList.length - 1; index >= 0; index -= 1) {
+		const currentItem = itemList[index];
+		if (currentItem.classList.contains('completed')) {
+			olTask.removeChild(currentItem);
+		}
+	}
 }
 
 deleteCompletedBtn.addEventListener('click', deleteCompletedTasks);
@@ -93,35 +93,35 @@ deleteCompletedBtn.addEventListener('click', deleteCompletedTasks);
 // Mover elementos
 // 1. Criar array com elementos atuais
 function moveItemUp() {
-    const olTaskChildren = olTask.children;
-    const newArray = [];
-    for (let index = 0; index < olTaskChildren.length; index += 1) {
-        const currentItem = olTaskChildren[index];
-        newArray.push(currentItem);
-    }
-    checkIfAnyElementIsSelected(newArray, 'up');
+	const olTaskChildren = olTask.children;
+	const newArray = [];
+	for (let index = 0; index < olTaskChildren.length; index += 1) {
+		const currentItem = olTaskChildren[index];
+		newArray.push(currentItem);
+	}
+	checkIfAnyElementIsSelected(newArray, 'up');
 }
 
 function moveItemDown() {
-    const olTaskChildren = olTask.children;
-    const newArray = [];
-    for (let index = 0; index < olTaskChildren.length; index += 1) {
-        const currentItem = olTaskChildren[index];
-        newArray.push(currentItem);
-    }
-    checkIfAnyElementIsSelected(newArray, 'down');
+	const olTaskChildren = olTask.children;
+	const newArray = [];
+	for (let index = 0; index < olTaskChildren.length; index += 1) {
+		const currentItem = olTaskChildren[index];
+		newArray.push(currentItem);
+	}
+	checkIfAnyElementIsSelected(newArray, 'down');
 }
 
 // 2. Verifica se tem algum elemento selecionado
 function checkIfAnyElementIsSelected(array, upOrDown) {
-    let currentIndex = undefined;
-    for (let index = 0; index < array.length; index += 1) {
-        const currentItem = array[index];
-        if (currentItem.style.backgroundColor !== 'white') {
-            currentIndex = array.indexOf(currentItem);
-        }
-    }
-    changeOrderOfElementsInArray(array, currentIndex, upOrDown);
+	let currentIndex = undefined;
+	for (let index = 0; index < array.length; index += 1) {
+		const currentItem = array[index];
+		if (currentItem.style.backgroundColor !== 'white') {
+			currentIndex = array.indexOf(currentItem);
+		}
+	}
+	changeOrderOfElementsInArray(array, currentIndex, upOrDown);
 }
 
 // 3. Muda a ordem dos elementos da array
@@ -130,30 +130,30 @@ function checkIfAnyElementIsSelected(array, upOrDown) {
  * Resposta do usuário Fragile Falcon no fórum: https://www.codegrepper.com/code-examples/javascript/change+position+of+element+in+array+javascript
  */
 function changeOrderOfElementsInArray(array, currentIndex, upOrDown) {
-    const element = array[currentIndex];
-    if (upOrDown === 'up') {
-        if (currentIndex > 0) {
-            const newIndex = currentIndex - 1;
-            array.splice(currentIndex, 1);
-            array.splice(newIndex, 0, element);
-            deleteAllTasks();
-            updateOlTask(array);
-        }
-    } else {
-        if (currentIndex < array.length - 1) {
-            array.splice(currentIndex, 1);
-            array.splice((currentIndex + 1), 0, element);
-            deleteAllTasks();
-            updateOlTask(array);
-        }
-    }
+	const element = array[currentIndex];
+	if (upOrDown === 'up') {
+		if (currentIndex > 0) {
+			const newIndex = currentIndex - 1;
+			array.splice(currentIndex, 1);
+			array.splice(newIndex, 0, element);
+			deleteAllTasks();
+			updateOlTask(array);
+		}
+	} else {
+		if (currentIndex < array.length - 1) {
+			array.splice(currentIndex, 1);
+			array.splice((currentIndex + 1), 0, element);
+			deleteAllTasks();
+			updateOlTask(array);
+		}
+	}
 }
 
 // 4. Atualiza lista
 function updateOlTask(array) {
-    for (let index = 0; index < array.length; index += 1) {
-        olTask.appendChild(array[index]);
-    }
+	for (let index = 0; index < array.length; index += 1) {
+		olTask.appendChild(array[index]);
+	}
 }
 
 moveUpBtn.addEventListener('click', moveItemUp);
@@ -161,50 +161,50 @@ moveDownBtn.addEventListener('click', moveItemDown);
 
 // Remove o item selecionado
 function removeSelected() {
-    for (let index = 0; index < olTask.children.length; index += 1) {
-        if (olTask.children[index].style.backgroundColor !== 'white') {
-            olTask.children[index].remove();
-        }
-    }
+	for (let index = 0; index < olTask.children.length; index += 1) {
+		if (olTask.children[index].style.backgroundColor !== 'white') {
+			olTask.children[index].remove();
+		}
+	}
 }
 
 removeSelectedBtn.addEventListener('click', removeSelected);
 
 // Atualiza lista de tarefas e salva no localStorage
 function saveListToLocalStorage() {
-    const currentList = olTask.children;
-    const newList = [];
-    for (let index = 0; index < currentList.length; index += 1) {
-        const currentListItem = currentList[index].innerHTML;
-        let status = false;
-        if (currentList[index].classList.contains('completed')) {
-            status = true;
-        }
-        const itemObject = {
-            content: currentListItem,
-            completed: status
-        };
-        newList.push(itemObject);
-    }
-    taskList = newList;
-    localStorage.setItem('tasks', JSON.stringify(taskList));
+	const currentList = olTask.children;
+	const newList = [];
+	for (let index = 0; index < currentList.length; index += 1) {
+		const currentListItem = currentList[index].innerHTML;
+		let status = false;
+		if (currentList[index].classList.contains('completed')) {
+			status = true;
+		}
+		const itemObject = {
+			content: currentListItem,
+			completed: status
+		};
+		newList.push(itemObject);
+	}
+	taskList = newList;
+	localStorage.setItem('tasks', JSON.stringify(taskList));
 }
 
 saveTasksBtn.addEventListener('click', saveListToLocalStorage);
 
 // Carregar lista do localStorage
 function loadList() {
-    const savedList = JSON.parse(localStorage.getItem('tasks'));
-    for (let index = 0; index < savedList.length; index += 1) {
-        const currentContent = savedList[index].content;
-        const currentStatus = savedList[index].completed;
-        createTask(currentContent, currentStatus);
-    }
+	const savedList = JSON.parse(localStorage.getItem('tasks'));
+	for (let index = 0; index < savedList.length; index += 1) {
+		const currentContent = savedList[index].content;
+		const currentStatus = savedList[index].completed;
+		createTask(currentContent, currentStatus);
+	}
 }
 
 // // Carregamento da página
 window.onload = function onLoad() {
-    if (localStorage.getItem('tasks') !== null) {
-        loadList();
-    }
+	if (localStorage.getItem('tasks') !== null) {
+		loadList();
+	}
 }
