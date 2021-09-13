4cbdfce322ea02a6ea44bee9ed50f862c1c0e6c0
joao <jgsspinelli92@gmail.com>
2021-09-13 17:47:09 -0300

    entrega do requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function createListItem () {
+function createListItem() {
   const listItem = document.createElement('li');
   const input = document.getElementById('texto-tarefa');
   const parentElement = document.getElementById('lista-tarefas')
   listItem.className = 'list-item'
   listItem.innerText = input.value
   parentElement.appendChild(listItem)
+  input.value = '';
 }
-
 document.getElementById('criar-tarefa').addEventListener('click', createListItem)
+
+
+function eraseAll() {
+  const listItems = document.querySelectorAll('.list-item');
+
+  for (let index = 0; index < listItems.length; index += 1) {
+   listItems[index].remove()
+  }
+}
+document.getElementById('apaga-tudo').addEventListener('click', eraseAll);
