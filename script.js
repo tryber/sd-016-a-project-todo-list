abd28857442e83fc6599a9434b93c9449a6b402b
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-09-13 16:46:01 -0300

    Cria funçao de seleçao de tarefa.

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ function createNewTask() {
   newTask.innerText = taskText;
   elementFather.appendChild(newTask);
   inputNewTask.value = '';
+  inputNewTask.focus();
 }
 buttonCreateTask.addEventListener('click', createNewTask);
+
+// Clicar em um item da lista deve alterar
+// a cor de fundo do item para cinza rgb(128,128,128)
+function clearBackgroundColor() {
+  let taskListItens = taskList.children;
+  for (const task of taskListItens) {
+    task.style.backgroundColor = 'white';
+  }
+}
+
+function changeBackgroundColor(element) {
+  clearBackgroundColor();
+  element.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+taskList.addEventListener('click', changeBackgroundColor);
