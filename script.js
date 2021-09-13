710e30d6de59ad834fc3db475935338d4ccab2c6
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2021-09-13 15:22:06 -0300

    adicionado o requisito 8 ao projeto

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const input = document.getElementById('texto-tarefa');
 botaoAdicionar.addEventListener('click', ()=> {
   listaOrdenada.appendChild(document.createElement('li'));
   listaOrdenada.lastChild.innerText = input.value;
-  
+
   listaOrdenada.lastChild.addEventListener('click', (event)=> {
     const element = event.target;
+    const previousElement = document.getElementsByClassName('selected')[0];
+
+    if (previousElement !== undefined) {
+      previousElement.style.backgroundColor = 'white';
+      previousElement.className = '';
+    }
+
+    element.className += ' selected';
     element.style.backgroundColor = 'rgb(128, 128, 128)';
+
   });
 
   input.value = '';
