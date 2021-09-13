84f827539846594b3f8184ac03cf09cc4e4f8208
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-09-13 15:29:15 -0400

    Completa o requisito 7

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function addTAsk() {
     // função já cria automaticamento os itens em ordem
     const newLi = document.createElement('li');
     newLi.innerText = valueInput.value;
-
     ordenedList.appendChild(newLi);
     valueInput.value = '';
-  } else {
-    alert('Adicione uma tarefa!');
   }
+} addTAsk();
+
+function itemColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
-addTAsk();
 
 function events() {
   const button = document.querySelector('#criar-tarefa');
-
+  const itemList = document.querySelector('#lista-tarefas');
+  // evento de click no button
   button.addEventListener('click', addTAsk);
+  // evento de click nos itens da lista
+  itemList.addEventListener('click', itemColor);
 } events();
