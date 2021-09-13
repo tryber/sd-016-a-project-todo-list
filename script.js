8ae373ef0e683fe303f028ba61624ef4d9598e80
Marcus <marcus10121@hotmail.com>
2021-09-13 19:51:29 -0300

    To conseguindo, olha so

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function createP() {
-  const sel = document.querySelector('body');
-  const paragrafo = document.createElement('p')
-  paragrafo.id = 'funcionamento'
-  paragrafo.innerHTML ='Clique duas vezes em um item para marcá-lo como completo'
-  sel.appendChild(paragrafo)
-}
+const input = document.querySelector('#texto-tarefa');
+const ol = document.querySelector('#lista-tarefas');
+const button = document.querySelector('#criar-tarefa');
+
+button.addEventListener('click', function() {
+  if (input.value != '') {
+    let li = document.createElement('li')
+    li.innerHTML = input.value;
+    ol.appendChild(li)
+    input.value = '';
+  }
+})
+
+
+ 
+
+input.style.backgroundColor = "yellow";
+input.style.fontSize = "15px";
+input.style.fontWeight = "900";
+let body = document.querySelector('body');
+body.style.backgroundColor = "rgb( 150, 150, 150)";
+// body.style.textAlign = "center";
+
+
+button.style.backgroundColor = "black";
+button.style.color = "white";
+button.style.fontSize = '20px';
