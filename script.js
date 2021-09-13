dd3911e04360c900e40cda5391eefa6f90adae58
joaovitordantas <joaovitordantasss@hotmail.com>
2021-09-13 18:20:00 -0300

    adicionando botoes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+let taskList = document.getElementById('lista-tarefas').innerHTML;
+let createdItem;
+let taskId;
+let task;
+// para criar as tarefas
 function taskInsert() {
   const taskInput = document.getElementById('texto-tarefa').value;
-  let taskList = document.getElementById('lista-tarefas').innerHTML;
-  taskList += "<li>" + taskInput + "</li>"
+  taskList += '<li>' + taskInput + '</li>';
   document.getElementById('lista-tarefas').innerHTML = taskList;
-  document.getElementById('texto-tarefa').value = "";
+  document.getElementById('texto-tarefa').value = '';
+}
+
+// selecionar as tarefas
+function selectItem() {
+
+}
+
+// remover tarefas
+function removeTask() {
+
 }
