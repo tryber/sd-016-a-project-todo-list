38584ac106b51bd083eef37816dca796924f3813
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2021-09-13 15:12:48 -0300

    adicionado o requisito 7 ao projeto

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ const input = document.getElementById('texto-tarefa');
 botaoAdicionar.addEventListener('click', ()=> {
   listaOrdenada.appendChild(document.createElement('li'));
   listaOrdenada.lastChild.innerText = input.value;
+  
+  listaOrdenada.lastChild.addEventListener('click', (event)=> {
+    const element = event.target;
+    element.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+
   input.value = '';
 });
