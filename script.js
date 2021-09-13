64495cf18a0cc039848d11b22176257cd694c2a7
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-09-13 14:17:10 -0400

    Completa o requisito 6, mantendo a qualidade do codigo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function addTAsk() {
   const valueInput = document.querySelector('input');
   const ordenedList = document.querySelector('#lista-tarefas');
   if (valueInput.value.length > 0) {
+    // função já cria automaticamento os itens em ordem
     const newLi = document.createElement('li');
     newLi.innerText = valueInput.value;
 
