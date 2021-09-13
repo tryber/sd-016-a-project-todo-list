5f11835d674d698801fd76c34200b56f64a1e21e
Jose <josetoitio@gmail.com>
2021-09-13 15:45:07 -0300

    Exercicio 5 e 6 feito

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function createDiv(idName) {
+  const div = document.createElement('div');
+  div.id = idName;
+  document.querySelector('main').appendChild(div);
+}
+
 function addTitle() {
   const title = document.createElement('h1');
   title.innerText = 'Minha Lista de Tarefas';
@@ -9,20 +15,49 @@ function addParagraph() {
   const paragraph = document.createElement('p');
   paragraph.id = 'funcionamento';
   paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
-  document.querySelector('main').appendChild(paragraph);
+  document.querySelector('header').appendChild(paragraph);
 }
 addParagraph();
 
 function addInput() {
   const input = document.createElement('input');
   input.id = 'texto-tarefa';
-  document.querySelector('main').appendChild(input);
+  document.querySelector('#input-button').appendChild(input);
 }
 addInput();
 
 function addOlist() {
   const oList = document.createElement('ol');
   oList.id = 'lista-tarefas';
-  document.querySelector('main').appendChild(oList);
+  createDiv('ol-list');
+  document.querySelector('#ol-list').appendChild(oList);
 }
 addOlist();
+
+function addButton() {
+  const button = document.createElement('button');
+  button.id = 'criar-tarefa';
+  button.innerText = 'Adicionar';
+  document.querySelector('#input-button').appendChild(button);
+}
+addButton();
+function clearInput() {
+  document.querySelector('#texto-tarefa').value = '';
+}
+
+function buttonClick() {
+  const button = document.querySelector('#criar-tarefa');
+  button.addEventListener('click', () => {
+    const olist = document.querySelector('#ol-list');
+    const elementLi = document.createElement('li');
+    const inputValue = document.querySelector('#texto-tarefa').value;
+    if (inputValue === '') {
+      console.log('Caixa vazia');
+    } else {
+      elementLi.innerText = inputValue;
+      olist.appendChild(elementLi);
+      clearInput();
+    }
+  });
+}
+buttonClick();
