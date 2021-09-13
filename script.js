45c3391fbf2ceee5e98d2810d2d37fc7670d3ec8
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-13 17:28:16 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Requisito 5
+// Requisito 5 e 6
 const buttonInput = document.querySelector('#buttonInput');
 const main = document.querySelector('main');
 const buttonCreateTask = document.createElement('button');
@@ -14,4 +14,16 @@ function addTask() {
   li.innerText = input.value;
   ol.appendChild(li);
   document.querySelector('#texto-tarefa').value = '';
+  // Requisito 7
+  let liTags = document.querySelectorAll('li');
+  for (let index = 0; index < liTags.length; index += 1) {
+   liTags[index].addEventListener('click', addBackgroundColor)
+    console.log('aqui');
+  
+    function addBackgroundColor() {
+      if (liTags[index].style.backgroundColor === '') {
+        liTags[index].style.backgroundColor = 'rgb(128, 128, 128)';
+      }
+    }
+  }
 }
