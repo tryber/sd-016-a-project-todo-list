51843d4c08b299cccb33c67f43b9e1b6d7c87a58
Zaqueu <zaqueufernandes@gmail.com>
2021-09-13 18:39:39 -0300

    Criaçao dos arquivos index.html, style.css, script.js e estruturaçao inicial do codigo  HTML

---
+++ script.js
@@ -0,0 +1,11 @@
+function    
+
+let addButton = document.querySelector("#criar-tarefa"); // Seleciona o Botão
+
+addButton.addEventListener('click', function(){
+let addInput = document.querySelector('#texto-tarefa').value; // Acessa o valor digitado no input
+// console.log(addInput);
+
+}) 
+
+    
