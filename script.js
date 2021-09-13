4516c5f3a58fd1879386475a3af617bc7b4df23c
Gustavo Meira <gmeira.antonio@gmail.com>
2021-09-13 16:37:22 -0300

    Decimo segundo requisito

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const listaTarefas = document.querySelector('#lista-tarefas');
 const inputNovaTarefa = document.querySelector('#texto-tarefa');
 const botaoApagaTudo = document.querySelector('#apaga-tudo');
 const botaoRemoveFinalizados = document.querySelector('#remover-finalizados');
+const botaoSalvarTarefas = document.querySelector('#salvar-tarefas');
 
 function adicionarClasseSelecionado(event) {
   const antigoItem = document.querySelector('.selected');
@@ -24,7 +25,7 @@ function adicionarClasseCompleto(event) {
   itemAtual.classList.add('completed');
 }
 
-function criandoItemDaLista() {
+function criandoItemLista() {
   const novaTarefa = document.createElement('li');
   novaTarefa.addEventListener('click', adicionarClasseSelecionado);
   novaTarefa.addEventListener('dblclick', adicionarClasseCompleto);
@@ -33,7 +34,7 @@ function criandoItemDaLista() {
 }
 
 function adicionarNovaTarefa() {
-  const novaTarefa = criandoItemDaLista();
+  const novaTarefa = criandoItemLista();
   listaTarefas.appendChild(novaTarefa);
   inputNovaTarefa.value = '';
 }
@@ -52,6 +53,35 @@ function removerTarefasFinalizadas() {
   });
 }
 
+function salvarTarefas() {
+  const tarefas = document.querySelectorAll('#lista-tarefas li');
+  const arrayTarefas = [];
+  tarefas.forEach((tarefa) => {
+    const arrayTarefa = [tarefa.innerText, tarefa.classList.value];
+    arrayTarefas.push(arrayTarefa);
+  });
+  localStorage.setItem('listaCompleta', JSON.stringify(arrayTarefas));
+}
+
+function criandoItemListaSalva(tarefa) {
+  const novaTarefa = document.createElement('li');
+  novaTarefa.innerText = tarefa[0];
+  novaTarefa.classList.value = tarefa[1];
+  novaTarefa.addEventListener('click', adicionarClasseSelecionado);
+  novaTarefa.addEventListener('dblclick', adicionarClasseCompleto);
+  return novaTarefa;
+}
+
+window.onload = function() {
+  const tarefasSalvas = JSON.parse(localStorage.getItem('listaCompleta'));
+  if (tarefasSalvas !== null) {
+    tarefasSalvas.forEach((tarefa) => {
+      listaTarefas.appendChild(criandoItemListaSalva(tarefa));
+    })
+  }
+}
+
 botaoCriarTarefa.addEventListener('click', adicionarNovaTarefa);
 botaoApagaTudo.addEventListener('click', apagarTodasTarefas);
 botaoRemoveFinalizados.addEventListener('click', removerTarefasFinalizadas);
+botaoSalvarTarefas.addEventListener('click', salvarTarefas);
