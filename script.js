0eb5493936195d40c82d7151185ac75ae708a61e
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-13 16:08:54 -0300

    quinto e sexto requisito

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-
-
+let listaTarefas = document.getElementById("lista-tarefas");
+function trazerFilho() {
+    let filho = document.createElement('li');
+    let valor = document.getElementById('texto-tarefa');
+    filho.innerText = valor.value;
+    listaTarefas.appendChild(filho);
+    valor.value = '';
+}
+let botao = document.getElementById('criar-tarefa');
+botao.addEventListener("click",trazerFilho);
