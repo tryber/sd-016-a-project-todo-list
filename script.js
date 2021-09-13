9704b305aa7a45eabeed5a907fc02f14d484c526
PauloSuriani <paulosuriani@gmail.com>
2021-09-13 16:04:18 -0300

    Requisitos-7

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function adicionaNovaTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
   const novaTarefa = document.createElement('li');
+  novaTarefa.addEventListener('click', () => {
+    novaTarefa.style.backgroundColor = 'rgb(128,128,128)';
+  });
   let input = document.getElementById('texto-tarefa');
   novaTarefa.innerText = input.value;
   input.value = '';
