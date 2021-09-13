6e1b9f1e898f6fd7fbf69bef5ccc4ebee34d7a9e
vitor <vitorsbsilva@gmail.com>
2021-09-13 17:12:08 -0300

    requisito 8 feito

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ button.addEventListener("click", addTask);
 // https://www.w3schools.com/howto/howto_html_clear_input.asp
 
 function addTask() {
+
+    // Requisito 5
     let inputBox = document.getElementById('texto-tarefa');
     let newElement = document.createElement("li");
     let list = document.getElementById("lista-tarefas");
@@ -15,15 +17,23 @@ function addTask() {
 
     inputBox.value = '';
 
+    // Requisito 8
     let listItems = document.body.getElementsByTagName("li");
 
     for (let index = 0; index < listItems.length; index += 1) {
-        listItems[index].addEventListener("click", paintedBackground);
+        listItems[index].addEventListener("click", paintedBackground)
     }
+    
+
 } 
 
-
 function paintedBackground(event) {
-      event.target.style.backgroundColor = "rgb(128, 128, 128)";
-}
+    let oldSelected = document.querySelector(".selected");
+    oldSelected.classList.remove("selected");
+    oldSelected.style.backgroundColor = "white";
 
+    event.target.className = "selected";
+    if (event.target.className === "selected") {
+        event.target.style.backgroundColor = "rgb(128, 128, 128)";
+    }
+}
