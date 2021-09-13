41c4702137321f60629d06d470c702bb51e0b5e3
lucasdoloh0v <lucasdolohov@gmail.com>
2021-09-13 16:22:05 -0300

    requisito 7

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ function addListItem() {
   document.querySelector('input').value = '';
 }
 
+function selectListItem(event) {
+  if (document.querySelectorAll('.selected').length > 0) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
 document.querySelector('#criar-tarefa').addEventListener('click', addListItem);
+document.querySelector('ol').addEventListener('click', selectListItem);
