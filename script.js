98920dc35410f17e80481b0633a42910b7648dba
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-13 16:52:12 -0300

    setimo requisito

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// Declaração de Variáveis e Constantes.
 const selectOl = document.querySelector('#lista-tarefas');
-const selectInput = document.querySelector('#texto-tarefa');
 const selectButton = document.querySelector('#criar-tarefa');
 
+function paintColorList() {
+  const selectLineList = document.querySelectorAll('.tasks-line');
+  for (let index = 0; index < selectLineList.length; index += 1) {
+    selectLineList[index].addEventListener('click', (event) => {
+      selectLineList[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+}
+
+// Evento de clique para botão de adicionar tarefas.
 selectButton.addEventListener('click', () => {
+  const selectInput = document.querySelector('#texto-tarefa');
   if (selectInput.value === '') {
     return alert('Sem Tarefa!, Favor informar um nome de Tarefa!');
   }
@@ -11,4 +22,5 @@ selectButton.addEventListener('click', () => {
   createtasks.classList.add('tasks-line');
   selectOl.appendChild(createtasks);
   selectInput.value = '';
+  paintColorList();
 });
