b3166bf87f27adf7f1b44cda9c1ca568ddc26fa8
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-13 16:38:59 -0300

    Resolvido Requisitos 7

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function createTask() {
-    const getTaskInput = document.querySelector('#texto-tarefa');
-    const textInput = getTaskInput.value;
-    const getList = document.querySelector('#lista-tarefas');
-    const taskLine = document.createElement('li');
-    taskLine.innerText = textInput;
-    getList.appendChild(taskLine);
-    getTaskInput.value = ''
+  const getTaskInput = document.querySelector('#texto-tarefa');
+  const textInput = getTaskInput.value;
+  const getList = document.querySelector('#lista-tarefas');
+  const taskLine = document.createElement('li');
+  taskLine.innerText = textInput;
+  getList.appendChild(taskLine);
+  getTaskInput.value = '';
+  selectListColor();
 }
 const getTaskButton = document.querySelector('#criar-tarefa');
 getTaskButton.addEventListener('click', createTask);
 
-function changeListColor(){
-    const getList = document.querySelectorAll('li');
-    for (let index = 0; index < getList.length; index += 1){
-        getList[index].addEventListener('click', function(event){
-            event.target.style.backgroundColor = 'red';
-        })
-    }
+function selectListColor() {
+  const getList = document.querySelectorAll('li');
+  for (let index = 0; index < getList.length; index += 1) {
+    getList[index].addEventListener('click', function(event){
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+   });
+  }
 }
-changeListColor();
