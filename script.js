b6f9bb7a597aef65666c27419b970b59a342b96b
FelipeVahia <felipevahia@gmail.com>
2021-09-13 16:49:21 -0300

    8) Realizado.

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ function buttomAdd() {
 }
 
 buttomAdd();
+
+function colorListAdd() {
+  const lista = document.getElementById('lista-tarefas');
+
+  lista.addEventListener('click', (event) => {
+    for (index = 0; index < lista.children.length; index += 1) {
+      lista.children[index].style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = 'gray';
+  });
+}
+
+colorListAdd();
