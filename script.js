304f1c2ee03b4c0632576b9b59e9954281554458
PauloSuriani <paulosuriani@gmail.com>
2021-09-13 16:17:00 -0300

    Requisito-8

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function adicionaNovaTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
   const novaTarefa = document.createElement('li');
   novaTarefa.addEventListener('click', () => {
+    let lista = document.getElementsByTagName('li');
+    for (let i = 0; i < lista.length; i += 1) {
+      lista[i].style.backgroundColor = '';
+    }
     novaTarefa.style.backgroundColor = 'rgb(128,128,128)';
   });
   let input = document.getElementById('texto-tarefa');
