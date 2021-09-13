6be2923dc81f6e92a29518215429e5e8f5f7d4bc
PauloSuriani <paulosuriani@gmail.com>
2021-09-13 16:58:05 -0300

    Requisito-9

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function adicionaNovaTarefa() {
     }
     novaTarefa.style.backgroundColor = 'rgb(128,128,128)';
   });
+  novaTarefa.addEventListener('dblclick', () => {
+    novaTarefa.classList.toggle('completed');
+  });
+
   let input = document.getElementById('texto-tarefa');
   novaTarefa.innerText = input.value;
   input.value = '';
   listaTarefas.appendChild(novaTarefa);
 }
+
+// let riscado = document.createElement('s');
+// riscado.innerText = novaTarefa.innerText;
+// novaTarefa.innerText = '';
+// novaTarefa.appendChild(riscado);
+// novaTarefa.classList.toggle('completed');
