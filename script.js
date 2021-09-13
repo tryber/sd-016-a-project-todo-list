25a6ec75e5df48ae6f2bf1e03677b6aa571d4c5a
Leandro Bonfim <leandrobonfim15@homtial.com>
2021-09-13 15:43:50 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const addButton = document.getElementById('criar-tarefa');
 
+function changeTaskBackgroundColor(event) {
+  const task = event.target;
+  const className = 'task-selected';
+  const lastTask = document.getElementsByClassName(className);
+  if (lastTask[0]) {
+    lastTask[0].classList.remove(className);
+  }
+  task.classList.add(className);
+}
+
 function addTask() {
   const task = document.getElementById('texto-tarefa');
-  const taskToList = document.createElement('li');
-  taskToList.innerText = task.value;
-  const list = document.getElementById('lista-tarefas');
-  list.appendChild(taskToList);
-  task.value = '';
+  if (task.value) {
+    const taskToList = document.createElement('li');
+    taskToList.addEventListener('click', changeTaskBackgroundColor);
+    taskToList.innerText = task.value;
+    const list = document.getElementById('lista-tarefas');
+    list.appendChild(taskToList);
+    task.value = '';
+  } else {
+    alert('Digite uma tarefa antes de adicionar');
+  }
 }
 
 addButton.addEventListener('click', addTask);
