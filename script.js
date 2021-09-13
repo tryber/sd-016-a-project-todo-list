38ac5435366b17716f29b0d48b21642cb96a9359
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-09-13 17:26:19 -0400

    Completa o requisito 7, mantendo a qualidade do codigo novamente

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
+const valueInput = document.querySelector('input');
+const ordenedList = document.querySelector('#lista-tarefas');
+const button = document.querySelector('#criar-tarefa');
+
+function itemColor(event) {
+  const paint = event;
+  paint.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function addTAsk() {
-  const valueInput = document.querySelector('input');
-  const ordenedList = document.querySelector('#lista-tarefas');
   if (valueInput.value.length > 0) {
     // função já cria automaticamento os itens em ordem
     const newLi = document.createElement('li');
     newLi.innerText = valueInput.value;
     ordenedList.appendChild(newLi);
     valueInput.value = '';
+
+    // evento de click nos itens da lista que muda a cor
+    newLi.addEventListener('click', itemColor);
   }
 } addTAsk();
 
-function itemColor(event) {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
-
 function events() {
-  const button = document.querySelector('#criar-tarefa');
-  const itemList = document.querySelector('#lista-tarefas');
   // evento de click no button
   button.addEventListener('click', addTAsk);
-  // evento de click nos itens da lista
-  itemList.addEventListener('click', itemColor);
 } events();
