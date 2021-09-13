bf2388f63de1565a3716bd0e6d2e14ebbaffa932
Gregory <gregorysak90@gmail.com>
2021-09-13 18:00:41 -0300

    Requisitos 5 e 6, feitos.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// init project
+// Requisitos 5 e 6
+
+function addTasks(){
+  let textInput = document.getElementById('texto-tarefa');
+  let tasksList = document.getElementById('lista-tarefas');
+  let task = document.createElement('li');
+  task.innerText = textInput.value;
+  tasksList.appendChild(task);
+  textInput.value = '';
+}
+
+document.querySelector('#criar-tarefa').addEventListener('click', addTasks);
