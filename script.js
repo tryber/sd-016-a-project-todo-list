dbd9e69e758659eacd5b9ff0728399aba74fb3d8
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-09-13 20:47:12 -0300

    corrigindo erros do lint e a referencia do Carlos

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+// Referência do código ao Brunão Turma 16 - Tribo A;
+function clearNotSelectedColor() {
+  const getOlChildren = document.querySelector('ol').children;
+  for (let index = 0; index < getOlChildren.length; index += 1) {
+    getOlChildren[index].style.backgroundColor = 'white';
+  }
+}
+
 function changeBackGroundColor() {
   const getLi = document.querySelectorAll('li');
   for (let index = 0; index < getLi.length; index += 1) {
-    getLi[index].addEventListener('click', function() {
+    getLi[index].addEventListener('click', function anonimous() {
       // Referência do código ao Brunão Turma 16 - Tribo A;
       clearNotSelectedColor();
       getLi[index].style.backgroundColor = 'rgb(128,128,128)';
-    })
+    });
   }
 }
-// Referência do código ao Brunão Turma 16 - Tribo A;
-function clearNotSelectedColor() {
-  const getOl = document.querySelector('ol').children;
-  for (let index = 0; index < getOl.length; index += 1) {
-    getOl[index].style.backgroundColor = 'white';  
-  }
-}
-// Referência do código para o Rafael Dal Soler, Turma 16 - Tribo A;
+
+// Referência do código para o Carlos Dal Soler, Turma 16 - Tribo A;
 function changeTextList(event) {
   let validate = false;
   if (event.target.classList.contains('completed')) {
@@ -24,13 +26,13 @@ function changeTextList(event) {
   } else if (validate === false) {
     event.target.classList.add('completed');
     validate = false;
-  } 
+  }
 }
 
 function deletedTasks() {
   const getButton = document.querySelector('#apaga-tudo');
   const getLi = document.querySelectorAll('li');
-  getButton.addEventListener('click', function() {
+  getButton.addEventListener('click', function anonimous() {
     for (let index = 0; index < getLi.length; index += 1) {
       getLi[index].remove();
     }
@@ -41,8 +43,8 @@ function deleteCompletedTasks() {
   const getButtonCompleted = document.querySelector('#remover-finalizados');
   const getLi = document.querySelectorAll('li');
   for (let index = 0; index < getLi.length; index += 1) {
-    getButtonCompleted.addEventListener('click', function x() {
-      //https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList, referenciando o método contains.
+    getButtonCompleted.addEventListener('click', function anonimous() {
+      // https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList, referenciando o método contains.
       if (getLi[index].classList.contains('completed')) {
         getLi[index].remove();
       }
@@ -55,17 +57,17 @@ function addList() {
   const getOl = document.querySelector('#lista-tarefas');
   const createLi = document.createElement('li');
   createLi.innerText = getInput.value;
-  getOl.appendChild(createLi);  
+  getOl.appendChild(createLi);
   getInput.value = '';
 
   changeBackGroundColor();
-  createLi.addEventListener('dblclick',changeTextList);
+  // Referência do código para o Carlos Dal Soler, Turma 16 - Tribo A;
+  createLi.addEventListener('dblclick', changeTextList);
   deletedTasks();
   deleteCompletedTasks();
 }
 
-function actionList() {
-  const getButton = document.querySelector('#criar-tarefa');
-  getButton.addEventListener('click', addList);
+function cretatList() {
+  document.querySelector('#criar-tarefa').addEventListener('click', addList);
 }
-actionList();
+cretatList();
