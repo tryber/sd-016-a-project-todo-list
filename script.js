c240adadb36ecfc19ec3daaabd0f52071965b8c6
Gustavo Meira <gmeira.antonio@gmail.com>
2021-09-13 17:11:28 -0300

    Decimo terceiro requisito

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const inputNovaTarefa = document.querySelector('#texto-tarefa');
 const botaoApagaTudo = document.querySelector('#apaga-tudo');
 const botaoRemoveFinalizados = document.querySelector('#remover-finalizados');
 const botaoSalvarTarefas = document.querySelector('#salvar-tarefas');
+const botaoMoverCima = document.querySelector('#mover-cima');
+const botaoMoverBaixo = document.querySelector('#mover-baixo');
 
 function adicionarClasseSelecionado(event) {
   const antigoItem = document.querySelector('.selected');
@@ -63,25 +65,60 @@ function salvarTarefas() {
   localStorage.setItem('listaCompleta', JSON.stringify(arrayTarefas));
 }
 
-function criandoItemListaSalva(tarefa) {
+function criandoItemPorArray(tarefa) {
   const novaTarefa = document.createElement('li');
   novaTarefa.innerText = tarefa[0];
   novaTarefa.classList.value = tarefa[1];
+  novaTarefa.classList.remove('selected');
   novaTarefa.addEventListener('click', adicionarClasseSelecionado);
   novaTarefa.addEventListener('dblclick', adicionarClasseCompleto);
   return novaTarefa;
 }
 
-window.onload = function() {
-  const tarefasSalvas = JSON.parse(localStorage.getItem('listaCompleta'));
-  if (tarefasSalvas !== null) {
-    tarefasSalvas.forEach((tarefa) => {
-      listaTarefas.appendChild(criandoItemListaSalva(tarefa));
-    })
+function trocandoElementosHTML(elemento1, elemento2) {
+  const tarefa1 = [elemento1.innerText, elemento1.classList.value];
+  elemento1.innerText = elemento2.innerText;
+  elemento1.classList.value = elemento2.classList.value;
+  elemento2.innerText = tarefa1[0];
+  elemento2.classList.value = tarefa1[1];
+}
+
+function moverTarefaCima() {
+  const tarefaParaSerMovida = document.querySelector('.selected');
+  if (tarefaParaSerMovida === null) {
+    return alert('Você não tem nenhuma tarefa selecionada!');
   }
+  const tarefaAcima = tarefaParaSerMovida.previousElementSibling;
+  if (tarefaAcima === null) {
+    return alert('Sua tarefa selecionada já é a primeira!');
+  }
+  trocandoElementosHTML(tarefaParaSerMovida, tarefaAcima);
+}
+
+function moverTarefaBaixo() {
+  const tarefaParaSerMovida = document.querySelector('.selected');
+  if (tarefaParaSerMovida === null) {
+    return alert('Você não tem nenhuma tarefa selecionada!');
+  }
+  const tarefaAbaixo = tarefaParaSerMovida.nextElementSibling;
+  if (tarefaAbaixo === null) {
+    return alert('Sua tarefa selecionada já é a ultima!');
+  }
+  trocandoElementosHTML(tarefaParaSerMovida, tarefaAbaixo);
 }
 
 botaoCriarTarefa.addEventListener('click', adicionarNovaTarefa);
 botaoApagaTudo.addEventListener('click', apagarTodasTarefas);
 botaoRemoveFinalizados.addEventListener('click', removerTarefasFinalizadas);
 botaoSalvarTarefas.addEventListener('click', salvarTarefas);
+botaoMoverCima.addEventListener('click', moverTarefaCima);
+botaoMoverBaixo.addEventListener('click', moverTarefaBaixo);
+
+window.onload = function() {
+  const tarefasSalvas = JSON.parse(localStorage.getItem('listaCompleta'));
+  if (tarefasSalvas !== null) {
+    tarefasSalvas.forEach((tarefa) => {
+      listaTarefas.appendChild(criandoItemPorArray(tarefa));
+    })
+  }
+}
