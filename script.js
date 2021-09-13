5e6795cf70416f15631d08b440bd2294a454812c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-09-13 18:44:43 -0300

    requisito 10

--- script.js
+++ script.js
@@ -13,21 +13,49 @@ function addList() {
   getInput.value = '';
 
   changeBackGroundColor();
+  changeTextList();
+  deletedTasks()
 }
 
 function changeBackGroundColor() {
   const getLi = document.querySelectorAll('li');
   for (let index = 0; index < getLi.length; index += 1) {
     getLi[index].addEventListener('click', function() {
+      // Referência do código ao Brunão Turma 16 - Tribo A;
       clearNotSelectedColor();
       getLi[index].style.backgroundColor = 'rgb(128,128,128)';
     })
   }
 }
-
+// Referência do código ao Brunão Turma 16 - Tribo A;
 function clearNotSelectedColor() {
   const getOl = document.querySelector('ol').children;
   for (let index = 0; index < getOl.length; index += 1) {
     getOl[index].style.backgroundColor = 'white';  
   }
+}
+
+function changeTextList() {
+  const getLi = document.querySelector('ol').children;
+  for (let index = 0; index < getLi.length; index += 1) {
+    getLi[index].addEventListener('dblclick', function(event) {     
+      if (getLi[index].className === '') {
+        getLi[index].classList.add('completed');
+        console.log(getLi[index])
+      } else {
+        getLi[index].className = '';
+        console.log(getLi[index]);
+      }
+    })
+  }
+}
+
+function deletedTasks() {
+  const getButton = document.querySelector('#apaga-tudo');
+  const getLi = document.querySelectorAll('li');
+  getButton.addEventListener('click', function() {
+    for (let index = 0; index < getLi.length; index += 1) {
+      getLi[index].remove();
+    }
+  });
 }
