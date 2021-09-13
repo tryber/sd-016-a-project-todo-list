cc8a8a6eccb734673de5a1d18bff9381db3effba
Your Name <you@example.com>
2021-09-13 18:22:38 -0300

    requisito 9 - concluido

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ function createLi() {
   novaLi.innerText = contentInput.value;
   localOl.appendChild(novaLi);
   contentInput.value = '';
+
   novaLi.addEventListener('click', function () {
     novaLi.style.backgroundColor = 'rgb(128,128,128)';
+  });
+
+  novaLi.addEventListener('dblclick', function () {
+    if (novaLi.className === '') {
+      novaLi.className = 'completed';
+    } else if (novaLi.className === 'completed') {
+      novaLi.className = 'notCompleted';
+    }
   });
 }
 
@@ -23,4 +32,4 @@ function erasedIten() {
   localOl.innerHTML = '';
 }
 
-buttonApagar.addEventListener('click',erasedIten);
+buttonApagar.addEventListener('click', erasedIten);
