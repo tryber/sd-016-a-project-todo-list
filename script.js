1d02c3da5a9c25094f6518c3dc33a6ff94bb13b5
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-13 18:52:20 -0300

    requisito 1 ao 8 - ok

--- script.js
+++ script.js
@@ -20,18 +20,62 @@ function eventCreateTarefa() {
 }
 eventCreateTarefa();
 
+/////////////////////////////
+
 function colorLi(event) {
     let li = document.querySelector('#lista-tarefas').children
     // let li = unorderedList.children;
     for (let index = 0; index < li.length; index += 1) {
-        event.target.style.backgroundColor = "rgb(128, 128, 128)"
+        li[index].style.backgroundColor = "rgb(255, 255, 255)"
     }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
 }
 
 function eventColorLi() {
-    let unorderedList = document.querySelector('#lista-tarefas');
-    let li = unorderedList.children;
+    let li = document.querySelector('#lista-tarefas').children
     for (let index = 0; index < li.length; index += 1) {
         li[index].addEventListener('click', colorLi)
     }
 }
+
+///////////////////////////
+
+function completedTarefa(event) {
+    // as tarefas sao as li filhas de ul
+    let li = document.querySelector('#lista-tarefas').children;
+    for (let index = 0; index < li.length; index += 1) {
+        event.target.classList.add('completed')
+    }
+}
+
+function removeCompletedTarefa() {
+    let li = document.querySelector('#lista-tarefas').children;
+    for (let index = 0; index < li.length; index += 1) {
+        if (li[index].className === 'completed') {
+        li[index].classList.remove('completed')    
+        }
+    }
+    eventCompletedTarefa() 
+}
+
+function eventCompletedTarefa() {
+    let li = document.querySelector('#lista-tarefas').children;
+    for (let index = 0; index < li.length; index += 1) {
+        li[index].addEventListener('dblclick', completedTarefa)
+    }
+}
+eventCompletedTarefa() 
+
+function eventRemoveCompletedTarefa() {
+    let li = document.querySelector('#lista-tarefas').children;
+    for (let index = 0; index < li.length; index += 1) {
+        li[index].addEventListener('dblclick', removeCompletedTarefa)
+    }
+}
+eventRemoveCompletedTarefa();
+
+// teste que dá classe selected para apenas 1 li
+// cria todos os Li
+// todos sem classe e com background color igual
+// cria função que add uma determinada classe ao Li[i] selecionado
+// ao li selecionado aplicar a mudança de color
