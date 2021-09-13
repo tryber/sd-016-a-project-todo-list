7fcb758460f6eae48477b6cda5313e373fa16ba7
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2021-09-13 17:03:44 -0300

    adicionado o requisito 11 ao projeto

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const listaOrdenada = document.getElementById('lista-tarefas');
 const botaoAdicionar = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const botaoApagarTudo = document.getElementById('apaga-tudo');
+const botaoApagarFinalizados = document.getElementById('remover-finalizados');
 
 botaoAdicionar.addEventListener('click', ()=> {
   listaOrdenada.appendChild(document.createElement('li'));
   listaOrdenada.lastChild.innerText = input.value;    
   
   listaOrdenada.lastChild.addEventListener('click', (event) => {
+    const element = event.target;
     for (let x = 0; x < listaOrdenada.children.length; x += 1) {
       listaOrdenada.children[x].style.backgroundColor = 'white';
     };
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
+    element.style.backgroundColor = 'rgb(128,128,128)';
 
   })
 
@@ -32,4 +34,11 @@ botaoApagarTudo.addEventListener('click', () => {
    do {
     listaOrdenada.children[0].remove();
   } while (listaOrdenada.children.length !== 0)
+});
+
+botaoApagarFinalizados.addEventListener('click', () => {
+  let completed = document.getElementsByClassName('completed');
+  do {
+    completed[0].remove();
+  } while (completed.length !== 0)
 });
