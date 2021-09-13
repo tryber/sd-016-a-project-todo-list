807fcc644822b57106785805686b9291e453d0e0
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-13 18:04:30 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let listaTarefas = document.getElementById("lista-tarefas");
 function trazerFilho() {
     let filho = document.createElement('li');
+    filho.className = 'item-lista';
     let valor = document.getElementById('texto-tarefa');
     filho.innerText = valor.value;
     listaTarefas.appendChild(filho);
     valor.value = '';
+    
 }
 let botao = document.getElementById('criar-tarefa');
-botao.addEventListener("click",trazerFilho);
+botao.addEventListener('click',trazerFilho);
 
+listaTarefas.addEventListener('click', mudaCor);
 
+function mudaCor(event) {
+ let click = event.target;
+ click.style.backgroundColor = "rgb(128, 128, 128)";
+ click.classList.add('selecionar') ;
+}
