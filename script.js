d78f56913b01f5b9d6e71bd2beca298f0067758d
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-09-13 18:58:23 -0300

    Alteraçao do codigo, mudança da cor do item da lista ao clicar

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 
-let button = document.querySelector('#criar-tarefa')
-let list = document.querySelector('#lista-tarefas')
-let input = document.querySelector('#texto-tarefa')
-let li = document.querySelectorAll('#lista-tarefas li')
-button.addEventListener('click', function(){
-let newTask = input.value;
-let createListItem = document.createElement('li');
-createListItem.addEventListener('click', function(){
-    createListItem.style.backgroundColor = 'rgb(128,128,128)'
+let button = document.querySelector('#criar-tarefa');
+let list = document.querySelector('#lista-tarefas');
+let input = document.querySelector('#texto-tarefa');
+
+let taskList = []
+
+
+function createTask(){
+
+    for (task of taskList){
+        let newTask = document.createElement('li');
+        let textTask = document.createTextNode(task)
+        newTask.appendChild(textTask);
+        list.appendChild(newTask)
+
+        taskList = []
+    }
+}
+
+createTask()
+
+
+function addTask(){
+    let inputText = input.value
+    taskList.push(inputText)
+    input.value = ''
+
+    createTask()
+}
+
+button.addEventListener('click', addTask)
+
+list.addEventListener('click', function (event){
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
 })
 
-createListItem.appendChild(document.createTextNode(newTask));
-list.appendChild(createListItem);
-input.value = '';
-})
+
+
+
+
+
+
+
+
 
