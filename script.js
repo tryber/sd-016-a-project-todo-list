f2d4ff931f155b5928b466e26342b420a0d1d967
alt-art <pedromendescraft@gmail.com>
2021-09-13 20:51:34 -0300

    Add save state

--- script.js
+++ script.js
@@ -3,22 +3,35 @@ const todoSubmitButton = document.querySelector('#criar-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
 const clearAllButton = document.querySelector('#apaga-tudo');
 const clearTerminatedButton = document.querySelector('#remover-finalizados');
+const saveButton = document.querySelector('#salvar-tarefas');
 
-let selected = null;
+todoList.innerHTML = window.localStorage.items === undefined ? '' : window.localStorage.items;
+
+function complete(event) {
+  const elem = event.target;
+  elem.classList.toggle('completed');
+}
+
+function select(event) {
+  const elem = event.target;
+  const selected = document.querySelector('.selected');
+  if (selected) {
+    selected.classList.toggle('selected');
+  }
+  elem.classList.toggle('selected');
+}
+
+const items = document.querySelectorAll('li');
+items.forEach((elem) => {
+  elem.addEventListener('click', select);
+  elem.addEventListener('dblclick', complete);
+});
 
 todoSubmitButton.addEventListener('click', () => {
   const item = document.createElement('li');
   item.innerText = todoTextInput.value;
-  item.addEventListener('click', () => {
-    if (selected) {
-      selected.style.backgroundColor = 'rgb(0, 0, 0)';
-    }
-    item.style.backgroundColor = 'rgb(128, 128, 128)';
-    selected = item;
-  });
-  item.addEventListener('dblclick', () => {
-    item.classList.toggle('completed');
-  });
+  item.addEventListener('click', select);
+  item.addEventListener('dblclick', complete);
   todoList.appendChild(item);
   todoTextInput.value = '';
 });
@@ -32,4 +45,8 @@ clearAllButton.addEventListener('click', () => {
 clearTerminatedButton.addEventListener('click', () => {
   const completed = document.querySelectorAll('.completed');
   completed.forEach((elem) => elem.remove());
+});
+
+saveButton.addEventListener('click', () => {
+  window.localStorage.items = todoList.innerHTML;
 });
