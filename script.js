d6cd6ebb9a224d6c9a9179466b1f6a0025ac2fac
joao <jgsspinelli92@gmail.com>
2021-09-13 19:54:26 -0300

    resolve os erros do lint

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
+function completedTasks(doubleClick) {
+  doubleClick.target.classList.toggle('completed');
+}
+
 function createListItem() {
   const listItem = document.createElement('li');
   const input = document.getElementById('texto-tarefa');
-  const parentElement = document.getElementById('lista-tarefas')
-  listItem.className = 'list-item'
-  listItem.innerText = input.value
-  listItem.addEventListener('dblclick', completedTasks)
-  parentElement.appendChild(listItem)
+  const parentElement = document.getElementById('lista-tarefas');
+  listItem.className = 'list-item';
+  listItem.innerText = input.value;
+  listItem.addEventListener('dblclick', completedTasks);
+  parentElement.appendChild(listItem);
   input.value = '';
 }
-document.getElementById('criar-tarefa').addEventListener('click', createListItem)
-
+document.getElementById('criar-tarefa').addEventListener('click', createListItem);
 
 function eraseAll() {
   const listItems = document.querySelectorAll('.list-item');
 
   for (let index = 0; index < listItems.length; index += 1) {
-   listItems[index].remove();
+    listItems[index].remove();
   }
 }
 document.getElementById('apaga-tudo').addEventListener('click', eraseAll);
 
-function completedTasks(doubleClick) {
-  doubleClick.target.classList.toggle('completed');
-}
-
 function eraseCompleted() {
-  const listCompletedItems = document.querySelectorAll('.completed')
+  const listCompletedItems = document.querySelectorAll('.completed');
 
   for (let index = 0; index < listCompletedItems.length; index += 1) {
     listCompletedItems[index].remove();
   }
 }
 document.getElementById('remover-finalizados').addEventListener('click', eraseCompleted);
-
