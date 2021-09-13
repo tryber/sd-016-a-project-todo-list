72ecd1886529e013709f6c1988bdd2fb857ee751
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-13 15:40:39 -0300

    Resolvido Requisitos 6

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ function createTask() {
     const textInput = getTaskInput.value;
     const getList = document.querySelector('#lista-tarefas');
     const taskLine = document.createElement('li');
-    taskLine.innerHTML = textInput;
+    taskLine.innerText = textInput;
     getList.appendChild(taskLine);
     getTaskInput.value = ''
 }
 const getTaskButton = document.querySelector('#criar-tarefa');
-getTaskButton.addEventListener('click', createTask)
+getTaskButton.addEventListener('click', createTask);
+
+function changeListColor(){
+    const getList = document.querySelectorAll('li');
+    for (let index = 0; index < getList.length; index += 1){
+        getList[index].addEventListener('click', function(event){
+            event.target.style.backgroundColor = 'red';
+        })
+    }
+}
+changeListColor();
