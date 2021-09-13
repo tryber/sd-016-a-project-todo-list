39e6a57d2c515e7033c6c12c6948fed013e0395f
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-13 17:17:32 -0300

    tentativa requisito 7 novamente

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 let listaTarefas = document.getElementById("lista-tarefas");
-let filho = document.createElement('li');
 function trazerFilho() {
+    let filho = document.createElement('li');
     let valor = document.getElementById('texto-tarefa');
     filho.innerText = valor.value;
     listaTarefas.appendChild(filho);
     valor.value = '';
 }
 let botao = document.getElementById('criar-tarefa');
-botao.addEventListener('click',trazerFilho);
-
-listaTarefas.addEventListener('click', criaClasse);
+botao.addEventListener("click",trazerFilho);
 
 
-function criaClasse() {
-    filho.className = 'selecionar';
-    
-}
-
-listaTarefas.addEventListener('click', mudaCor);
-
-function mudaCor() {
-  filho.style.backgroundColor = 'rgb(128, 128, 128)';
-}
