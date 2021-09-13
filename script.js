16d4186e713d7179491f134d433880e75debd329
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-13 15:58:00 -0300

    sexto requisito

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ selectButton.addEventListener('click', () => {
     return alert('Sem Tarefa!, Favor informar um nome de Tarefa!');
   }
   const createtasks = document.createElement('li');
-  createtasks.innerHTML = selectInput.value.toUpperCase();
+  createtasks.innerHTML = selectInput.value;
   createtasks.classList.add('tasks-line');
   selectOl.appendChild(createtasks);
   selectInput.value = '';
