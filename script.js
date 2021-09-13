63c45e6bcb22c01e5a8645b07f591199dbc01bfa
Emerson <eemr3@yahoo.com.br>
2021-09-13 16:53:48 -0300

    Adiciona funcionalidade dos requistos: 7 e 8

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-function addNewTask() {
-  const btnAddNewTask = document.querySelector('#criar-tarefa');
-  const inputTask = document.querySelector('#texto-tarefa');
-  const lisTask = document.querySelector('#lista-tarefas');
-
-  btnAddNewTask.addEventListener('click', () => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = inputTask.value;
-    lisTask.appendChild(listItem);
-    inputTask.value = '';
-  });
+function removeAddClassItemList(event) {
+  const classListItem = document.querySelector('.selected');
+  const eventListItem = event.target;
+  classListItem.classList.remove('selected');
+  classListItem.style.backgroundColor = 'unset';
+  eventListItem.classList.add('selected');
+  eventListItem.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
-addNewTask();
+function addNewTask() {
+  const inputTask = document.querySelector('#texto-tarefa');
+  if (inputTask.value === '') {
+    return alert('Não pode ser adicionado uma tarfa vazia');
+  }
+  const lisTask = document.querySelector('#lista-tarefas');
+  const listItem = document.createElement('li');
+  listItem.classList.add('item-list');
+  listItem.innerHTML = inputTask.value;
+  listItem.addEventListener('click', removeAddClassItemList);
+
+  lisTask.appendChild(listItem);
+  inputTask.value = '';
+}
+
+function getItensList() {
+  const itemListCalss = document.querySelector('.item-list');
+  return itemListCalss;
+}
+
+function addClassItemList() {
+  addNewTask();
+  const itemList = getItensList();
+  itemList.classList.add('selected');
+}
+
+const btnAddNewTask = document.querySelector('#criar-tarefa');
+btnAddNewTask.addEventListener('click', addClassItemList);
