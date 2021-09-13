628b92e16b2f5fe08c919051cd19038cdec8ada5
rbfeliciano <rb.feliciano@hotmail.com>
2021-09-13 17:01:54 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function addtask() {
-    const taskText  = document.querySelector('#texto-tarefa');
-    const text = taskText .value;
-    const list = document.querySelector('#lista-tarefas');
-    const generateList  = document.createElement('li');
-    generateList .innerHTML = text
-    list.appendChild(generateList );
-    taskText .value = ''
-}
 const taskButton = document.querySelector('#criar-tarefa');
-taskButton.addEventListener('click', addtask)
+taskButton.addEventListener('click', addtask);
+
+function selectListColor() {
+  const selecList = document.querySelectorAll('li');
+  for (let i = 0; i < selecList.length; i += 1) {
+    selecList[i].addEventListener('click', function(event){
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+}
+
+function addtask() {
+  const taskText = document.querySelector('#texto-tarefa');
+  const text = taskText.value;
+  const list = document.querySelector('#lista-tarefas');
+  const generateList = document.createElement('li');
+  generateList.innerHTML = text
+  list.appendChild(generateList);
+  taskText.value = '';
+  selectListColor();
+}
