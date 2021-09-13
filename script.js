eb43608eb2494efa1eb54f207d124b1ec80d45ab
Laura <lahfumagalli@gmail.com>
2021-09-13 16:55:35 -0300

    requisito 6

--- script.js
+++ script.js
@@ -3,12 +3,32 @@
 let textInput = document.getElementById('texto-tarefa')
 let roadButton = document.querySelector('#criar-tarefa')
 roadButton.innerHTML = 'Adicionar Tarefa'
-function addAssign () {
-    let ordList = document.getElementById('lista-tarefas');
-    let newListItem = document.createElement('li');
-    newListItem.innerText = textInput.value
-    ordList.appendChild(newListItem)
+function addAssign() {
+  let ordList = document.getElementById('lista-tarefas');
+  let newListItem = document.createElement('li');
+  newListItem.innerText = textInput.value
+  ordList.appendChild(newListItem)
+  textInput.value = '';
 }
 roadButton.addEventListener('click', addAssign)
 
-// Ordene os itens da lista de tarefas por ordem de criação
+function addLiListenner(event) {
+
+  const listLi = document.querySelectorAll('ol')
+  for (let index = 0; index < listLi.length; index += 1) {
+    listLi[index].addEventListener('click', function (event) {
+      for (let i = 0; i < listLi.length; i += 1) {
+       listLi[i].style.background = 'white'
+      }
+      event.target.style.background = 'rgb(128,128,128)'
+    })
+  }
+}
+
+document.querySelector('#lista-tarefas').addEventListener('click', addLiListenner);
+
+
+// let caminhoDeOndeCliquei = document.querySelectorAll('.pixel');
+// for (let i = 0; i < caminhoDeOndeCliquei.length; i += 1){
+// caminhoDeOndeCliquei[i].addEventListener('click', pintarQuadrado);
+// }
