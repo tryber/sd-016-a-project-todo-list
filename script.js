49ea0231e65f56d13897a7c8732a342f8de6c659
Felipe David <felipe.besorean@hotmail.com>
2021-09-14 15:56:12 -0300

    adiciona botao criar tarefa

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function createOl() {
-  const oL = document.createElement('ol');
-  document.body.appendChild(oL);
-  oL.id = 'lista-tarefas';
+const input = document.querySelector('#texto-tarefa');
+const addButton = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
+
+function createItem(expectedText) {
+  if (expectedText.target === addButton && input.value !== '') {
+    const li = document.createElement('li');
+    list.appendChild(li).innerHTML = input.value;
+    input.value = '';
+  }
 }
 
-createOl();
+document.addEventListener('click', createItem);
