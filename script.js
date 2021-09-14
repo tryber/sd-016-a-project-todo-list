8b8bea53bd8e3395b96dc6f9b44a8c1b3803c90b
Leandro Bonfim <leandrobonfim15@homtial.com>
2021-09-13 23:29:43 -0300

    requisito 12

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const addButton = document.getElementById('criar-tarefa');
 const clearButton = document.getElementById('apaga-tudo');
 const clearCompletedButton = document.getElementById('remover-finalizados');
+const inputTasks = document.getElementById('texto-tarefa');
+const listTasks = document.getElementById('lista-tarefas');
+const saveButton = document.getElementById('salvar-tarefas');
 
 function AddSelectedClassToListItem(event) {
   const task = event.target;
@@ -27,14 +30,12 @@ function addEventToList(listElement) {
 }
 
 function addTask() {
-  const task = document.getElementById('texto-tarefa');
-  if (task.value) {
+  if (inputTasks.value) {
     const taskToList = document.createElement('li');
     addEventToList(taskToList);
-    taskToList.innerText = task.value;
-    const list = document.getElementById('lista-tarefas');
-    list.appendChild(taskToList);
-    task.value = '';
+    taskToList.innerText = inputTasks.value;
+    listTasks.appendChild(taskToList);
+    inputTasks.value = '';
   } else {
     alert('Digite uma tarefa antes de adicionar');
   }
@@ -43,7 +44,7 @@ function addTask() {
 addButton.addEventListener('click', addTask);
 
 function clearTasks() {
-  const tasks = document.getElementById('lista-tarefas').children;
+  const tasks = listTasks.children;
   for (let index = tasks.length - 1; index > -1; index -= 1) {
     const task = tasks[index];
     task.parentNode.removeChild(task);
@@ -61,3 +62,77 @@ function clearComplete() {
 }
 
 clearCompletedButton.addEventListener('click', clearComplete);
+
+function getTaskKeys() {
+  let index = 0;
+  const keys = [];
+  const taskKeys = [];
+  while (localStorage.key(index)) {
+    keys.push(localStorage.key(index));
+    index += 1;
+  }
+  for (index = 0; index < keys.length; index += 1) {
+    const regeExp = /tarefa[0-9]+/;
+    const key = keys[index];
+    if (key.match(regeExp)) {
+      taskKeys.push(key);
+    }
+  }
+  let k = taskKeys;
+  k = k.sort((a, b) => parseInt(a.match(/[0-9]+/)[0], 10) - parseInt(b.match(/[0-9]+/)[0], 10));
+  return k;
+}
+
+function removeSavedTasks(_positionTask = 0) {
+  const taskKeys = getTaskKeys();
+  for (let index = taskKeys.length - 1; index > -1; index -= 1) {
+    if (index >= _positionTask) {
+      localStorage.removeItem(`tarefa${index}`);
+    }
+  }
+  return taskKeys;
+}
+
+function saveTasks() {
+  const tasks = listTasks.children;
+  removeSavedTasks(tasks.length);
+  const tasksCompleted = [];
+  for (let index = 0; index < tasks.length; index += 1) {
+    const task = tasks[index].innerText;
+    localStorage.setItem(`tarefa${index}`, task);
+    if (tasks[index].className.match(/completed/)) {
+      tasksCompleted.push(index);
+    }
+  }
+  if (tasksCompleted.length) {
+    localStorage.tasksCompleted = tasksCompleted.join(',');
+  } else {
+    localStorage.removeItem('tasksCompleted');
+  }
+}
+
+saveButton.addEventListener('click', saveTasks);
+
+function loadTaskList() {
+  const keys = getTaskKeys();
+  const tasksCompleted = localStorage.tasksCompleted.split(',');
+  let taskIndex = 0;
+  for (let index = 0; index < keys.length; index += 1) {
+    const listItem = document.createElement('li');
+    listItem.innerText = localStorage.getItem(keys[index]);
+    addEventToList(listItem);
+    listTasks.appendChild(listItem);
+    if (index === parseInt(tasksCompleted[taskIndex], 10)) {
+      listItem.classList.add('completed');
+      taskIndex += 1;
+    }
+  }
+}
+
+function checkSaves() {
+  if (localStorage.tasksCompleted) {
+    loadTaskList();
+  }
+}
+
+window.onload = checkSaves;
