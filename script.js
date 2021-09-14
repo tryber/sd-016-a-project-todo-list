0dd9a4c0705d9f0b3275994399e82203ed9e99e3
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-13 22:49:44 -0300

    decimo requisito

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
-// Declaração de Variáveis e Constantes.
+// Declaração de Variáveis e Constantes Globais.
 const selectOl = document.querySelector('#lista-tarefas');
 const selectButton = document.querySelector('#criar-tarefa');
+const selectInput = document.querySelector('#texto-tarefa');
+const clearButton = document.querySelector('#apaga-tudo');
+let selectLineList = null;
 
 // Função remove cor de fundo das linhas.
 function removeColorLine() {
-  const selectLineList = document.querySelectorAll('.tasks-line');
   for (let index = 0; index < selectLineList.length; index += 1) {
     selectLineList[index].style.backgroundColor = '';
   }
 }
 
 // Função que preenche o fundo da linha selecionada.
-function paintColorList() {
-  const selectLineList = document.querySelectorAll('.tasks-line');
+function paintColorList(event) {
+  const events = event.target;
   for (let index = 0; index < selectLineList.length; index += 1) {
-    selectLineList[index].addEventListener('click', () => {
-      removeColorLine();
-      selectLineList[index].style.backgroundColor = 'rgb(128, 128, 128)';
-    });
+    removeColorLine();
+    events.style.backgroundColor = 'rgb(128, 128, 128)';
   }
 }
 
 // Função que passa um risco na Tarefa quando clicado duas vezes.
-function doubleClick() {
-  const selectLineList = document.querySelectorAll('.tasks-line');
-  for (let index = 0; index < selectLineList.length; index += 1) {
-    selectLineList[index].addEventListener('dblclick', () => {
-      selectLineList[index].style.textDecoration = 'line-through';
-    });
+function addClassCompleted(event) {
+  if (!event.target.classList.contains('completed')) {
+    event.target.classList.add('completed');
+  } else {
+    event.target.classList.remove('completed');
   }
 }
 
-// Evento de clique para botão de adicionar tarefas.
-selectButton.addEventListener('click', () => {
-  const selectInput = document.querySelector('#texto-tarefa');
-  if (selectInput.value === '') {
-    return alert('Sem Tarefa!, Favor informar um nome de Tarefa!');
+// Função Apaga todas as tarefas criadas
+function removeTasksList() {
+  for (let index = 0; index < selectLineList.length; index += 1) {
+    selectOl.removeChild(selectLineList[index]);
   }
+}
+
+function generatorMain() {
+  for (let index = 0; index < selectLineList.length; index += 1) {
+    selectLineList[index].addEventListener('click', paintColorList);
+    selectLineList[index].addEventListener('dblclick', addClassCompleted);
+    clearButton.addEventListener('click', removeTasksList);
+  }
+}
+
+function createTasksList() {
   const createtasks = document.createElement('li');
   createtasks.innerHTML = selectInput.value;
   createtasks.classList.add('tasks-line');
   selectOl.appendChild(createtasks);
   selectInput.value = '';
-  paintColorList();
-  doubleClick();
-});
+  selectLineList = document.querySelectorAll('.tasks-line');
+  generatorMain();
+}
+
+selectButton.addEventListener('click', createTasksList);
