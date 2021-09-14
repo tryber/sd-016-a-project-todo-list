2aae37a6441194d1ae6a6ceda6b74fa7ccd72061
LeleC0 <maxmullermiranda@gmail.com>
2021-09-14 18:55:28 -0300

    ESLint Done

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function addlist2(event) {
 }
 
 function addlist(event) {
-  let cont = count();
+  const cont = count();
   for (let i = 0; i < cont.length; i += 1) {
     cont[i].classList.remove('slista');
   }
@@ -33,13 +33,13 @@ function addlist(event) {
 function reloadlist() {
   const cont11 = document.querySelectorAll('li');
   for (let i = 0; i < cont11.length; i += 1) {
-    cont11[i].addEventListener('click', function (e) {
-    e.preventDefault();
-    addlist(e);
+    cont11[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      addlist(e);
     });
-    cont11[i].addEventListener('dblclick', function (e) {
-    e.preventDefault();
-    addlist2(e);
+    cont11[i].addEventListener('dblclick', (e) => {
+      e.preventDefault();
+      addlist2(e);
     });
   }
 }
@@ -60,68 +60,68 @@ function addassign() {
   assign.className = 'lista';
   valueassign.value = null;
   document.querySelector('#lista-tarefas').appendChild(assign);
-  assign.addEventListener('click', function (e) {
+  assign.addEventListener('click', (e) => {
     e.preventDefault();
     addlist(e);
   });
-  assign.addEventListener('dblclick', function (e) {
+  assign.addEventListener('dblclick', (e) => {
     e.preventDefault();
     addlist2(e);
   });
 }
 
 function funcremove() {
-  let cont1 = count();
+  const cont1 = count();
   for (let i = 0; i < cont1.length; i += 1) {
     if (cont1[i].classList.contains('slista')) {
       cont1[i].remove(i);
-    };
+    }
   }
 }
 
 function funcmoveup() {
-  let cont2 = count();
+  const cont2 = count();
   for (let i = 1; i < cont2.length; i += 1) {
     if (cont2[i].classList.contains('slista')) {
       cont2[i].classList.remove('slista');
-      cont2[i-1].classList.add('slista');
-      let contx = cont2[i].innerHTML;
-      cont2[i].innerHTML = cont2[i-1].innerHTML;
-      cont2[i-1].innerHTML = contx;
-    };
+      cont2[i - 1].classList.add('slista');
+      const contx = cont2[i].innerHTML;
+      cont2[i].innerHTML = cont2[i - 1].innerHTML;
+      cont2[i - 1].innerHTML = contx;
+    }
   }
 }
 
 function funcmovedown() {
-  let cont3 = count();
-  for (let i = 0; i < cont3.length-1; i += 1) {
+  const cont3 = count();
+  for (let i = 0; i < cont3.length - 1; i += 1) {
     if (cont3[i].classList.contains('slista')) {
       cont3[i].classList.remove('slista');
-      cont3[i+1].classList.add('slista');
-      let contx = cont3[i].innerHTML;
-      cont3[i].innerHTML = cont3[i+1].innerHTML;
-      cont3[i+1].innerHTML = contx;
-      return
-    };
+      cont3[i + 1].classList.add('slista');
+      const contx = cont3[i].innerHTML;
+      cont3[i].innerHTML = cont3[i + 1].innerHTML;
+      cont3[i + 1].innerHTML = contx;
+      return;
+    }
   }
 }
 
 function funcdeleteall() {
-  let cont3 = count();
+  const cont3 = count();
   for (let i = 0; i < cont3.length; i += 1) {
     if (cont3[i].classList.contains('lista')) {
       cont3[i].remove(i);
-    };
+    }
   }
   localStorage.clear();
 }
 
 function funcdeletedone() {
-  let cont3 = count();
+  const cont3 = count();
   for (let i = 0; i < cont3.length; i += 1) {
     if (cont3[i].classList.contains('completed')) {
       cont3[i].remove(i);
-    };
+    }
   }
 }
 
@@ -130,37 +130,37 @@ function funcbackupassign() {
   localStorage.setItem('lista', listassign.innerHTML);
 }
 
-remove.addEventListener('click', function (e) {
+remove.addEventListener('click', (e) => {
   e.preventDefault();
   funcremove();
 });
 
-moveup.addEventListener('click', function (e) {
+moveup.addEventListener('click', (e) => {
   e.preventDefault();
   funcmoveup();
 });
 
-movedown.addEventListener('click', function (e) {
+movedown.addEventListener('click', (e) => {
   e.preventDefault();
   funcmovedown();
 });
 
-deleteall.addEventListener('click', function (e) {
+deleteall.addEventListener('click', (e) => {
   e.preventDefault();
   funcdeleteall();
 });
 
-deletedone.addEventListener('click', function (e) {
+deletedone.addEventListener('click', (e) => {
   e.preventDefault();
   funcdeletedone();
 });
 
-backupassign.addEventListener('click', function (e) {
+backupassign.addEventListener('click', (e) => {
   e.preventDefault();
   funcbackupassign();
 });
 
-newassign.addEventListener('click', function (e) {
+newassign.addEventListener('click', (e) => {
   e.preventDefault();
   addassign();
 });
