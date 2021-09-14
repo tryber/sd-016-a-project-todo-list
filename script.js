ab5f87a23d3cf8dccb763c4a136772f07fd5531d
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-13 22:50:36 -0300

    adiciona estilo a pagina

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ function createItenList() {
   input.value = '';
 }
 buttonAdd.onclick = createItenList;
-let lis = document.getElementsByTagName('li');
+const lis = document.getElementsByTagName('li');
 function selectItem(event) {
   for (let i = 0; i < lis.length; i += 1) {
     lis[i].classList.remove('selected');
   }
-  event.target.classList.add('selected')
+  event.target.classList.add('selected');
 }
 list.addEventListener('click', selectItem);
 
 function completeItem(event) {
-  event.target.classList.add('completed')
+  event.target.classList.add('completed');
 }
 list.addEventListener('dblclick', completeItem);
 
