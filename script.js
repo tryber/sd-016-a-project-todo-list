6c9d82a4d6fb82fdbd05d4fdff7d2a039bd2669b
Leandro Leao <leandro.leao@outlook.com>
2021-09-14 17:02:40 -0300

    Envio do item 7.

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-let listButton = document.getElementById('lista-tarefas');
-let inputText = document.getElementById('texto-tarefa');
 let button = document.createElement('button');
+let parentButton = document.getElementById('lista-tarefas');
+let inputText = document.getElementById('texto-tarefa');
 
-function createbutton(criarTarefa) {
+function createbutton() {
   button.id = 'criar-tarefa';
-  button.innerText = 'Criar Tarefa'
+  button.innerText = 'Incluir Tarefa'
 
-  listButton.appendChild(button);
+  parentButton.appendChild(button);
 }
 
-createbutton()
+createbutton();
 
+
+function colorList() {
+  let listColor = document.getElementsByClassName('item-list')
+  for (let index = 0; index < listColor.length; index += 1) {
+  listColor[index].addEventListener('click', function() {
+        if (listColor[index].style.background === 'white') {
+          listColor[index].style.background = 'rgb(128, 128, 128)';
+        } else {
+          listColor[index].style.background = 'white';
+        }
+      })
+    }
+    console.log(listColor);
+  }
+
+
+function clickButton() {
 button.addEventListener('click', function() {
   let taskList = document.createElement('li');
+  taskList.className = 'item-list';
   taskList.innerText = inputText.value;
-  listButton.appendChild(taskList);
+  parentButton.appendChild(taskList);
   inputText.value = '';
+  colorList()
   }
-)
+  )}
+
+clickButton();
+
+  
+
+
+
+
+
+
+
