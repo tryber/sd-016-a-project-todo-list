7c173a803ac99ab7936c836c15fa71d303449ae6
Ryogazz <josuex.x.x@hotmail.com>
2021-09-13 23:24:01 -0300

    adicionado funçao de completar tarefa

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 
 
-function mudarCor(event){
+function mudarCor(event) {
     const todos = document.querySelectorAll(".tarefa");
-    for(let index = 0; index < todos.length; index ++){
+    for (let index = 0; index < todos.length; index++) {
         todos[index].classList.remove("cor");
     }
-    
+
     const escolhido = " cor";
     event.target.className += escolhido;
 }
 
-function criarLista(){
+function completado(event) {
+    var todos = document.querySelectorAll(".tarefa");
+
+    if (event.target.classList.contains("completed")) {
+        event.target.classList.remove("completed");
+    } else {
+        event.target.classList.add("completed")
+    }
+    
+
+}
+
+
+
+function criarLista() {
     let pai = document.querySelector("#lista-tarefas");
     let entrada = document.querySelector("#texto-tarefa");
-    
+
 
 
     const itemLista = document.createElement("li");
@@ -22,18 +36,24 @@ function criarLista(){
     pai.appendChild(itemLista);
     entrada.value = "";
 
-    const selecionar =  document.querySelectorAll(".tarefa");
-    
-    for(index = 0; index < selecionar.length; index++){
+    const selecionar = document.querySelectorAll(".tarefa");
+
+    for (index = 0; index < selecionar.length; index++) {
         selecionar[index].addEventListener("click", mudarCor);
     }
+
+    for (index = 0; index < selecionar.length; index++) {
+        selecionar[index].addEventListener("dblclick", completado, false);
+    }
+
+
 
 }
 
 
 
 
-function eventos(){
+function eventos() {
     const botao = document.querySelector("#criar-tarefa");
 
     botao.addEventListener("click", criarLista);
