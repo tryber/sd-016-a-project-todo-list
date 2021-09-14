3fc4099e57c203c56c9b8144957f3e5d3c92693d
obastosleandro <bastosleandrodesigner@gmail.com>
2021-09-14 13:03:14 -0300

    selecionando o iten da lista com a cor de fundo pedida na 7 e com a 8 junto

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const botao = document.querySelector("#criar-tarefa");
 botao.addEventListener("click", inputBotao);
+const listItem = document.querySelector("#lista-tarefas")
+
+function selecionaBGColor(event) {
+    let linha = document.querySelectorAll("li");
+    for(let index = 0; index < linha.length; index +=1) {
+        linha[index].style.backgroundColor = "white";
+    }
+    event.target.style.backgroundColor = "rgb(128, 128, 128)"
+}
+
+listItem.addEventListener("click", selecionaBGColor)
 
 function inputBotao() {
     let inputValor = document.querySelector("#texto-tarefa");
     let valor = inputValor.value;
-    let itemLista = document.createElement("li");
+    let itemLista = document.createElement("li")
     itemLista.innerText = valor;
     document.querySelector("#lista-tarefas").appendChild(itemLista);
     inputValor.value = "";
