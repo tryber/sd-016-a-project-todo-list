6f44305139e4134a1d8713ba338ad9fec38da8a9
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-14 14:36:56 -0300

    Requisito 12: Salva arquivos no localStorage e apaga eles do storage quando apaga todas as tarefas

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
 let items = [];
+const clearButton = document.getElementById('apaga-tudo');
+const clearTaskButton = document.getElementById('remover-finalizados');
+const saveButton = document.getElementById('salvar-tarefas');
+
+function saveTasks() {
+  const itemArray = [];
+  items.forEach((item) => {
+    itemArray.push(item.innerText);
+  });
+  localStorage.setItem('tasks', JSON.stringify(itemArray));
+}
+
+window.onload = () => {
+  // source: https://stackoverflow.com/questions/22991871/localstorage-save-array/22992002
+  // usei para aprender a transformar em objeto JSON e depois recuperá-lo
+  if (localStorage.getItem('tasks')) {
+    const storage = JSON.parse(localStorage.getItem('tasks'));
+    storage.forEach((task) => {
+      const liTask = document.createElement('li');
+      liTask.innerText = task;
+      lista.appendChild(liTask);
+      liTask.classList.add('items');
+      items = document.querySelectorAll('.items');
+    });
+  }
+};
 
 function changeBackground(item) {
   items.forEach((element) => {
@@ -35,3 +61,21 @@ button.addEventListener('click', () => {
     item.addEventListener('dblclick', lineThrough);
   });
 });
+
+clearButton.addEventListener('click', () => {
+  items.forEach((item) => {
+    item.remove();
+  });
+  localStorage.clear();
+});
+
+clearTaskButton.addEventListener('click', () => {
+  items.forEach((item) => {
+    if (item.classList.contains('completed')) {
+      item.remove();
+      saveTasks();
+    }
+  });
+});
+
+saveButton.addEventListener('click', saveTasks);
