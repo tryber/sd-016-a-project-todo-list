e22eec416950ebd0623a4cb902f3ceb17e0a304d
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2021-09-14 17:34:34 -0300

    Requisito 8 finalizado.

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 var listItemCounter = 0;        
 var list = document.getElementById('lista-tarefas');
-let color = 'rgb(128,128,128)';
 let entry = document.createElement('li');
-// const li = document.getElementsByClassName('li');
-function changeText2() {
 
-    
+function changeText2() {   
     var input = document.getElementById('texto-tarefa').value;
     entry = document.createElement('li');
     entry.id='list-item'+listItemCounter++;
     entry.appendChild(document.createTextNode(input));
     list.appendChild(entry);
     document.getElementById('texto-tarefa').value = "" ;
-    console.log(entry);
+  
+    addBackgroundColor();
 }
 
+let selecionado = document.getElementById("lista-tarefas");
 
 function addBackgroundColor () {
-    // entry.style.backgroundColor = color;
-    // console.log(entry);
-    // const change = list.getElementById('li');
-    list.addEventListener('click', function() {
-    list.querySelector('li').style.backgroundColor = 'rgb(128,128,128)';
-    console.log(list.lastElementChild)
-   })       
-}
-addBackgroundColor();
+    
+    for (let i = 0; i < list.childNodes.length; i += 1) {
+        list.childNodes[i].addEventListener('click', function (e) {
+            selecionado.classList.remove('color');
+            list.childNodes[i].className = 'color';
+            selecionado = list.childNodes[i]
+        });
+} }
 
-// entry.addEventListener('click', addBackgroundColor);
-
-// for (let i = 0; i < list.length; i += 1) {
-//     list[i].addEventListener('click', function (e) {
-//         list[i].style.backgroundColor = color;
-//         // entry[i].style.backgroundColor = "red";
-//         console.log(list);
-//     });
-// }
 
 
 
