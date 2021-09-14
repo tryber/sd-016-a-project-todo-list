18f5bab6be920c53cd003cee77debaf22d11df9c
Leonardo_Nunes <leonardokarate95@hotmail.com>
2021-09-14 15:31:47 -0300

    adiciona botão apaga tudo

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 let button = document.getElementsByTagName("button")[0];
 let input = document.getElementById("texto-tarefa");
-let tabela = document.getElementById("lista-tarefas")
+let tabela = document.getElementById("lista-tarefas");
+let apagar = document.getElementById("apaga-tudo");
+let selected = null;
 console.log(button);
-button.addEventListener("click",adicionaItem);
-function adicionaItem(event){
-let texto = input.value;
-let listaDeTarefas = document.createElement("li");
-listaDeTarefas.innerText = texto;
-tabela.appendChild(listaDeTarefas);
-input.value ="";
+button.addEventListener("click", adicionaItem);
+apagar.addEventListener("dblclick", apagarLista);
+apagar.addEventListener("click", apagarLista);
+function adicionaItem(event) {
+  let texto = input.value;
+  let listaDeTarefas = document.createElement("li");
+  listaDeTarefas.innerText = texto;
+  listaDeTarefas.className = "lista";
+  tabela.appendChild(listaDeTarefas);
+  listaDeTarefas.addEventListener("click", () => {
+    if (selected) {
+      listaDeTarefas.style.backgroundColor = 'rgb(0, 0, 0)';
+      selected = null;
+    }
+    else {
+      listaDeTarefas.style.backgroundColor = 'rgb(128, 128, 128)'
+    selected = true;
+    }
+  })
+  input.value = "";
+}
+function apagarLista(event) {
+  let listaDeTarefas = document.querySelectorAll(".lista")
+  for (let i = 0; i < listaDeTarefas.length; i += 1) {
+    let element = listaDeTarefas[i];
+    tabela.removeChild(element);
+  }
 }
