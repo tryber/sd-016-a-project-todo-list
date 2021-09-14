29e82fe1afdef7461bbf0cafd57aebb214df6067
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-14 18:49:54 -0300

    adiciona desafios 10 e 14

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const list = document.getElementById('lista-tarefas');
 const buttonAdd = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
+const finalizados = document.getElementsByClassName('completed');
+const lis = document.getElementsByTagName('li');
 
+//  cria itens de lista
 function createItenList() {
   const listItem = document.createElement('li');
   listItem.innerText = input.value;
@@ -9,7 +12,8 @@ function createItenList() {
   input.value = '';
 }
 buttonAdd.onclick = createItenList;
-const lis = document.getElementsByTagName('li');
+
+// seleciona itens de lista
 function selectItem(event) {
   for (let i = 0; i < lis.length; i += 1) {
     lis[i].classList.remove('selected');
@@ -18,12 +22,28 @@ function selectItem(event) {
 }
 list.addEventListener('click', selectItem);
 
+// marca como item de lista completado
 function completeItem(event) {
   event.target.classList.add('completed');
 }
 list.addEventListener('dblclick', completeItem);
 
+//  deleta todos itens de lista 
 function deleteAllLi() {
   list.innerHTML = '';
 }
 document.getElementById('apaga-tudo').onclick = deleteAllLi;
+
+//  remove itens finalizados
+function deletaFinalizados() {
+  for (let i = 0; i < 1000; i -= 1) {
+    finalizados[0].remove()
+  }
+}
+document.getElementById('remover-finalizados').onclick = deletaFinalizados;
+
+//  remove item selecionado
+document.getElementById('remover-selecionado').addEventListener('click', function(){
+  const selecionado = document.getElementsByClassName('selected')[0];
+  selecionado.remove(); 
+});
