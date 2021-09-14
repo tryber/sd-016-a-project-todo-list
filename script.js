8930a10a04296a1fc5e57685b2c9cfebedef95ac
Igor <iigorfelipe@gmail.com>
2021-09-14 15:35:17 -0300

    finaliza requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const orderedList = document.querySelector('#lista-tarefas');
 const text = document.querySelector('#texto-tarefa');
-let adicionar = true;
 
 function createList() {
   let valueOfText = text.value;
   let li = document.createElement('li');
+  li.className = 'list';
   li.innerText = valueOfText;
   orderedList.appendChild(li);
   text.value = '';
 }
 
-
+function changeColor(e) {
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+orderedList.addEventListener('click', changeColor);
