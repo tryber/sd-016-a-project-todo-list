fe8e644981d25ddf8d3d198839379a09ea77d2b0
Christian Lessa <christianclessa@gmail.com>
2021-09-14 17:36:30 -0300

    estilizando

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const buttonCreatTarefa = document.querySelector('#criar-tarefa');
 const listOrder = document.querySelector('#lista-tarefas');
 
-buttonCreatTarefa.addEventListener('click', inputButton);
+buttonCreatTarefa.addEventListener('click', inputOfButton);
 
-function inputButton() {
+function inputOfButton() {
   const inputButton = document.querySelector('#texto-tarefa');
   const creatList = document.createElement('li');
   const addText = inputButton.value;
-  creatList.innerText = addText
+  creatList.innerText = addText;
   listOrder.appendChild(creatList);
-  inputButton.value = "";
+  inputButton.value = '';
 }
 listOrder.addEventListener('click', selectBackgroundColor); 
 
-function selectBackgroundColor (event) {
+function selectBackgroundColor(event) {
 const selectLi = document.querySelectorAll('li');
   for (let index = 0; index < selectLi.length; index += 1) {
     selectLi[index].style.backgroundColor = 'white';
   }  
-event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+event.target.style.backgroundColor = 'grey';
+// event.target.classList.add('move');
 }
 listOrder.addEventListener('dblclick', textRiscado);
 
@@ -29,20 +30,34 @@ event.target.classList.toggle('completed');
 const buttonClearList = document.getElementById('apaga-tudo');
 buttonClearList.addEventListener('click', removeItemList);
 
-function removeItemList () {
+function removeItemList() {
 const selectLi = document.querySelectorAll('li');
-  for (let index = 0; index < selectLi.length; index += 1) {
-  const list = selectLi[index];
-  listOrder.removeChild(list);
-  }
+ for (let index = 0; index < selectLi.length; index += 1) {
+ const list = selectLi[index];
+ listOrder.removeChild(list);
+ }
 }
 const buttonClearListRiscados = document.querySelector('#remover-finalizados');
 buttonClearListRiscados.addEventListener('click', clearListRiscados);
 
 function clearListRiscados () {
  const listRiscado = document.querySelectorAll('.completed');
- for (let index = 0; index < listRiscado.length; index += 1) {
+  for (let index = 0; index < listRiscado.length; index += 1) {
   const list = listRiscado[index];
   listOrder.removeChild(list);
   }
 }
+
+// const selectLi = document.querySelectorAll('li');
+// const classMove = document.querySelector('.move');
+// const buttonDown = document.getElementById('mover-baixo');
+// const buttonUp = document.getElementById('mover-cima');
+// buttonDown.addEventListener('click', buttonDownList);
+// buttonUp.addEventListener('click', buttonUpList);
+
+// function buttonDownList () {
+//   for (let index = 0; index < selectLi.length; index += 1) {
+//     const element = classMove[index];
+//     element = element + 1;
+//   }
+// }  
