6169ca2a88a3eaf1ec0ca779c4d242907cd39818
douglas <douglasfmr1@gmail.com>
2021-09-13 21:16:49 -0300

    exercicios ateh o 10

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 let button = document.querySelector('#criar-tarefa');
 let input = document.querySelector('#texto-tarefa');
 let list = document.querySelector('#lista-tarefas');
+let clearAll = document.querySelector('#apaga-tudo');
 
 
 
-
-
-
-
-  button.addEventListener('click', function() {
-    let createLi = document.createElement('li');
-    createLi.innerText = input.value;   
-    list.appendChild(createLi);
-    input.value = '';
+button.addEventListener('click', function () {
+  let createLi = document.createElement('li');
+  createLi.innerText = input.value;
+  createLi.className = 'li-lista'
+  list.appendChild(createLi);
+  input.value = '';
+  createLi.addEventListener('click', addGrey)
+  createLi.addEventListener('dblclick', addRisk)
 })
 
+function addGrey(event) {
+  let selectList = document.querySelector('.graycolor');
+  if (selectList != null) {
+    selectList.classList.remove('graycolor');
+  }
+  event.target.classList.add('graycolor');
+}
 
+function Clear() {
+  while (list.firstElementChild) {
+    list.removeChild(list.firstElementChild);
+  }
+}
+
+clearAll.addEventListener('click', function () {
+  while (list.firstElementChild) {
+    list.removeChild(list.firstElementChild);
+  }
+})
+
+function addRisk(event) {
+  event.target.classList.toggle('completed');
+}
