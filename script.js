019d3f646df50538a2af0d35ce2e639da98e0831
Angelica <angelicapedroso05@gmail.com>
2021-09-13 23:53:59 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const list = document.getElementById('lista-tarefas');
 const input = document.getElementById('texto-tarefa');
 button.addEventListener('click', addTask);
 
-function addTask(selection) {
-  if (selection.target === button && input.value !== '') {
-    const task = document.createElement('li');
-
-    list.appendChild(task).innerHTML = input.value;
-    input.value = '';
-  }
+function addTask() {
+  const task = document.createElement('li');
+  task.innerHTML = input.value;
+  list.appendChild(task);
+  input.value = '';
 }
+
 
 
