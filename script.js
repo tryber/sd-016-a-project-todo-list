d77acf23804ea44daef82c207c6bf82d53cbcf01
João Victor <joao.lenny100@gmail.com>
2021-09-13 22:54:13 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ function addLi(selection) {
   const input = document.querySelector('#texto-tarefa');
   const buttonAdd = document.querySelector('#criar-tarefa');
 
-  if (input.value !== '') {
-    if (selection.target === buttonAdd) {
-      const li = document.createElement('li');
-      ol.appendChild(li).innerHTML = input.value;
-      input.value = '';
-    }
+  if (selection.target === buttonAdd && input.value !== '') {
+    const li = document.createElement('li');
+
+    ol.appendChild(li).innerHTML = input.value;
+    input.value = '';
   }
 
   return ol;
 }
 
+function changeColorItem(selection) {
+  const li = document.querySelector('li');
+
+  if (selection.target === li) {
+    li.style.backgroundColor = 'rgb(128,128,128)';
+  }
+}
+
 document.addEventListener('click', addLi);
+document.addEventListener('click', changeColorItem);
