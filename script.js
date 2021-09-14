0e89d7fc2fd267c0a0732d57e9c1a54ed68cb70f
Danilo Meneguela <meneguelaprog@hotmail.com>
2021-09-14 14:36:08 -0300

    Requisito 8 Completo

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 const addButton = document.getElementById('criar-tarefa');
 const allList = document.getElementById('lista-tarefas');
 const task = document.getElementById('texto-tarefa');
-let taskList = document.getElementsByClassName('task-list');
+const taskList = document.getElementsByClassName('task-list');
 
 addButton.addEventListener('click', () => {
   const newList = document.createElement('li');
   newList.innerHTML = task.value;
   newList.classList = 'task-list';
-  // newList.style.backgroundColor = 'white';
+  newList.style.backgroundColor = 'white';
   allList.appendChild(newList);
   task.value = '';
   console.log(taskList);
   for (let index = 0; index < taskList.length; index += 1) {
     taskList[index].addEventListener('click', (event) => {
+      const paintLi = event.target;
       console.log(taskList);
-      taskList[index].style.backgroundColor = 'white';
-      event.target.style.backgroundColor = 'rgb(128,128,128)';
-    });  
+      for (let index2 = 0; index2 < taskList.length; index2 += 1){
+        taskList[index2].style.backgroundColor = 'white';
+      }
+      paintLi.style.backgroundColor = 'rgb(128,128,128)';
+    });
   }
 });
-// for (let index = 0; index < taskList.length; index += 1) {
-//   taskList.addEventListener('click', (event) => {
-//     const taskList = document.getElementsByClassName('task-list');
-//     if (taskList[index].style.backgroundColor == 'white'){
-//       event.target.style.backgroundColor = 'rgb(128,128,128)';
-//     } else {
-//       event.target.style.backgroundColor = 'white';
-//     }
-//   });  
-// }
 
-// for (let index = 0; index < taskList.length; index += 1) {
-//   taskList[index].addEventListener('click', (event) => {
-//     console.log(taskList);
-//     taskList[index].style.backgroundColor = 'white';
-//     event.target.style.backgroundColor = 'rgb(128,128,128)';
-//   });  
-// }
