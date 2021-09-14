4d12786ccbd8d945170a5a1ac398d80b5f7b4329
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-14 12:02:34 -0300

    REquisito 8

--- script.js
+++ script.js
@@ -3,23 +3,31 @@ const getInput = document.querySelector('input');
 const getBut = document.querySelector('button');
 const assignment = [];
 
+function pintaTudo (){
+  const cList = document.querySelectorAll('li');
+  for (let index = 0; index < cList.length; index += 1) {
+    const listPost = cList[index];
+    listPost.style.backgroundColor = 'unset';
+  }
+}
+
+function troca(event) {
+      pintaTudo();
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+
 function showTask() {
 
   getBut.addEventListener('click', function () {
     const list = document.createElement('li');
+    list.addEventListener('click', troca);
+    //list.style.backgroundColor = 'red';
     list.innerText = getInput.value;
     getLista.appendChild(list);
     getInput.value = '';
-  });  
-}
-showTask();
-
-function newColor (){
-  getLista.addEventListener('click', function (event) {
-    event.target.style.backgroundColor = "rgb(128, 128, 128)";
   });
 }
-newColor();
+showTask()
 
 /*function clenList (){
 
