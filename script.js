cbfd4575aed3568a92c5b9b4a805f60a1b2dc0a0
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-09-13 23:16:01 -0300

    adicionado requisito 13 e corrigido lint issues

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function addNewTask() {
   const addButton = document.querySelector('#criar-tarefa');
   const inputField = document.querySelector('#texto-tarefa');
 
-  addButton.addEventListener('click', function () {
+  addButton.addEventListener('click', () => {
     if (inputField.value.length > 0) {
       const addLi = document.createElement('li');
       addLi.innerText = inputField.value;
@@ -80,18 +80,20 @@ function deleteTaskBkgrdColor() {
 }
 
 function changeTaskBkgrdColor() {
-  tasklist.addEventListener('click', function (clicked) {
+  tasklist.addEventListener('click', (clicked) => {
     deleteTaskBkgrdColor();
     const event = clicked.target;
-    event.id = 'selected';
-    const selected = document.querySelector('#selected');
-    selected.style.backgroundColor = 'rgb(128, 128, 128)';
+    if (event.className === 'task') {
+      event.id = 'selected';
+      const selected = document.querySelector('#selected');
+      selected.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
   });
 }
 changeTaskBkgrdColor();
 
 function completeTask() {
-  tasklist.addEventListener('dblclick', function (clicked) {
+  tasklist.addEventListener('dblclick', (clicked) => {
     const event = clicked.target;
     if (event.className !== 'task completed') {
       event.className = 'task completed';
@@ -117,44 +119,44 @@ function addActionButtons() {
 }
 addActionButtons();
 
-function eraseAllButton() {
+function eraseAllBttn() {
   const eraseAll = document.querySelector('#apaga-tudo');
-  eraseAll.addEventListener('click', function () {
-    const tasks = document.getElementsByClassName('task');
+  eraseAll.addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
     for (let index = tasks.length - 1; index >= 0; index -= 1) {
       tasks[index].remove();
     }
   });
 }
-eraseAllButton();
+eraseAllBttn();
 
-function eraseFinishedButton() {
+function eraseFinishedBttn() {
   const eraseFinished = document.querySelector('#remover-finalizados');
-  eraseFinished.addEventListener('click', function () {
+  eraseFinished.addEventListener('click', () => {
     const tasksCompleted = document.getElementsByClassName('completed');
     for (let index = tasksCompleted.length - 1; index >= 0; index -= 1) {
       tasksCompleted[index].remove();
     }
   });
 }
-eraseFinishedButton();
+eraseFinishedBttn();
 
 const actionButtons = document.querySelector('#action-buttons');
 
-function taskSaveButton() {
+function taskSaveBttn() {
   const taskSaveButton = document.createElement('button');
   taskSaveButton.id = 'salvar-tarefas';
   taskSaveButton.innerText = 'Salvar lista';
   actionButtons.appendChild(taskSaveButton);
 }
-taskSaveButton();
+taskSaveBttn();
 
 function taskSave() {
-  const taskSaveBttn = document.querySelector('#salvar-tarefas');
-  taskSaveBttn.addEventListener('click', function () {
+  const taskSaveButtonQ = document.querySelector('#salvar-tarefas');
+  taskSaveButtonQ.addEventListener('click', () => {
     const currentList = document.querySelector('ol').innerHTML;
     localStorage.setItem('saveList', currentList);
-  })
+  });
 }
 taskSave();
 
@@ -165,3 +167,44 @@ function taskSaveReload() {
 window.onload = function onload() {
   taskSaveReload();
 };
+
+function moveSelectedTaskButton() {
+  const moveUpButton = document.createElement('button');
+  const moveDownButton = document.createElement('button');
+  moveUpButton.id = 'mover-cima';
+  moveUpButton.innerText = 'mover para cima';
+  moveDownButton.id = 'mover-baixo';
+  moveDownButton.innerText = 'mover para baixo';
+  actionButtons.appendChild(moveUpButton);
+  actionButtons.appendChild(moveDownButton);
+}
+moveSelectedTaskButton();
+
+function moveSelectedTaskUp() {
+  const moveUpBttn = document.querySelector('#mover-cima');
+  moveUpBttn.addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
+    for (let index = 0; index < tasks.length; index += 1) {
+      if (tasks[index].id === 'selected' && index !== 0) {
+        tasklist.insertBefore(tasks[index], tasks[index - 1]);
+      }
+    }
+  });
+}
+moveSelectedTaskUp();
+
+function moveSelectedTaskDown() {
+  const moveDownBttn = document.querySelector('#mover-baixo');
+  moveDownBttn.addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
+    for (let index = 0; index < tasks.length; index += 1) {
+      if (tasks[index].id === 'selected' && index !== tasks.length - 1) {
+        tasklist.insertBefore(tasks[index], tasks[index + 2]);
+      }
+    }
+  });
+}
+moveSelectedTaskDown();
+
+/* insertBefore retirado do site http://devfuria.com.br/javascript/dom-insert-before/ */
+/* https://developer.mozilla.org/pt-BR/docs/Web/API/Node/insertBefore */
