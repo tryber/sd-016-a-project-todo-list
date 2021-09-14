821602ff79a5a87da733ef6111daf3d96706073d
Pedro <pedro.oficial1969@gmail.com>
2021-09-14 16:07:16 -0300

    adiciona requsito 9

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
-const li = document.createElement('li');
 
 function liStyle() {
-  ol.addEventListener("click", function (event) {
-    for (index = 0; index < ol.children.length; index += 1) {
-      ol.children[index].style.background = 'white';
+  ol.addEventListener("click", (event) => {
+    for (let index = 0; index < ol.children.length; index += 1) {
+      ol.children[index].style.background = 'rgb(12 , 93 , 168)';
     };
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   });
-};
+}
 liStyle();
 
 function buttonValue() {
@@ -19,5 +18,12 @@ function buttonValue() {
   li.addEventListener("click", liStyle);
   ol.appendChild(li);
   input.value = '';
-};
-button.addEventListener('click', buttonValue)
+}
+button.addEventListener('click', buttonValue);
+
+function completed() {
+  ol.addEventListener("dblclick", (event) => {
+    event.target.classList.toggle('completed');
+  })
+}
+completed()
