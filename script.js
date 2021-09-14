7649dcc8a1ed04597ecf92d7e71136171bb61c0f
Fabiana Lopes Santana <fabianalopes93@outlook.com>
2021-09-14 10:50:12 -0300

    requisito 7

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ addInput.value = '';
 
    
     workButton.addEventListener('click', adicionarItem);
+//requisito 5 e 6, acima, feito com o auxilio do Leandro, Bruno, Cristiane, Priscila, Pedro e Brunão - T 16 / A
 
+function corDeFundo () {
+createList.addEventListener('click', function(event) {
+event.target.style.background = "rgb(128,128,128)";
+});
+}
 
+corDeFundo();
 
