c5b8a4a2525f355ad2b703c9e0362e134a64fbc5
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-13 21:29:42 -0300

    requisitos ate o remover tudo

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function createTarefa() {
     // zerar o valor do input para a proxima tarefa
     inputValue.value = ''
     eventColorLi();
+    eventCompletedTarefa() 
 }
 
 function eventCreateTarefa() {
@@ -19,8 +20,6 @@ function eventCreateTarefa() {
     createButton.addEventListener('click', createTarefa)
 }
 eventCreateTarefa();
-
-/////////////////////////////
 
 function colorLi(event) {
     let li = document.querySelector('#lista-tarefas').children
@@ -38,7 +37,26 @@ function eventColorLi() {
     }
 }
 
-///////////////////////////
+function completedTarefa(event) {
+    let li = document.querySelector('#lista-tarefas').children
+    for (let index = 0; index < li.length; index += 1) {
+        if (li[index].className === 'completed') {
+            li[index].classList.remove('completed')    
+        }
+        else {
+            event.target.classList.add('completed')
+        }        
+    }    
+}
+
+function eventCompletedTarefa() {
+    let li = document.querySelector('#lista-tarefas').children;
+    for (let index = 0; index < li.length; index += 1) {
+        li[index].addEventListener('dblclick', completedTarefa)
+    }
+}
+
+//////////////////////////
 
 function removeAllTarefas() {
     let tarefaList = document.querySelector('#lista-tarefas').children;
@@ -52,45 +70,3 @@ function removeAllButton() {
     removeButton.addEventListener('click', removeAllTarefas)
 }
 removeAllButton()
-
-////////////////////////
-
-function completedTarefa(event) {
-    // as tarefas sao as li filhas de ul
-    let li = document.querySelector('#lista-tarefas').children;
-    for (let index = 0; index < li.length; index += 1) {
-        event.target.classList.add('completed')
-    }
-}
-
-function removeCompletedTarefa() {
-    let li = document.querySelector('#lista-tarefas').children;
-    for (let index = 0; index < li.length; index += 1) {
-        if (li[index].className === 'completed') {
-        li[index].classList.remove('completed')    
-        }
-    }
-    eventCompletedTarefa() 
-}
-
-function eventCompletedTarefa() {
-    let li = document.querySelector('#lista-tarefas').children;
-    for (let index = 0; index < li.length; index += 1) {
-        li[index].addEventListener('dblclick', completedTarefa)
-    }
-}
-eventCompletedTarefa() 
-
-function eventRemoveCompletedTarefa() {
-    let li = document.querySelector('#lista-tarefas').children;
-    for (let index = 0; index < li.length; index += 1) {
-        li[index].addEventListener('dblclick', removeCompletedTarefa)
-    }
-}
-eventRemoveCompletedTarefa();
-
-// teste que dá classe selected para apenas 1 li
-// cria todos os Li
-// todos sem classe e com background color igual
-// cria função que add uma determinada classe ao Li[i] selecionado
-// ao li selecionado aplicar a mudança de color
