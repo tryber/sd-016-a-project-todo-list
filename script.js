51e60619fc9ac10facf05c5ae6e28585247886df
morkertt <andremorkertt@hotmail.com>
2021-09-14 16:02:20 -0300

    [ADD] 1 - 11 finalizado

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const btn = document.getElementById('criar-tarefa');
 const tarefaTxt = document.getElementById('texto-tarefa');
 const tarefaList = document.getElementById('lista-tarefas');
 const allList = document.getElementsByTagName('li');
+const delAll = document.getElementById('apaga-tudo');
+const removeDone = document.getElementById('remover-finalizados');
+const saveTarefas = document.getElementById('salvar-tarefas');
+const moveUp = document.getElementById('mover-cima');
+const moveDown = document.getElementById('mover-baixo');
+const removSelected = document.getElementById('remover-selecionado');
 
 const tempList = [];
 let i = 0;
@@ -24,9 +30,47 @@ function list() {
   i += 1;
 }
 
+delAll.addEventListener('click', () => {
+  while (tarefaList.firstChild) {
+    tarefaList.removeChild(tarefaList.firstChild);
+  }
+  tempList.splice(0, tempList.length);
+  i = 0;
+});
+
+removeDone.addEventListener('click', () => {
+  const li = document.getElementsByTagName('li');
+  for (i = 0; i < li.length; i += 1) {
+    if (li[i].className === 'completed') {
+      tarefaList.removeChild(li[i]);
+    }
+    if (li[i].className === 'highlight completed') {
+      tarefaList.removeChild(li[i]);
+    }
+  }
+});
+
+saveTarefas.addEventListener('click', () => {
+
+});
+
+moveUp.addEventListener('click', () => {
+
+});
+
+moveDown.addEventListener('click', () => {
+
+});
+
+removSelected.addEventListener('click', () => {
+
+});
+
 btn.addEventListener('click', () => {
-  tempList.push(tarefaTxt.value);
-  console.log(tempList);
-  list();
-  tarefaTxt.value = '';
+  if (tarefaTxt.value !== '') {
+    tempList.push(tarefaTxt.value);
+    console.log(tempList);
+    list();
+    tarefaTxt.value = '';
+  }
 });
