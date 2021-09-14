1935a09d8ed9f17408d2bc7465dfb113efce7e07
Christian Lessa <christianclessa@gmail.com>
2021-09-13 21:08:12 -0300

    Requisito 8 concluido

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ buttonCreatTarefa.addEventListener('click', inputButton);
 function inputButton () {
   const inputButton = document.querySelector('#texto-tarefa');
   const creatList = document.createElement('li');
-  creatList.addEventListener('click', selectBackgroundColor); 
+  creatList.classList.add('color');
   const addText = inputButton.value;
   creatList.innerText = addText
   listOrder.appendChild(creatList);
   inputButton.value = "";
+}
+listOrder.addEventListener('click', selectBackgroundColor); 
 
-
-  function selectBackgroundColor (event) {
-    creatList.classList.remove('background-color');
-    creatList.classList.add('background-color');
-  }
+function selectBackgroundColor (event) {
+  const selectLi = document.querySelectorAll('li');
+    for (let index = 0; index < selectLi.length; index += 1) {
+      selectLi[index].style.backgroundColor = 'white';
+    }  
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
