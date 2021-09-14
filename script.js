58653d75e2fd77900aa9761d645cb94d8b61d403
Pedro <pedro.oficial1969@gmail.com>
2021-09-13 22:02:24 -0300

    adiciona atualizaçao

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 
+// requisito 7.
+function style (event) {
+event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function buttonValue() {
   const li = document.createElement('li');
   li.innerText = input.value;
+  li.addEventListener("click", style)
   ol.appendChild(li);
   input.value = '';
 }
-button.addEventListener('click', buttonValue);
+button.addEventListener('click', buttonValue)
