7236ed0ac5775a8a674d2b48bcb1bf0aae4e4840
Tiago Abranges <tiagoabranges@gmail.com>
2021-09-14 15:52:22 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let listaTarefas = document.getElementById("lista-tarefas");
+
 function trazerFilho() {
-    let filho = document.createElement('li');
-    filho.className = 'item-lista';
-    let valor = document.getElementById('texto-tarefa');
-    filho.innerText = valor.value;
-    listaTarefas.appendChild(filho);
-    valor.value = '';
-    
+  let filho = document.createElement('li');
+  filho.className = 'item-lista';
+  let valor = document.getElementById('texto-tarefa');
+  filho.innerText = valor.value;
+  listaTarefas.appendChild(filho);
+  valor.value = '';
 }
 let botao = document.getElementById('criar-tarefa');
-botao.addEventListener('click',trazerFilho);
 
-listaTarefas.addEventListener('click', mudaCor);
+botao.addEventListener('click', trazerFilho);
 
 function mudaCor(event) {
- let click = event.target;
- click.style.backgroundColor = "rgb(128, 128, 128)";
- click.classList.add('selecionar') ;
+  const item = document.querySelector('.selecionar');
+  if (item != null) {
+    item.style.backgroundColor = 'white';
+    item.classList.remove('selecionar');
+  }
+  let click = event.target;
+  click.style.backgroundColor = "rgb(128, 128, 128)";
+  click.classList.add('selecionar');
+
 }
+listaTarefas.addEventListener('click', mudaCor);
+
+
+
