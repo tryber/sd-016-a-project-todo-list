68ba67ac20f661c87df8b7fc243a32547fd9e13f
Pedro <pedro.oficial1969@gmail.com>
2021-09-14 17:07:21 -0300

    adiciona requisito 10

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 
+// requisito 8.
 function liStyle() {
   ol.addEventListener("click", (event) => {
     for (let index = 0; index < ol.children.length; index += 1) {
@@ -12,8 +13,10 @@ function liStyle() {
 }
 liStyle();
 
+// requisito 5
 function buttonValue() {
   const li = document.createElement('li');
+  li.classList.add('clear');
   li.innerText = input.value;
   li.addEventListener("click", liStyle);
   ol.appendChild(li);
@@ -21,9 +24,24 @@ function buttonValue() {
 }
 button.addEventListener('click', buttonValue);
 
+// requisito 9.
 function completed() {
   ol.addEventListener("dblclick", (event) => {
     event.target.classList.toggle('completed');
   })
 }
 completed()
+
+//requisito 10
+function clearAll() {
+  const clear = document.getElementById('apaga-tudo');
+  clear.addEventListener('click', () => {
+    let ol = document.getElementById('lista-tarefas');
+    const value = document.querySelectorAll('.clear');
+    for (let index = 0; index < value.length; index += 1) {
+      let list = value[index];
+      ol.removeChild(list);
+    }
+  })
+}
+clearAll()
