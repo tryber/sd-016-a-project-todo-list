8a812ead28ae20584f0a6e8d754b94d7f7201f20
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-09-14 11:38:33 -0300

    add requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-
 const button = document.querySelector('#criar-tarefa');
-const list = document.querySelector('#lista-tarefas')
+const list = document.querySelector('#lista-tarefas');
+
+function listElementHilight(event) {
+  if (event.target.classList.contains('hilight') === false) {
+    event.target.classList.add('hilight');
+  } else {
+    event.target.classList.remove('hilight');
+  }
+}
 
 function createListElement() {
   const input = document.querySelector('#texto-tarefa');
   const listElement = document.createElement('li');
+  listElement.addEventListener('click', listElementHilight);
   listElement.innerText = input.value;
   list.appendChild(listElement);
   input.value = '';
