263b8e6da5abc8cbccdedaee9429ac58e2f4f8fa
Laura <lahfumagalli@gmail.com>
2021-09-13 21:24:29 -0300

    requisito 9

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let textInput = document.getElementById('texto-tarefa')
 let roadButton = document.querySelector('#criar-tarefa')
 roadButton.innerHTML = 'Adicionar Tarefa'
+
 function addAssign() {
   let ordList = document.getElementById('lista-tarefas');
   let newListItem = document.createElement('li');
@@ -22,74 +23,11 @@ function addLiListenner(event) {
 
 document.querySelector('#lista-tarefas').addEventListener('click', addLiListenner);
 
+// Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item
+// 
 
-// let caminhoDeOndeCliquei = document.querySelectorAll('.pixel');
-// for (let i = 0; i < caminhoDeOndeCliquei.length; i += 1){
-// caminhoDeOndeCliquei[i].addEventListener('click', pintarQuadrado);
-// }
+function riskTask(event) {
+  event.target.classList.toggle('completed'); //se tem a class tira, se nao tem poe
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const input = document.getElementById('texto-tarefa');
-// const button = document.getElementById('criar-tarefa');
-// const taskList = document.getElementById('lista-tarefas');
-
-
-// function criarInput() {
-//   const newList = document.createElement('li');
-//   newList.innerText = input.value;
-//   taskList.appendChild(newList);
-//   input.value = '';
-
-// }
-
-// button.addEventListener('click', criarInput);
-
-// function colorirList(){
-//   const listItem = document.getElementsByTagName('li');
-//   listItem[i].addEventListener('click', (event) => {
-//   event.target.style.backgroundColor = 'rgb (128, 128, 128)';
-// });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function colorLi(event) {
-//   const listLi = document.querySelectorAll('li');
-//   for (let index = 0; index < listLi.length; index += 1) {
-//     const element = listLi[index];
-//     document.querySelector('.selected').style.backgroundColor = 'white';
-//     element.classList.remove('selected');
-//   }
-
-//   event.target.className = 'selected';
-//   document.querySelector('.selected').style.backgroundColor = 'rgb(128, 128, 128)';
-// }
+document.querySelector('#lista-tarefas').addEventListener('dblclick', riskTask)
