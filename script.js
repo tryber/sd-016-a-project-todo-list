fb1c2f1b20a1165cb5c23b1a1b123c7c2b8ef920
Danilo Meneguela <meneguelaprog@hotmail.com>
2021-09-14 15:51:31 -0300

    Requisito 10 Completo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addButton = document.getElementById('criar-tarefa');
 const allList = document.getElementById('lista-tarefas');
 const task = document.getElementById('texto-tarefa');
 const taskList = document.getElementsByClassName('task-list');
+const clearBtn = document.getElementById('apaga-tudo');
 
 addButton.addEventListener('click', () => {
   const newList = document.createElement('li');
@@ -10,16 +11,33 @@ addButton.addEventListener('click', () => {
   newList.style.backgroundColor = 'white';
   allList.appendChild(newList);
   task.value = '';
-  console.log(taskList);
+  
+  // requisito 7 e 8
   for (let index = 0; index < taskList.length; index += 1) {
     taskList[index].addEventListener('click', (event) => {
       const paintLi = event.target;
-      console.log(taskList);
       for (let index2 = 0; index2 < taskList.length; index2 += 1){
         taskList[index2].style.backgroundColor = 'white';
       }
       paintLi.style.backgroundColor = 'rgb(128,128,128)';
     });
   }
+
+  // requisito 9
+  for (let index = 0; index < taskList.length; index += 1) {
+    taskList[index].addEventListener('dblclick' , (event) => {
+      const setComplete = event.target;
+      setComplete.classList.remove('completed');
+      setComplete.classList.add('completed');
+    })
+    
+    //requisito 10
+    clearBtn.addEventListener('click', () => {
+      for (let index = 0; index < taskList.length; index += 1 ){
+        allList.removeChild(taskList[index]);
+      }
+    })
+  }
 });
+
 
