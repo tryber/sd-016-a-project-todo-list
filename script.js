fbdd513c6c32c9530a306b9133ac3367e1d80f03
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-09-14 16:22:34 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const createButton = document.getElementById("criar-tarefa");
 const addList = document.getElementById("lista-tarefas");
 
 function createTask() {
-    const createListElement = document.createElement('li');
-    createListElement.innerText = input.value;
-    addList.appendChild(createListElement)
-    input.value = "";
+  const createListElement = document.createElement('li');
+  createListElement.innerText = input.value;
+  addList.appendChild(createListElement)
+  createListElement.className="list-item";
+  input.value = "";
+  createListElement.addEventListener('click', function(){
+    createListElement.style.backgroundColor="rgb(128,128,128)";
+  })
 }
 
 createButton.addEventListener('click', createTask);
