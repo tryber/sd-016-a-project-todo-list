58df13365a3fa5f2b4113a641d2451114911dc20
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-09-14 11:53:31 -0300

    requisito 10

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function add () {
   const inputAdd = document.querySelector('#texto-tarefa');
   const textInput = inputAdd.value;
   const taskLine = document.createElement('li');
+  taskLine.classList.add('line');
   taskLine.innerHTML = textInput;
   addList.appendChild(taskLine);
   inputAdd.value = '';
-  
 }
 
 addList.addEventListener('click', changeBackground);
@@ -18,13 +18,25 @@ addList.addEventListener('click', changeBackground);
 function changeBackground (event) {
   const selectLi = document.querySelectorAll('li');
   for (let index = 0; index < selectLi.length; index += 1) {
-    selectLi[index].style.backgroundColor = 'white';    
+    selectLi[index].style.backgroundColor = 'white';
   }
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 addList.addEventListener('dblclick', changeTextStyle);
 
 function changeTextStyle (event) {
-  event.target.classList.toggle('completed')
+  event.target.classList.toggle('completed');
+}
+//https://www.w3schools.com/howto/howto_js_toggle_class.asp
+
+const deleteButton = document.querySelector('#apaga-tudo');
+deleteButton.addEventListener('click', deleteAll);
+
+function deleteAll () {
+  const removeList = document.querySelectorAll('.line')
+ for (let index = 0; index < removeList.length; index += 1) {
+   const element = removeList[index];
+   addList.removeChild(element);
+ }
 }
