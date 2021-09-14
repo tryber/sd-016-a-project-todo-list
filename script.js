29839b34529a11e054b996adad6aff3bb4561ed9
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-14 15:25:49 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const getLista = document.querySelector('ol');
 const getInput = document.querySelector('input');
 const getBut = document.querySelector('button');
+const getButLimp = document.getElementById('apaga-tudo');
 const assignment = [];
-
-function pintaTudo (){
+//Função pinta tudo feita com ajuda do Brunão que me explicou o unset 
+function pintaTudo() {
   const cList = document.querySelectorAll('li');
   for (let index = 0; index < cList.length; index += 1) {
     const listPost = cList[index];
@@ -12,16 +13,26 @@ function pintaTudo (){
 }
 
 function troca(event) {
-      pintaTudo();
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    }
+  pintaTudo();
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function dbClick (event){
+  console.log("cliquei duas vezes");
+  if(event.target.className === 'completed'){
+    event.target.classList.remove('completed')
+  }else{
+    event.target.className = 'completed';
+  }
+  
+}
 
 function showTask() {
 
   getBut.addEventListener('click', function () {
     const list = document.createElement('li');
     list.addEventListener('click', troca);
-    //list.style.backgroundColor = 'red';
+    list.addEventListener('dblclick', dbClick);
     list.innerText = getInput.value;
     getLista.appendChild(list);
     getInput.value = '';
@@ -29,11 +40,21 @@ function showTask() {
 }
 showTask()
 
-/*function clenList (){
-
-  const limp = document.getElementById(apaga-tudo);
-  const taskList = document.querySelector('li');
-  limp.addEventListener('click', function (){
-
+/*function risk (){
+  const riskList = document.querySelectorAll('li');
+  for (let index = 0; index < riskList.length; index += 1){
+    const rList = riskList[index];
+    if()
+    rList.addEventListener('dblclick', function (event){
+      rList.event.target.classList.add('completed');
+    });
   }
-}*/
+}
+risk()*/
+
+const clearList = document.querySelectorAll('li');
+
+  getButLimp.addEventListener('click', function (event){
+    clearList.innerText = getInput.value;
+    clearList.event.target.remove('li');
+  });
