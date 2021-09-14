baaffb9ab426d58080d9efc03574e7c9f6b72d60
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-09-13 23:30:25 -0300

    Adiciona novos requisitos

--- script.js
+++ script.js
@@ -15,8 +15,14 @@ function addItem() {
 }
 button.addEventListener('click', addItem);
 
+// Código elaborado com a ajuda da Laura Fumagalli, Turma 16 - Tribo A.
 function paintList(event) {
-  console.log(event.target);
+  //  Todos os elementos da lista (li) precisam ter PRIMEIRAMENTE um fundo branco.
+  const listItem = document.getElementsByTagName('li');
+  console.log(listItem);
+  for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].style.backgroundColor = 'white';
+  }
   // Ao clicar em um elemento da lista o fundo dele ficará cinza rgb (128, 128, 128).
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
