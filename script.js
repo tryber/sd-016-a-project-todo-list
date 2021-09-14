16eea26cee248d2b11f6663d7cfbf331c8ff289c
Julia Barcelos <julaai@gmail.com>
2021-09-13 23:32:31 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
 
 // transporta o input para uma funcao de adicionar um lista
-
 const taskText = document.querySelector('#texto-tarefa'); // input
 const taskList = document.querySelector('#lista-tarefas'); // ol
 const body = document.querySelector('body'); // o corpo todo
 const createTask = document.querySelector('#criar-tarefa'); //button
-
-
 createTask.addEventListener('click', (event) => { // escuta o button
   const newTask = document.createElement('li'); // cria um l1
   newTask.className = 'task'; // add classe
@@ -16,7 +13,7 @@ createTask.addEventListener('click', (event) => { // escuta o button
   taskText.value = '';
 });
 
-// change background-color - requisito 8
+// change background-color - requisito 8 - me baseei na ideia do Guilherme Augusto para resolver esse desafio, ele me apresentou o contains.
 function changeColor(e) {
   for (let index = 0; index < taskList.children.length; index += 1) {
     if (taskList.children[index].classList.contains('selected')) {
@@ -29,12 +26,33 @@ function changeColor(e) {
 // forListener all tasks
 body.addEventListener('click', (e) => {
   if (e.target.classList.contains('task')) {  //
-    // e.preventDefault();   //ignora os outros enevtListener.
     changeColor(e);
   }
 });
 
 // requisito 9 - Guilherme Spinelli nos mostrou como usar o toggle e ai reduziu o código que eu havia criado como o anterior.
-taskList.addEventListener('dblclick', (event) => {
+taskList.addEventListener('dblclick', (event) => { 
   event.target.classList.toggle('completed')
-})
+});
+// mark with completed task
+// function changeToCompleted(e) {
+//   let task = e.target;
+//   if (task.classList.contains('completed')) {
+//     task.classList.remove('completed');
+//   } else {
+//     task.classList.add('completed');
+//   }
+// }
+// // forListener all finished tasks
+// body.addEventListener('dblclick', (e) => {
+//   if (e.target.classList.contains('task')) {
+//     e.preventDefault();   //ignora os outros enevtListener.
+//     changeToCompleted(e);
+//   }
+// });
+
+// requisito 10
+const clearAll = document.querySelector('#apaga-tudo');
+clearAll.addEventListener('click', () => {
+  taskList.innerHTML = '';
+});
