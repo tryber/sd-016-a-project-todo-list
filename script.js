ca3ae2440d4f96126f238dbfebaaeb43074d7c42
Daniel <danielsantsouza94@gmail.com>
2021-09-14 17:56:57 -0300

    requisitos 7 a 9

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-function taskList() {
-  const input = document.getElementById('texto-tarefa');
-  const listContent = input.value;
-  let list = document.getElementById('lista-tarefas');
+const input = document.getElementById('texto-tarefa');
+const list = document.getElementById('lista-tarefas');
+const button = document.getElementById('criar-tarefa');
 
+function taskList() {
+  const listContent = input.value;
   const creatList = document.createElement('li')
+  creatList.className = 'task'
   list.appendChild(creatList);
   creatList.innerText = listContent;
   input.value = '';
  }
- 
+
+ function paintList(event2){
+   const listContent = document.querySelector('.selected');
+   if (event2.target.className === 'task') {
+     event2.target.classList.add('selected');
+      if (listContent !== null){
+        listContent.classList.remove('selected')
+      } 
+      } 
+   }
+
+ function taskComplete(event3) {
+  const creatList = document.getElementById('lista-tarefas');
+  if (event3.target.classList.contains('completed') === false) {
+    event3.target.classList.add('completed');
+  } else if (event3.target.classList.contains('completed')) {
+    event3.target.classList.remove('completed');
+  }
+}
+
 function addEvents() {
-  const button = document.getElementById('criar-tarefa');
   button.addEventListener('click',taskList);
+  list.addEventListener('click', paintList);
+  list.addEventListener('dblclick', taskComplete);
+  
  }
 
- function selectedClass() {
 
- }
 
 addEvents()
  
