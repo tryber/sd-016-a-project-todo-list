e8990668be314545c247e7434380e6053fad0e03
Angelica <angelicapedroso05@gmail.com>
2021-09-14 00:57:45 -0300

    style

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ function addTask() {
 }
 button.addEventListener('click', addTask);
 
+// 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)
+// O que será verificado:
+
+// Será verificado que, ao se carregar a página, os itens da lista não tem o estilo CSS background-color: rgb(128, 128, 128)
+
+// Será verificado que, ao se clicar em um item da lista, ele passa a ter o estilo CSS background-color: rgb(128, 128, 128).
