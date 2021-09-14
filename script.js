49e44fc5ffb99a856e02f0d5691be8cc0da06201
aparecidag365 <mgegoulart365@gmail.com>
2021-09-14 15:05:43 -0300

    adicionando item 7

--- script.js
+++ script.js
@@ -6,21 +6,22 @@ btnCriarTarefa.addEventListener('click', function () {
   let texto = document.createTextNode(textoTarefa);
   li.appendChild(texto);
   document.getElementById("lista-tarefas").appendChild(li);
-    
   // if (textoTarefa != "") {
-    
-   
-   
   // }
-  
-  li.innerHTML=document.querySelector("#texto-tarefa").value;
-  listatarefas =document.querySelector("#lista-tarefas")
+  li.innerHTML = document.querySelector("#texto-tarefa").value;
+  listatarefas = document.querySelector("#lista-tarefas")
+  //alterar a cor da linha
+  listatarefas.addEventListener('click', newColor)
+  //li.addEventListener('click', newColor) 
   //selista.appendChild(li)=listatarefas
-  document.querySelector("#texto-tarefa").value=""
-  //textoTarefa = ""
+  document.querySelector("#texto-tarefa").value = ""
 })
 
-
+function newColor(event){
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  //document.BodyStyle.backgroundColor = 'rgb(0, 128, 0)';
+  //li.style.backgroundColor = 'rgb(0, 128, 0)';
+}
 
 // function createItemEl(itemValue,itemId){
 //   let btn = document.createElement("li");
@@ -53,58 +54,6 @@ btnCriarTarefa.addEventListener('click', function () {
 //   }
 // })
 
-// const fridayDays = [6, 13, 20, 27];
-// const btnFriday = document.getElementById('btn-friday');
-// btnFriday.addEventListener('click', function () {
-//   const fridays = document.getElementsByClassName('friday-day');
-//   for (let index = 0; index < fridays.length; index += 1) {
-//     if (fridays[index].innerHTML !== "SEXTOU!") {
-//       fridays[index].innerHTML = "SEXTOU!";
-//     } else {
-//       fridays[index].innerHTML = fridayDays[index];
-//     }
-//   }
-// })
-
-// const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-// const weekDaysList = document.querySelector('.week-days');
-
-// for (let index = 0; index < weekDays.length; index += 1) {
-//   let days = weekDays[index];
-
-//   let dayListItem = document.createElement('li');
-//   dayListItem.innerHTML = days;
-
-//   weekDaysList.appendChild(dayListItem);
-// }
-
-// const daysList = [];
-// for (index = 1; index < 31; index += 1) {
-//   daysList.push(index);
-// }
-
-// const getDaysList = document.querySelector('#days');
-
-// for (let index = 0; index < daysList.length; index += 1) {
-//   const day = daysList[index];
-//   const dayItem = document.createElement('li');
-//   if (day === 2 | day === 15) {
-//     dayItem.className = 'holiday';
-//     dayItem.innerHTML = day;
-//     getDaysList.appendChild(dayItem);
-//   } else if (day === 6 | day === 13 | day === 27) {
-//     dayItem.className = 'friday-day';
-//     dayItem.innerHTML = day;
-//     getDaysList.appendChild(dayItem);
-//   } else if (day === 20) {
-//     dayItem.className = 'holiday friday-day';
-//     dayItem.innerHTML = day;
-//     getDaysList.appendChild(dayItem);
-//   } else {
-//     dayItem.innerHTML = day;
-//     getDaysList.appendChild(dayItem);
-//   }
-// }
 
 // function changeColor(elements, color) {
 //   let selectedItems = document.querySelectorAll(elements);
