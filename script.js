5360ad8d4aada0435290cda1c43c7825fc9d90e1
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-09-14 15:21:42 -0300

    7 requisito

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-    //Requisito 5 - inserir texto a lista
+  // Lista de Variaveis
 const button = document.getElementById('criar-tarefa');
 const tarefa = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const lista = document.getElementsByTagName('li');
 
+  // Requisito 5 - inserir texto a lista - Cria a Constante para auxilia na criação da li. Add o texto a li e add o li como filho da ol
 function addTarefa() {
   const listIten = document.createElement('li');
   listIten.innerHTML = tarefa.value;
@@ -10,3 +13,14 @@ function addTarefa() {
 }
 
 button.addEventListener('click', addTarefa);
+
+  // Requisito 7 - inserir Fundo cinza - A função adiciona um fundo branço a todos os itens da lista e ao item q foi clicado ele add o cinza
+function paintList(event) {
+  for (let index = 0; index < lista.length; index += 1) {
+    lista[index].style.backgroundColor = 'white';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+listaTarefas.addEventListener('click', paintList);
+
