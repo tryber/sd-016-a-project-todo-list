509e8e0dadd166da90ec52108bbb5fef1e7121e6
Igor <iigorfelipe@gmail.com>
2021-09-14 18:50:54 -0300

    finaliza requisito 8

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ function createList() {
   let valueOfText = text.value;
   let li = document.createElement('li');
   li.className = 'list';
-  li.innerText = valueOfText;
+  li.innerHTML = valueOfText;
   orderedList.appendChild(li);
   text.value = '';
 }
 
-function changeColor(e) {
+function changeBackground(e) {
+  const list = document.querySelectorAll('.list');
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].style.backgroundColor = '';
+    list[i].className = 'list';
+  }
   e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  e.target.className = 'list selected';
+
 }
-orderedList.addEventListener('click', changeColor);
+orderedList.addEventListener('click', changeBackground);
