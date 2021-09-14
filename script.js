baea1bec00a03d24b9294f4ec931868cfd0b56b8
Daniel <danielsantsouza94@gmail.com>
2021-09-14 18:59:50 -0300

    corrgigindo linter

--- script.js
+++ script.js
@@ -4,25 +4,24 @@ const button = document.getElementById('criar-tarefa');
 
 function taskList() {
   const listContent = input.value;
-  const creatList = document.createElement('li')
-  creatList.className = 'task'
+  const creatList = document.createElement('li');
+  creatList.className = 'task';
   list.appendChild(creatList);
   creatList.innerText = listContent;
   input.value = '';
- }
+}
 
  function paintList(event2){
    const listContent = document.querySelector('.selected');
    if (event2.target.className === 'task') {
      event2.target.classList.add('selected');
       if (listContent !== null){
-        listContent.classList.remove('selected')
-      } 
-      } 
-   }
+      listContent.classList.remove('selected');
+    }
+  }
+}
 
- function taskComplete(event3) {
-  const creatList = document.getElementById('lista-tarefas');
+function taskComplete(event3) {
   if (event3.target.classList.contains('completed') === false) {
     event3.target.classList.add('completed');
   } else if (event3.target.classList.contains('completed')) {
@@ -31,18 +30,9 @@ function taskList() {
 }
 
 function addEvents() {
-  button.addEventListener('click',taskList);
+  button.addEventListener('click', taskList);
   list.addEventListener('click', paintList);
   list.addEventListener('dblclick', taskComplete);
   
  }
-
-
-
 addEvents()
- 
-
-
-
- 
- 
