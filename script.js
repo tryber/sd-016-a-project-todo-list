bf0d4786a9744d6b510d486b0e13b6298efd57e2
douglas <douglasfmr1@gmail.com>
2021-09-14 04:15:58 -0300

    exercicios

--- script.js
+++ script.js
@@ -8,18 +8,17 @@ const down = document.querySelector('#mover-baixo');
 const removeGraySelected = document.querySelector('#remover-selecionado');
 // const save = document.querySelector('#salvar-tarefas');
 
-
 button.addEventListener('click', function () {
   const createLi = document.createElement('li');
   if (input.value.length > 0) {
     createLi.innerText = input.value;
-    createLi.className = 'li-lista'
+    createLi.className = 'li-lista';
     list.appendChild(createLi);
     input.value = '';
-    createLi.addEventListener('click', addGrey)
-    createLi.addEventListener('dblclick', addRisk)
+    createLi.addEventListener('click', addGrey);
+    createLi.addEventListener('dblclick', addRisk);
   }
-})
+});
 
 function addGrey(event) {
   const selectList = document.querySelector('.graycolor');
@@ -29,34 +28,25 @@ function addGrey(event) {
   event.target.classList.add('graycolor');
 }
 
-
-
 clearAll.addEventListener('click', function () {
   while (list.firstElementChild) {
     list.removeChild(list.firstElementChild);
   }
-})
+});
 
 function addRisk(event) {
   event.target.classList.toggle('completed');
-
 }
 
-
 function removeFinished() {
-  finisHim = document.querySelectorAll('li.completed');
+  const finisHim = document.querySelectorAll('li.completed');
   for (let index = 0; index < finisHim.length; index += 1) {
     list.removeChild(document.querySelector('li.completed'));
   }
 }
 
-
-
-
-
-
 function moveUp() {
-  let upUp = document.querySelector('li.graycolor');
+  const upUp = document.querySelector('li.graycolor');
   if (upUp != null) {
     if (upUp.previousElementSibling != null) {
       upUp.parentNode.insertBefore(upUp, upUp.previousElementSibling);
@@ -65,7 +55,7 @@ function moveUp() {
 }
 
 function moveDown() {
-  let downDown = document.querySelector('li.graycolor');
+  const downDown = document.querySelector('li.graycolor');
   if (downDown != null) {
     if (downDown.nextElementSibling != null) {
       downDown.parentNode.insertBefore(downDown, downDown.nextElementSibling.nextElementSibling);
@@ -74,20 +64,17 @@ function moveDown() {
 }
 
 function removeSelect() {
-  let selectHim = document.querySelector('li.graycolor');
+  const selectHim = document.querySelector('li.graycolor');
   if (selectHim != null) {
-  selectHim.remove()
+    selectHim.remove();
   }
 }
 
 // function savePage() {
 // }
 
-
-
 up.addEventListener('click', moveUp);
 down.addEventListener('click', moveDown);
 clearFinalizados.addEventListener('click', removeFinished);
 removeGraySelected.addEventListener('click', removeSelect);
 // save.addEventListener('click', savePage);
-
