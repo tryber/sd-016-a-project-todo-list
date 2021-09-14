4417c3ff69c8f43b44fcb3ae36d94a8267dcc64a
Danilo Meneguela <meneguelaprog@hotmail.com>
2021-09-13 23:40:01 -0300

    Requisito 7 Completo

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const addButton = document.getElementById('criar-tarefa');
 const allList = document.getElementById('lista-tarefas');
 const task = document.getElementById('texto-tarefa');
+let taskList = document.getElementsByClassName('task-list');
 
-addButton.addEventListener('click', () =>{
+addButton.addEventListener('click', () => {
   const newList = document.createElement('li');
   newList.innerHTML = task.value;
+  newList.classList = 'task-list';
+  // newList.style.backgroundColor = 'white';
   allList.appendChild(newList);
-  task.value = "";
-})
+  task.value = '';
+  console.log(taskList);
+  for (let index = 0; index < taskList.length; index += 1) {
+    taskList[index].addEventListener('click', (event) => {
+      console.log(taskList);
+      taskList[index].style.backgroundColor = 'white';
+      event.target.style.backgroundColor = 'rgb(128,128,128)';
+    });  
+  }
+});
+// for (let index = 0; index < taskList.length; index += 1) {
+//   taskList.addEventListener('click', (event) => {
+//     const taskList = document.getElementsByClassName('task-list');
+//     if (taskList[index].style.backgroundColor == 'white'){
+//       event.target.style.backgroundColor = 'rgb(128,128,128)';
+//     } else {
+//       event.target.style.backgroundColor = 'white';
+//     }
+//   });  
+// }
+
+// for (let index = 0; index < taskList.length; index += 1) {
+//   taskList[index].addEventListener('click', (event) => {
+//     console.log(taskList);
+//     taskList[index].style.backgroundColor = 'white';
+//     event.target.style.backgroundColor = 'rgb(128,128,128)';
+//   });  
+// }
