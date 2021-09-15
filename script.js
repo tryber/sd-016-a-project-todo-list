a2277a7089dac966188e79a10c88feaf646afb06
Daniel <danielsantsouza94@gmail.com>
2021-09-15 10:41:09 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
+const button2 = document.getElementById('apaga-tudo');
+const button3 = document.getElementById('remover-finalizados');
 
 function taskList() {
   const listContent = input.value;
@@ -10,6 +12,8 @@ function taskList() {
   creatList.innerText = listContent;
   input.value = '';
 }
+
+//ref: lógica aprendida através de codereview de codigo da turma 13;
 
 function paintList(event2) {
   const listContent = document.querySelector('.selected');
@@ -21,6 +25,8 @@ function paintList(event2) {
   }
 }
 
+//ref: lógica aprendida através de codereview de codigo da turma 13;
+
 function taskComplete(event3) {
   if (event3.target.classList.contains('completed') === false) {
     event3.target.classList.add('completed');
@@ -29,9 +35,31 @@ function taskComplete(event3) {
   }
 }
 
+// ref: https://www.w3schools.com/jsref/met_node_removechild.asp
+
+function apagaTudo() {
+  let lista = document.getElementById('lista-tarefas');
+  while (lista.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+//ref: lógica aprendida através de codereview de codigo da turma 13;
+
+function apagaFinalizados() {
+  let finalizados = document.querySelectorAll('.completed');
+  for (let index = 0; index < finalizados.length; index += 1) {
+    if (finalizados[index].classList.contains('completed')) {
+      list.removeChild(finalizados[index]);
+    }
+  }
+}
+
 function addEvents() {
   button.addEventListener('click', taskList);
   list.addEventListener('click', paintList);
   list.addEventListener('dblclick', taskComplete);
+  button2.addEventListener('click', apagaTudo);
+  button3.addEventListener('click', apagaFinalizados);
 }
 addEvents();
