b3c90ca2308fe47432a9b9e61947ea8af4a05f74
Leandro Leao <leandro.leao@outlook.com>
2021-09-14 23:18:55 -0300

    inclusao do item 9

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let button = document.createElement('button');
-let parentButton = document.getElementById('lista-tarefas');
+let parentList = document.getElementById('lista-tarefas');
+let parentbutton = document.getElementById('mainList')
 let inputText = document.getElementById('texto-tarefa');
 
 function createbutton() {
   button.id = 'criar-tarefa';
   button.innerText = 'Incluir Tarefa'
 
-  parentButton.appendChild(button);
+  parentbutton.appendChild(button);
 }
 
 createbutton();
@@ -15,18 +16,28 @@ function clickButton() {
   let taskList = document.createElement('li');
   taskList.className = 'item-list';
   taskList.innerText = inputText.value;
-  parentButton.appendChild(taskList);
+  parentList.appendChild(taskList);
   inputText.value = '';
 }
 
 button.addEventListener('click', clickButton);
 
 function colorList(event) {
-  let itemList = document.getElementsByClassName('item-list')
+  let itemList = document.getElementsByClassName('item-list');
   for (let i = 0; i < itemList.length; i += 1) {
     itemList[i].style.background = 'white';
   }
   event.target.style.background = 'rgb(128, 128, 128)';
   }
 
-  parentButton.addEventListener('click', colorList);
+parentList.addEventListener('click', colorList);
+
+function completedTasks (event) {
+  let uncompletedTasks = document.getElementsByClassName('item-list');
+  for (let i = 0; i < uncompletedTasks.length; i += 1) {
+    uncompletedTasks[i].classList.add('uncompleted');
+  }
+  event.target.classList.toggle('completed');
+}
+
+parentList.addEventListener('dblclick', completedTasks);
