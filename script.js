f0505f425d04bcd80463cd4f470d8e716bc8e8a0
FernandoMos92 <fernandomosp@gmail.com>
2021-09-15 15:53:27 -0300

    refactoring the code to be cleaner.

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-const header = document.createElement('header');
-document.body.appendChild(header);
+const main = document.getElementsByTagName('main');
+const inputTasks = document.getElementById('texto-tarefa');
+const orderedList = document.getElementById('lista-tarefas');
+const btnClear = document.getElementById('apaga-tudo');
+const btnAllClear = document.createElement('remover-finalizados');
+const btnAdd = document.getElementById('criar-tarefa');
+const btnSalveTask = document.createElement('salvar-tarefas');
+const btnRemoveSelected = document.createElement('remover-selecionado');
 
-const titleH1 = document.createElement('h1');
-titleH1.innerHTML = 'Minha Lista de Tarefas';
-titleH1.style.textAlign = 'center';
-header.appendChild(titleH1);
-
-const titleH2 = document.createElement('h2');
-titleH2.id = 'funcionamento';
-titleH2.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-titleH2.style.textAlign = 'center';
-header.appendChild(titleH2);
-
-const main = document.createElement('main');
-document.body.appendChild(main);
-
-const containerInput = document.createElement('div');
-containerInput.id = 'containerInput';
-main.appendChild(containerInput);
-
-const inputTasks = document.createElement('input');
-inputTasks.id = 'texto-tarefa';
-inputTasks.placeholder = 'Insira sua nova tarefa';
-containerInput.appendChild(inputTasks);
-
-const btnAdd = document.createElement('button');
-btnAdd.id = 'criar-tarefa';
-btnAdd.innerText = 'Adicionar';
-containerInput.appendChild(btnAdd);
-
-const orderedList = document.createElement('ol');
-orderedList.id = 'lista-tarefas';
-orderedList.style.userSelect = 'none';
-orderedList.style.width = '100vw';
 const repairItens = JSON.parse(localStorage.getItem('saveList'));
 if (repairItens !== null) {
   repairItens.forEach((el) => {
@@ -45,17 +19,7 @@ if (repairItens !== null) {
     orderedList.appendChild(itemList);
     console.log(el.className.split(' '));
   });
-} main.appendChild(orderedList);
-
-const btnClear = document.createElement('button');
-btnClear.innerText = 'Limpar';
-btnClear.id = 'apaga-tudo';
-containerInput.appendChild(btnClear);
-
-const btnAllClear = document.createElement('button');
-btnAllClear.innerText = 'Concluídos';
-btnAllClear.id = 'remover-finalizados';
-containerInput.appendChild(btnAllClear);
+}
 
 btnClear.addEventListener('click', () => {
   orderedList.innerHTML = '';
@@ -81,7 +45,7 @@ orderedList.addEventListener('click', (event) => {
   const listLi = document.querySelectorAll('li');
   const currentLi = event.target;
   for (let index = 0; index < listLi.length; index += 1) {
-    listLi[index].style.backgroundColor = 'white';
+    listLi[index].style.backgroundColor = 'rgb(253 , 77 , 176)';
   }
   currentLi.style.backgroundColor = 'rgb(128,128,128)';
 });
@@ -90,11 +54,6 @@ orderedList.addEventListener('dblclick', (event) => {
 });
 
 // salvar-tarefas
-
-const btnSalveTask = document.createElement('button');
-btnSalveTask.id = 'salvar-tarefas';
-btnSalveTask.innerText = 'Salvar tarefas';
-containerInput.appendChild(btnSalveTask);
 btnSalveTask.addEventListener('click', () => {
   const contentList = Array.from(main.children[1].children);
   const containerSave = contentList.map((el) => ({
@@ -106,10 +65,6 @@ btnSalveTask.addEventListener('click', () => {
 });
 
 // remover-selecionado
-const btnRemoveSelected = document.createElement('button');
-btnRemoveSelected.id = 'remover-selecionado';
-btnRemoveSelected.innerText = 'Remover selecionado';
-containerInput.appendChild(btnRemoveSelected);
 btnRemoveSelected.addEventListener('click', () => {
   const findItem = Array.from(main.children[1].children);
   const newOutput = findItem.filter((el) => el.style.backgroundColor !== 'rgb(128, 128, 128)');
