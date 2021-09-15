e7007c5cda0877f2ad82c8a7cf1f5dcfe853cea6
joaovitordantas <joaovitordantasss@hotmail.com>
2021-09-15 15:52:51 -0300

    ajuste taskinsert

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let taskList = document.getElementById('lista-tarefas').innerHTML;
-let createdItem;
-let taskId;
-let task;
-// para criar as tarefas
+// para inserir as tarefas
+
 function taskInsert() {
-  const taskInput = document.getElementById('texto-tarefa').value;
-  taskList += '<li>' + taskInput + '</li>';
-  document.getElementById('lista-tarefas').innerHTML = taskList;
-  document.getElementById('texto-tarefa').value = '';
+  const taskInput = document.getElementById('texto-tarefa').value; // receber valor do input
+  let taskList = document.getElementById('lista-tarefas').innerHTML; // inicializar variável para inserir tarefas
+  taskList += ('<li>' + taskInput + '</li>'); // criando os itens de lista
+  document.getElementById('lista-tarefas').innerHTML = taskList; // adicionando o item na lista ordenada
+  document.getElementById('texto-tarefa').value = ''; // zerar valor do input
+  let tasks = document.querySelectorAll("li"); // definir id 'task' para as tarefas criadas
+  for (let index = 0; index < tasks.length; index++) {
+  tasks[index].id = 'task';
+}
 }
 
-// selecionar as tarefas
-function selectItem() {
+// para selecionar as tarefas criadas
 
-}
 
-// remover tarefas
-function removeTask() {
+// para remover as tarefas da lista
 
-}
+
