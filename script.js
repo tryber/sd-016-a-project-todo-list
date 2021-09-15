91afca8549d06daf79f0c84d77a182834d1766e0
Leandro Leao <leandro.leao@outlook.com>
2021-09-15 00:31:52 -0300

    Codigo refatorando para tirar o botao sendo criado atraves de JS , incluido assim como os outros no HTML, e criaçao de uma unica variavel para document.getElementsByClassName('item-list') chamada de taskItemList, visto que a mesma se repetia variadas vezes nas funcoes do codigo.

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let button = document.createElement('button');
+let button = document.getElementById('criar-tarefa');
 let eraseButton = document.getElementById('apaga-tudo');
 let eraseDoneButton = document.getElementById('remover-finalizados');
 let parentList = document.getElementById('lista-tarefas');
 let parentbutton = document.getElementById('mainList')
 let inputText = document.getElementById('texto-tarefa');
-
-function createbutton() {
-  button.id = 'criar-tarefa';
-  button.innerText = 'Incluir Tarefa'
-
-  parentbutton.appendChild(button);
-}
-
-createbutton();
+let taskItemList = document.getElementsByClassName('item-list');
 
 function clickButton() {
   let taskList = document.createElement('li');
@@ -25,9 +17,8 @@ function clickButton() {
 button.addEventListener('click', clickButton);
 
 function colorList(event) {
-  let itemList = document.getElementsByClassName('item-list');
-  for (let i = 0; i < itemList.length; i += 1) {
-    itemList[i].style.background = 'white';
+  for (let i = 0; i < taskItemList.length; i += 1) {
+  taskItemList[i].style.background = 'white';
   }
   event.target.style.background = 'rgb(128, 128, 128)';
   }
@@ -35,20 +26,19 @@ function colorList(event) {
 parentList.addEventListener('click', colorList);
 
 function completedTasks (event) {
-  let uncompletedTasks = document.getElementsByClassName('item-list');
-  for (let i = 0; i < uncompletedTasks.length; i += 1) {
-    uncompletedTasks[i].classList.add('uncompleted');
+  for (let i = 0; i < taskItemList.length; i += 1) {
+    taskItemList[i].classList.add('uncompleted');
   }
   event.target.classList.toggle('completed');
 }
 
 parentList.addEventListener('dblclick', completedTasks);
 
-// Função removeAll com *while* feita com base na fonte StackOverflow link: https://stackoverflow.com/questions/4777077/removing-elements-by-class-name //
+// Função removeAll e removeListDone com *while* feitas com base na fonte StackOverflow link: https://stackoverflow.com/questions/4777077/removing-elements-by-class-name //
+
 function removeAll () {
-  let removeList = document.getElementsByClassName('item-list');
-  while (removeList.length > 0) {
-    removeList[0].parentNode.removeChild(removeList[0]);
+  while (taskItemList.length > 0) {
+  taskItemList[0].parentNode.removeChild(taskItemList[0]);
   }
 }
 
