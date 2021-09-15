32ea70405f38d0d485963cfa39a7e23a02e6360b
Gregory <gregorysak90@gmail.com>
2021-09-15 11:40:32 -0300

    Requisitos 7 e 8, feitos

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-// Requisitos 5 e 6
+/*                                          Requisitos 5, 6, 7, 8                                           */
 
 function addTasks(){
   let textInput = document.getElementById('texto-tarefa');
   let tasksList = document.getElementById('lista-tarefas');
   let task = document.createElement('li');
+  task.classList.add('localLi')
+
   task.innerText = textInput.value;
-  tasksList.appendChild(task);
+  tasksList.appendChild(task); 
+
   textInput.value = '';
 }
+ 
+
+function bckColor(e){
+  
+  const lis = document.querySelectorAll('.localLi');
+
+  for(let i = 0; i < document.querySelectorAll('.localLi').length; i += 1){
+    lis[i].style.backgroundColor = 'white';
+    lis[i].classList.remove('selected');
+  }
+
+  e.classList.add('selected');
+  e.style.backgroundColor = 'rgb(128,128,128)';
+
+}
+
+// helped and reference code of Ádran.
+document.addEventListener('click',(e) =>{
+  if(e.target.classList.contains('localLi')){
+    bckColor(e.target);
+  }
+});
 
 document.querySelector('#criar-tarefa').addEventListener('click', addTasks);
