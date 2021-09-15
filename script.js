1c5929a48bf16addd32e6bd31febddd4ad6ad5db
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-09-15 10:11:06 -0300

    adicionando comentarios

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const buttonAdd = document.querySelector('#criar-tarefa');
 const addList = document.querySelector('#lista-tarefas');
-buttonAdd.addEventListener('click', add);
 
+// quinto requisito - clicar no botão adiciona um novo item.
+// sexto requisito - adicionar ao menos 3 intens a lista;
+buttonAdd.addEventListener('click', add);
 
 function add () {
   const inputAdd = document.querySelector('#texto-tarefa');
@@ -13,6 +15,8 @@ function add () {
   inputAdd.value = '';
 }
 
+// setimo requisito - clicar em um item altera o fundo.
+// oitavo requisito - selecionar um por vez.
 addList.addEventListener('click', changeBackground);
 
 function changeBackground (event) {
@@ -20,9 +24,10 @@ function changeBackground (event) {
   for (let index = 0; index < selectLi.length; index += 1) {
     selectLi[index].style.backgroundColor = 'white';
   }
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.style.backgroundColor = 'grey';
 }
 
+// nono requisito - duplo click risca o item depois desfaz o risco. 
 addList.addEventListener('dblclick', changeTextStyle);
 
 function changeTextStyle (event) {
@@ -30,23 +35,26 @@ function changeTextStyle (event) {
 }
 //https://www.w3schools.com/howto/howto_js_toggle_class.asp
 
+// décimo requisito - click no botão apaga todos os itens da lista.
 const deleteButton = document.querySelector('#apaga-tudo');
 deleteButton.addEventListener('click', deleteAll);
 
 function deleteAll () {
   const removeList = document.querySelectorAll('.line')
- for (let index = 0; index < removeList.length; index += 1) {
+  for (let index = 0; index < removeList.length; index += 1) {
    const element = removeList[index];
    addList.removeChild(element);
  }
 }
+
+// décimo primeiro requisito - click no botão remove os itens riscados.
 const buttonRemoveStyle = document.querySelector('#remover-finalizados');
 buttonRemoveStyle.addEventListener('click', removeStyle);
 
- function removeStyle () {
-  const removeListStyle = document.querySelectorAll('.completed')
- for (let index = 0; index < removeListStyle.length; index += 1) {
+function removeStyle () {
+  const removeListStyle = document.querySelectorAll('.completed');
+  for (let index = 0; index < removeListStyle.length; index += 1) {
    const element = removeListStyle[index];
    addList.removeChild(element);
- }
- }
+  }
+}
