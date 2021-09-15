5da015ec8be328c07ed39d556ff11b9343c1985b
Fabricio Martins <fabriciocsmartins@gmail.com>
2021-09-15 09:36:41 -0300

    novos requisitos completos

--- script.js
+++ script.js
@@ -1,53 +1,75 @@
 const inputList = document.querySelector('#texto-tarefa');
 const buttonInput = document.querySelector('#criar-tarefa');
 const button = document.querySelector('#apaga-tudo');
-let list = document.querySelector('#lista-tarefas');
-let text = null
+const save = document.querySelector('#salvar-tarefas');
+const itemsList = document.querySelectorAll('li');
+const list = document.querySelector('#lista-tarefas');
+let text = null;
 
-function getText(){  
+function getSavedItems() {
+  let savedItems = Object.values(localStorage);
+  for (index = 0; index < savedItems.length; index += 1) {
+    let assignment = document.createElement('li');
+    assignment.className = 'tarefas'
+    assignment.innerText = savedItems[index];
+    list.appendChild(assignment);
+  }
+}
+getSavedItems();
+
+function getText() {  
   inputList.addEventListener('keyup', function (e) {
-      text = e.target.value;
-      console.log(text)
-      return text
+    text = e.target.value;
+    return text;
   });
 }
-getText()
+getText();
 
 function insertList() {
   buttonInput.addEventListener('click', function () {
     let assignment = document.createElement('li');
-    assignment.className = 'tarefas'
+    assignment.className = 'tarefas';
     assignment.innerText = text;
     if (assignment.innerText === '') {
-      alert('Desculpe, não pode acrescentar um item vazio!')
+      alert('Desculpe, não pode acrescentar um item vazio!');
       return
     }
     list.appendChild(assignment);
     inputList.value = '';
     assignment.removeAttribute('innerText');
     text = '';
-    selectItemsList()
-    return text
+    selectItemsList();
+    return text;
   });
 }
-insertList()
+insertList();
 
 function selectItemsList() {
-  let itemsList = document.querySelectorAll('li')
-  console.log(itemsList)
+  let itemsList = document.querySelectorAll('li');
   itemsList.forEach( ev => ev.addEventListener('click', function () {
     for (index = 0; index < itemsList.length; index += 1) {
       itemsList[index].style.backgroundColor = 'white';
     }
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   }));
 }
 
 function cleanList() {
-  let itemsList = document.querySelectorAll('li');
   button.addEventListener('click', function () {
-    itemsList = []
-    })
-    return itemsList;
+    const items = document.querySelectorAll('.tarefas');
+    items.forEach(ev => ev.remove());
+  })
+    return
 }
-cleanList()
+cleanList();
+
+function saveItems () {
+  save.addEventListener('click', function () {
+    const items = document.querySelectorAll('.tarefas');
+    for (index = 0; index < items.length; index += 1) {
+      localStorage.setItem(index, items[index].innerText);
+    }
+  })
+}
+saveItems();
+
