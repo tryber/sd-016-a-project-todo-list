61253ec412e502bae91977d83ca09576b5f88296
severo <oliveira.severo@gmail.com>
2021-09-15 11:34:26 -0300

    Nao consigo pegar os elementos da lista

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const adicionarTarefa = document.getElementById('criar-tarefa');
+const myList = document.querySelector('ol').childNodes;
+// const lista = document.getElementsByTagName('li');
+
+// console.log(myList, lista);
 
 adicionarTarefa.addEventListener('click', function () {
-  let node = document.createElement('li');
-  let text = document.getElementById('texto-tarefa').value; 
-  let textnode=document.createTextNode(text);
+  const node = document.createElement('li');
+  const text = document.getElementById('texto-tarefa').value;
+  const textnode = document.createTextNode(text);
+
   document.getElementById('texto-tarefa').value = '';
   if (text === '') {
     alert('🤪 Tá doido? Uma tarefa precisa ser preenchida.');
   } else {
     node.appendChild(textnode);
-    document.getElementById("lista-tarefas").appendChild(node);
+    document.getElementById('lista-tarefas').appendChild(node);
   }
 });
+
+
+function selecionaItem(alvo) {
+  for (let i = 0; i < myList.length; i += 1) {
+    myList[i].classList.remove('select_list_item');
+  }
+  alvo.target.classList.add('select_list_item');
+}
+
+function adicionarEscutador() {
+  for (let i = 0; i< myList.length; i += 1) {
+    myList[i].addEventListener('click', selecionaItem);
+  }
+}
+
+adicionarEscutador();
