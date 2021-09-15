95a6fe8a9a16e114b206af93fab4b91db4f3e7fc
baky2708 <flavioisaque123@gmail.com>
2021-09-15 08:10:20 -0300

    07 requeriment - Complete

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const btnCreateTask = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
+function markTaskItem(event) {
+  const backgroundColor = 'rgb(128, 128, 128)';
+  const taskItem = event;
+  taskItem.target.style.backgroundColor = backgroundColor;
+}
+
 function createTask() {
   const listItem = document.createElement('li');
   listItem.className = 'listItem';
   const contentInput = document.querySelector('#texto-tarefa').value;
   listItem.innerHTML = contentInput;
+  listItem.addEventListener('click', markTaskItem);
   document.querySelector('#texto-tarefa').value = '';
   console.log(`valor input = ${contentInput}`);
   list.appendChild(listItem);
