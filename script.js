0817f25249633c4b430d95328b2430583b318f1e
André Luís Wenningkamp <andre.wenningkamp1@gmail.com>
2021-09-15 20:41:56 -0300

    Requisitos ate o 9

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const listItems = document.querySelector('#lista-tarefas').children;
+
+function changeBgColor(event) {
+  const elementSelected = event.target.style;
+  let color = 'rgb(128,128,128)';
+  for (let index = 0; index < listItems.length; index += 1) {
+    listItems[index].style.backgroundColor = 'white';
+    if (elementSelected.backgroundColor === 'rgb(128,128,128)') {
+      color = 'white';
+      elementSelected.backgroundColor = 'color';
+    }
+  }
+  elementSelected.backgroundColor = color;
+}
+
+function finishedTask(event) {
+  const targetTask = event.target;
+  if (targetTask.className === 'completed') {
+    targetTask.className = '';
+    targetTask.style.textDecoration = 'none';
+    return;
+  }
+  targetTask.style.textDecoration = 'line-through';
+  targetTask.style.backgroundColor = 'white';
+  targetTask.className = 'completed';
+}
 
 function addTask() {
   const task = document.querySelector('#texto-tarefa').value;
@@ -7,6 +33,10 @@ function addTask() {
   newElement.innerHTML = task;
   document.querySelector('#texto-tarefa').value = '';
   taskList.appendChild(newElement);
+  for (let index = 0; index < listItems.length; index += 1) {
+    listItems[index].addEventListener('click', changeBgColor);
+    listItems[index].addEventListener('dblclick', finishedTask);
+  }
 }
 
 button.addEventListener('click', addTask);
