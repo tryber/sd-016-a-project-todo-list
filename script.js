82a203a32cbeb6cba2104633e778822410acdedb
cristianoterra <cristianoterr@gmail.com>
2021-09-15 08:53:08 -0300

    altera a cor de fundo e seleciona elementos

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ document.querySelector('#criar-tarefa').addEventListener('click', () => {
   document.querySelector('ol').appendChild(newItem);
   newItem.innerText = document.querySelector('input').value;
   document.querySelector('input').value = '';
+
+  newItem.addEventListener('click', () => {
+    const selectedItem = document.querySelector('.selected');
+    if (selectedItem) {
+      selectedItem.classList.remove('selected');
+    }
+    newItem.classList.add('selected');
+  });
 });
