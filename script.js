8a89d6fe96de6412401db7a5ef133656f33c0c35
severo <oliveira.severo@gmail.com>
2021-09-14 22:00:54 -0300

    limpando o codigo

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let adicionarTarefa = document.getElementById('criar-tarefa');
+const adicionarTarefa = document.getElementById('criar-tarefa');
 
 adicionarTarefa.addEventListener('click', function () {
-  var node = document.createElement("li");
-  var text = document.getElementById("texto-tarefa").value; 
-  var textnode=document.createTextNode(text);
-  document.getElementById("texto-tarefa").value = '';
-  if (text === ''){
-    alert("🤪 Tá doido? Uma tarefa precisa ser preenchida.");
+  let node = document.createElement('li');
+  let text = document.getElementById('texto-tarefa').value; 
+  let textnode=document.createTextNode(text);
+  document.getElementById('texto-tarefa').value = '';
+  if (text === '') {
+    alert('🤪 Tá doido? Uma tarefa precisa ser preenchida.');
   } else {
     node.appendChild(textnode);
-  document.getElementById("lista-tarefas").appendChild(node);
+    document.getElementById("lista-tarefas").appendChild(node);
   }
 });
