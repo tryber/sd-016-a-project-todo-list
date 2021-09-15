be3c448d6865c18416c0b5a5556ccb7ca8ff107c
Felipe David <felipe.besorean@hotmail.com>
2021-09-14 21:11:56 -0300

    adiciona linha quando usado o double-click em itens

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector('#texto-tarefa');
 const addButton = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
-function cleaClass() {
+function clearClass() {
   const allLI = document.getElementsByTagName('li');
   for (let i = 0; i < allLI.length; i += 1) {
     if (allLI[i].classList.contains('selected')) {
@@ -16,9 +16,13 @@ function createItem(expectedText) {
     const li = document.createElement('li');
     list.appendChild(li).innerHTML = input.value;
     input.value = '';
+    li.addEventListener('dblclick', (e) => {
+      const select = e.target;
+      select.classList.toggle('completed');
+    });
     li.addEventListener('click', (event) => {
       const selector = event.target;
-      cleaClass();
+      clearClass();
       selector.classList.toggle('selected');
     });
   }
