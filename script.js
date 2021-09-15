4b179ab901971c4844059653a0dc32471e78f666
Leandro Leao <leandro.leao@outlook.com>
2021-09-14 23:06:15 -0300

    refatoraçao do codigo, inclusao do item 8

--- script.js
+++ script.js
@@ -11,40 +11,22 @@ function createbutton() {
 
 createbutton();
 
-
-function colorList() {
-  let listColor = document.getElementsByClassName('item-list')
-  for (let index = 0; index < listColor.length; index += 1) {
-  listColor[index].addEventListener('click', function() {
-        if (listColor[index].style.background === 'white') {
-          listColor[index].style.background = 'rgb(128, 128, 128)';
-        } else {
-          listColor[index].style.background = 'white';
-        }
-      })
-    }
-    console.log(listColor);
-  }
-
-
 function clickButton() {
-button.addEventListener('click', function() {
   let taskList = document.createElement('li');
   taskList.className = 'item-list';
   taskList.innerText = inputText.value;
   parentButton.appendChild(taskList);
   inputText.value = '';
-  colorList()
+}
+
+button.addEventListener('click', clickButton);
+
+function colorList(event) {
+  let itemList = document.getElementsByClassName('item-list')
+  for (let i = 0; i < itemList.length; i += 1) {
+    itemList[i].style.background = 'white';
   }
-  )}
+  event.target.style.background = 'rgb(128, 128, 128)';
+  }
 
-clickButton();
-
-  
-
-
-
-
-
-
-
+  parentButton.addEventListener('click', colorList);
