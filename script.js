bb8da61e2e724f17a228a98cb1218c613899e902
IgorHamzi <igorwhamzi@gmail.com>
2021-09-15 12:47:54 -0300

    adiciona req 14

--- script.js
+++ script.js
@@ -1,6 +1,82 @@
+// Requisito 7 e 8
+function taskColor(origin) {
+  const tasks = document.getElementsByTagName('li');
+  for (let index = 0; index < tasks.length; index += 1) {
+    tasks[index].style.backgroundColor = 'white';
+    tasks[index].classList.remove('color');
+  }
+  origin.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  origin.target.classList.add('color');
+}
+
+function addEvent() {
+  const tarefas = document.getElementsByTagName('li');
+  for (let index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].addEventListener('click', taskColor);
+  }
+}
+
+// Requisito 9
+function lineTask(event) {
+  event.target.classList.toggle('completed');
+}
+
+function taskEvent() {
+  const task = document.getElementsByTagName('li');
+  for (let index = 0; index < task.length; index += 1) {
+    task[index].addEventListener('dblclick', lineTask);
+  }
+}
+
+// Requisito 10 - Botão apagar tudo 
+function deleteAll() {
+  const task = document.querySelectorAll('.line');
+//  const task2 = document.getElementsByClassName('line');
+//  const task3 = document.getElementsByTagName('li');
+  const lista = document.querySelector('#lista-tarefas');
+  for (let index = 0; index < task.length; index += 1) {
+    lista.removeChild(task[index]);
+  }
+}//Dúvida
+
+function deleteAllClick() {
+  const buttonDeleteAll = document.getElementById('apaga-tudo')
+  buttonDeleteAll.addEventListener('click', deleteAll);
+}
+
+// Requisito 11
+function deleteCompleted() {
+  console.log('teste');
+  const lista = document.querySelector('#lista-tarefas');
+  const element = document.querySelectorAll('.completed');
+  for (let index = 0; index < element.length; index += 1) {
+    lista.removeChild(element[index]);
+  }
+}
+
+function deleteCompletedClick() {
+  const buttonDeleteSelected = document.getElementById('remover-finalizados');
+  buttonDeleteSelected.addEventListener('click', deleteCompleted);
+}
+
+// Requisito 14
+ function deleteSelected() {
+   const lista = document.querySelector('#lista-tarefas');
+   const completed = document.querySelectorAll('.color');
+   console.log('teste');
+   for (let index = 0; index < completed.length; index += 1) {
+     lista.removeChild(completed[index]);
+   }
+ }
+
+ function deleteSelectedClick() {
+   const taskCompleted = document.getElementById('remover-selecionado');
+   taskCompleted.addEventListener('click', deleteSelected);
+ }
+
 // Requisito 4,5,6
 const button = document.getElementById('criar-tarefa');
-button.addEventListener('click', adicionaTarefa);
+
 function adicionaTarefa() {
   const input = document.querySelector('#texto-tarefa');
   const lista = document.querySelector('#lista-tarefas');
@@ -13,61 +89,17 @@ function adicionaTarefa() {
   addEvent();
   taskEvent();
   deleteAllClick();
-  deleteSelectedClick();
+  deleteCompletedClick();
+  deleteSelectedClick()
 }
+button.addEventListener('click', adicionaTarefa);
 
-// Requisito 7 e 8
-function addEvent() {
-  const tarefas = document.getElementsByTagName('li');
-  for (let index = 0; index < tarefas.length; index += 1) {
-    tarefas[index].addEventListener('click', taskColor);
-  }
-}
-function taskColor(origin) {
-  const tasks = document.getElementsByTagName('li');
-  for (let index = 0; index < tasks.length; index += 1) {
-    tasks[index].style.backgroundColor = 'white';
-  }
-  origin.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
 
-// Requisito 9
-function taskEvent() {
-  const task = document.getElementsByTagName('li');
-  for (let index = 0; index < task.length; index += 1) {
-    task[index].addEventListener('dblclick', lineTask);
-  }
-}
-function lineTask(event) {
-  event.target.classList.toggle('completed');
-}
 
-// Requisito 10 - Botão apagar tudo 
-function deleteAllClick() {
-  const buttonDeleteAll = document.getElementById('apaga-tudo')
-  buttonDeleteAll.addEventListener('click', deleteAll);
-}
 
-function deleteAll() {
-  const task = document.querySelectorAll('.line');
-//  const task2 = document.getElementsByClassName('line');
-//  const task3 = document.getElementsByTagName('li');
-  const lista = document.querySelector('#lista-tarefas');
-  for (let index = 0; index < task.length; index += 1) {
-    lista.removeChild(task[index]);
-  }
-}//Dúvida
 
-// Requisito 11
-function deleteSelectedClick() {
-  const buttonDeleteSelected = document.getElementById('remover-finalizados');
-  buttonDeleteSelected.addEventListener('click', deleteSelected);
-}
 
-function deleteSelected() {
-  const lista = document.querySelector('#lista-tarefas');
-  const element = document.querySelectorAll('.completed');
-  for (let index = 0; index < element.length; index += 1) {
-    lista.removeChild(element[index]);
-  }
-}
+
+
+
+
