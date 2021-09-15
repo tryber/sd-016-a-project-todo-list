4987964f6011508a8d79eb9bc48816848ec462b0
joao <jgsspinelli92@gmail.com>
2021-09-15 00:49:02 -0300

    meu vscode bugou

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function getItemFromStorage() {
+  const colectedItem = localStorage.getItem('lista1');
+  const parentElement = document.getElementById('lista-tarefas')
+  const array = colectedItem.split(',');
+  const savedItems = document.createElement('li')
+
+  JSON.stringify
+
+  for (let index = 0; index < array.length; index += 1) {
+    savedItems.innerText = array[index];
+    savedItems.className = 'list-item';
+    // savedItems[index].className = 'list-item';
+    parentElement.appendChild(savedItems);
+  }
+  createPin();
+}
+getItemFromStorage();
+
 function completedTasks(doubleClick) {
   doubleClick.target.classList.toggle('completed');
 }
@@ -22,7 +40,8 @@ function createListItem() {
   parentElement.appendChild(listItem);
   input.value = '';
 }
-document.getElementById('criar-tarefa').addEventListener('click', createListItem);
+const btnCreateTask = document.getElementById('criar-tarefa')
+btnCreateTask.addEventListener('click', createListItem);
 
 function eraseAll() {
   const listItems = document.querySelectorAll('.list-item');
@@ -31,7 +50,8 @@ function eraseAll() {
     listItems[index].remove();
   }
 }
-document.getElementById('apaga-tudo').addEventListener('click', eraseAll);
+const btnEraseAll = document.getElementById('apaga-tudo');
+btnEraseAll.addEventListener('click', eraseAll);
 
 function eraseCompleted() {
   const listCompletedItems = document.querySelectorAll('.completed');
@@ -47,4 +67,43 @@ function eraseActive() {
 }
 document.getElementById('remover-selecionado').addEventListener('click', eraseActive)
 
+function storage() {
+  const lista = document.querySelectorAll('.list-item');
+  let key = 'lista1'
+  let array = [];
+  for (let index = 0; index < lista.length; index += 1) {
+    array.push(lista[index].innerHTML)
+  }
+  localStorage.setItem(key, array);
+} 
+document.querySelector('#salvar-tarefas').addEventListener('click', storage)
 
+function createPin() {
+  const pin = document.querySelector('#pin')
+  pin.src = '/pin.png'
+}
+btnCreateTask.addEventListener('click', createPin);
+
+function erasePin() {
+  const pin = document.querySelector('#pin')
+  pin.src = '';
+}
+btnEraseAll.addEventListener('click', erasePin);
+
+function moveUp() {
+  const activeItem = document.querySelector('.active');
+  const elementBefore = document.querySelector('.active').previousElementSibling;
+    
+  activeItem.parentNode.insertBefore(elementBefore, activeItem);
+  elementBefore.parentNode.insertBefore(activeItem, elementBefore);
+}
+document.querySelector('#mover-cima').addEventListener('click', moveUp);
+
+function moveDown() {
+  const activeItem = document.querySelector('.active');
+  const elementNext = document.querySelector('.active').nextElementSibling;
+
+  activeItem.parentNode.insertBefore(activeItem, elementNext);
+  elementNext.parentNode.insertBefore(elementNext, activeItem);
+}
+document.querySelector('#mover-baixo').addEventListener('click', moveDown);
