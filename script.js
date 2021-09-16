ebc9240e812e8054c09e1b5f0074a95de4940380
emmanoel <emmanoelporto@gmail.com>
2021-09-15 21:13:56 -0300

    requisitos 5, 6 e 7

--- script.js
+++ script.js
@@ -1,34 +1,67 @@
-function html1(){
+function html1() {
   // variáveis de elementos Pai dos elementos filhos abaixo.
-  let body = document.querySelector('body');
-  let header = document.querySelector('header');
-  let section = document.querySelector('section');
-  let nav = document.querySelector('nav');
+  const header = document.querySelector('header');
+  const section = document.querySelector('section');
   // cria o titulo.
-  let h1 = document.createElement('h1');
+  const h1 = document.createElement('h1');
   header.appendChild(h1);
   document.querySelector('h1').innerText = 'Minha Lista de Tarefas';
   // cria o paragrafo <p>.
-  let paragrafo = document.createElement('p');
+  const paragrafo = document.createElement('p');
   section.appendChild(paragrafo);
   document.querySelector('p').id = 'funcionamento';
   document.querySelector('p').innerText = 'Clique duas vezes em um item para marcá-lo como completo';
-  // cria o input.
-  let input = document.createElement('input');
-  nav.appendChild(input);
-  document.querySelector('input').id = 'texto-tarefa';
 }
-html1();
 
 function nav() {
-  let nav = document.querySelector('nav');
-  let ol = document.createElement('ol');
+  //constantes Pai.
+  const nav = document.querySelector('nav');
+  const ol = document.createElement('ol');
+  // cria o input.
+  const input = document.createElement('input');
+  nav.appendChild(input);
+  document.querySelector('input').id = 'texto-tarefa';
   // cria lista ordenada.
   nav.appendChild(ol);
   ol.id = 'lista-tarefas';
-  // cria botão.
-  let btn = document.createElement('button');
+  //constantes Filho.
+   const olAcess = document.querySelector('ol');
+   const inputText = document.querySelector('#texto-tarefa');
+  // cria o botão Na Página.
+  const btn = document.createElement('button');
   nav.appendChild(btn);
   document.querySelector('button').id = 'criar-tarefa';
+  document.querySelector('button').innerText = 'Create';
+  // acessa o botão
+  const btnAcess = document.querySelector('#criar-tarefa');
+  // mensagem de erro! 
+  const error = document.createElement('p');
+  nav.appendChild(error);
+  // funçao do botão.
+
+  btnAcess.addEventListener('click', function (){
+   
+    if (inputText.value !== '') {
+      const li = document.createElement('li');
+      li.innerText = inputText.value;
+      li.classList.add('task');
+      olAcess.appendChild(li);
+      inputText.value = '';
+      // limpa mensagem de erro ao criar uma tarefa.
+      document.querySelector('nav p').innerText = ''; 
+
+      const grayList = document.querySelector('li');
+      grayList.addEventListener('click', function(){
+        li.style.backgroundColor = 'gray';
+      });
+
+      } else {
+        error.innerText = 'erro: Digite uma lista de tarefa !';
+        error.style.color = 'red';
+        console.log('erro: Digite uma lista de tarefa !');
+      }
+  });
 }
+
+html1();
 nav();
