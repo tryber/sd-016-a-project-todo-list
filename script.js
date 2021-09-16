6e874f341b7402fceb300bb1a7780fadab51ed0b
Lucas (Junqueira) Lima <lucascavalcantelima21@gmail.com>
2021-09-16 16:04:48 -0300

    Requisito 5

---
+++ script.js
@@ -0,0 +1,13 @@
+function addNewTask () {
+  const button = document.querySelector('#criar-tarefa');
+  const task = document.querySelector('#texto-tarefa');
+  button.addEventListener('click', () => {
+    const taskName = task.value;
+    const list = document.querySelector('#lista-tarefas');
+    newTask = document.createElement('li');
+    list.appendChild(newTask);
+    newTask.innerHTML = taskName;
+    document.querySelector('#texto-tarefa').value = '';
+  });
+}
+addNewTask ();
