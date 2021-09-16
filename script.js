0b657c2d3313bd093325773fc76073b662bdf487
Muller Borges <msbhero@gmail.com>
2021-09-16 18:46:21 -0300

    requisitos 9(de forma parcial) e 10 adicionados

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputText = document.getElementById('texto-tarefa');
 const btnAdd = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-
+const btnDel = document.getElementById('apaga-tudo');
 
 btnAdd.addEventListener('click', function(e) {
     e.preventDefault()
@@ -9,16 +9,33 @@ btnAdd.addEventListener('click', function(e) {
     let item = document.createElement('li');
     item.innerHTML = value;
     item.classList.add('default');
+    item.setAttribute("ondblclick", "toggle (this)")
     taskList.appendChild(item);
     inputText.value = '';
 });
 
 taskList.addEventListener('click', function(event) {
-    let lItem = taskList.children;
-    for (let i = 0; i < lItem.length; i += 1) {
-        lItem[i].className = 'default';
-        event.target.className = 'liGray';
-
+    let listItem = document.querySelector('ol').children;
+    for (let i = 0; i < listItem.length; i += 1) {
+        listItem[i].className = 'default';
+        event.target.className = 'li-Gray';
     }
 
 })
+
+
+//função funciona apenas isolada de taskList.addEventListener, com a anterior ativada esta funciona parcialmente =(
+function toggle(element) {
+    if (element.className == "default" || element.className == "li-Gray") {
+        element.classList.add('completed');
+    } else {
+        element.className = 'default' || element.className == "li-Gray";
+    }
+}
+
+btnDel.addEventListener('click', function() {
+    const listItem = document.querySelector("ol");
+    while (listItem.firstChild) {
+        listItem.removeChild(listItem.lastChild);
+    }
+});
