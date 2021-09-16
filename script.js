d7068e6e5835435026f16afd51ff31d34a79b172
André Luís Wenningkamp <andre.wenningkamp1@gmail.com>
2021-09-15 21:44:53 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const listItems = document.querySelector('#lista-tarefas').children;
 const clearAll = document.querySelector('#apaga-tudo');
+const buttonRemove = document.querySelector('#remover-finalizados');
+
+function removeSelected() {
+  for (let index = 0; index < listItems.length; index += 1) {
+    if (listItems[index].className === 'completed') {
+      listItems[index].remove();
+      index -= 1;
+    }
+  }
+}
+
+buttonRemove.addEventListener('click', removeSelected);
 
 function clear() {
   for (let index = 0; index < listItems.length; index += 1) {
@@ -19,7 +31,7 @@ function changeBgColor(event) {
     listItems[index].style.backgroundColor = 'white';
     if (elementSelected.backgroundColor === 'rgb(128,128,128)') {
       color = 'white';
-      elementSelected.backgroundColor = 'color';
+      elementSelected.backgroundColor = color;
     }
   }
   elementSelected.backgroundColor = color;
