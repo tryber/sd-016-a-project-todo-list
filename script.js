1a7c4d685754f5636ec4f7f78e5e83b6a37a71eb
FernandoMos92 <fernandomosp@gmail.com>
2021-09-15 22:54:28 -0300

    new layout 100% ok

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const main = document.getElementsByTagName('main');
 const inputText = document.getElementById('texto-tarefa');
 const orderedList = document.getElementById('lista-tarefas');
 const btnClear = document.getElementById('apaga-tudo');
-const btnAllClear = document.createElement('remover-finalizados');
+const btnRemoveDone = document.getElementById('remover-finalizados');
 const btnAdd = document.getElementById('criar-tarefa');
-const btnSalveTask = document.createElement('salvar-tarefas');
-const btnRemoveSelected = document.createElement('remover-selecionado');
+const btnSalveTask = document.getElementById('salvar-tarefas');
+const btnRemoveSelected = document.getElementById('remover-selecionado');
 
 const repairItens = JSON.parse(localStorage.getItem('saveList'));
 if (repairItens !== null) {
@@ -17,15 +17,16 @@ if (repairItens !== null) {
     itemList.innerText = el.text;
     itemList.style.backgroundColor = el.style;
     orderedList.appendChild(itemList);
-    console.log(el.className.split(' '));
   });
 }
 
 btnClear.addEventListener('click', () => {
   orderedList.innerHTML = '';
 });
-btnAllClear.addEventListener('click', () => {
-  const findItem = Array.from(main.children[1].children);
+
+// butão limpar completo
+btnRemoveDone.addEventListener('click', () => {
+  const findItem = Array.from(main[0].children[2].children[0].children);
   const newOutput = findItem.filter((el) => el.className !== 'clickItem completed');
   orderedList.innerHTML = '';
   newOutput.forEach((el) => {
@@ -45,9 +46,11 @@ orderedList.addEventListener('click', (event) => {
   const listLi = document.querySelectorAll('li');
   const currentLi = event.target;
   for (let index = 0; index < listLi.length; index += 1) {
-    listLi[index].style.backgroundColor = 'rgb(253 , 77 , 176)';
+    listLi[index].style.backgroundColor = 'rgb(255 , 255 , 165)';
+    listLi[index].transform = 'rotate(20)';
   }
-  currentLi.style.backgroundColor = 'rgb(128,128,128)';
+  currentLi.style.backgroundColor = 'rgb(128, 128, 128)';
+  currentLi.style.transform = 'rotate(0)';
 });
 orderedList.addEventListener('dblclick', (event) => {
   event.target.classList.toggle('completed');
@@ -55,7 +58,7 @@ orderedList.addEventListener('dblclick', (event) => {
 
 // salvar-tarefas
 btnSalveTask.addEventListener('click', () => {
-  const contentList = Array.from(main.children[1].children);
+  const contentList = Array.from(main[0].children[2].children[0].children);
   const containerSave = contentList.map((el) => ({
     text: el.innerText,
     className: el.className,
@@ -66,9 +69,8 @@ btnSalveTask.addEventListener('click', () => {
 
 // remover-selecionado
 btnRemoveSelected.addEventListener('click', () => {
-  const findItem = Array.from(main.children[1].children);
+  const findItem = Array.from(main[0].children[2].children[0].children);
   const newOutput = findItem.filter((el) => el.style.backgroundColor !== 'rgb(128, 128, 128)');
-  console.log(newOutput);
   orderedList.innerHTML = '';
   newOutput.forEach((el) => {
     orderedList.appendChild(el);
