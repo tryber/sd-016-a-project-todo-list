104f59df21ce5bfd195678f63307a51bdcc9a737
raffael marinho <silvamarinho10@gmail.com>
2021-09-16 00:23:14 -0300

    feat: 10

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-
 const listaTarefas = document.querySelector('#lista-tarefas');
-const inputTarefa = document.querySelector("#texto-tarefa");
-const criarTarefaBtn = document.getElementById('criar-tarefa').addEventListener('click', criarTarefa);
+const inputTarefa = document.querySelector('#texto-tarefa');
+const criarTarefaBtn = document
+  .getElementById('criar-tarefa')
+  .addEventListener('click', criarTarefa);
 
-function adicionarRisco(event){
-  const tarefaClicada2x = event.target; 
-  if(tarefaClicada2x.classList.contains("completed")){
-    tarefaClicada2x.classList.remove("completed")
-  }else{
-    tarefaClicada2x.classList.add("completed")
+function adicionarRisco(event) {
+  const tarefaClicada2x = event.target;
+  if (tarefaClicada2x.classList.contains('completed')) {
+    tarefaClicada2x.classList.remove('completed');
+  } else {
+    tarefaClicada2x.classList.add('completed');
   }
 }
 
 function addCor(event) {
   const tarefaClicada = event.target;
-  if(tarefaClicada.classList.contains('corCinza')) {
+  if (tarefaClicada.classList.contains('corCinza')) {
     tarefaClicada.classList.remove('corCinza');
   } else {
     tarefaClicada.classList.add('corCinza');
@@ -22,10 +23,10 @@ function addCor(event) {
 }
 
 function selecionarTarefas() {
-  const tarefaLis = document.getElementsByClassName("ordem");
+  const tarefaLis = document.getElementsByClassName('ordem');
   for (let i = 0; i < tarefaLis.length; i++) {
-    tarefaLis[i].addEventListener("click", addCor);
-    tarefaLis[i].addEventListener("dblclick", adicionarRisco);
+    tarefaLis[i].addEventListener('click', addCor);
+    tarefaLis[i].addEventListener('dblclick', adicionarRisco);
   }
 }
 
@@ -34,8 +35,13 @@ function criarTarefa() {
   tarefa.classList.add('ordem');
   tarefa.innerHTML = inputTarefa.value;
   listaTarefas.appendChild(tarefa);
-  inputTarefa.value = "";
+  inputTarefa.value = '';
   selecionarTarefas();
 }
 
+function removerLista() {
+  listaTarefas.innerHTML = '';
+}
 
+const removerTarefaBtn = document.getElementById('apaga-tudo');
+  removerTarefaBtn.addEventListener('click', removerLista);
