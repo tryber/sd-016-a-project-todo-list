786fe109d8622f95f7726946e181a5b462eeab68
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-09-17 02:38:42 -0300

    Conclui o requisito 10

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 function adicionaTarefa() {
-  const textoTarefa = document.getElementById('texto-tarefa');
   const listaTarefas = document.getElementById('lista-tarefas');
-  const tarefa = document.createElement('li');
-  tarefa.className = 'item';
-  tarefa.innerHTML = textoTarefa.value;
-  listaTarefas.appendChild(tarefa);
-  textoTarefa.value = '';
+  const textoTarefa = document.getElementById('texto-tarefa');
+  if (textoTarefa.value != ''){
+    const tarefa = document.createElement('li');
+    tarefa.className = 'item';
+    tarefa.innerHTML = textoTarefa.value;
+    listaTarefas.appendChild(tarefa);
+    textoTarefa.value = '';
+  }
 }
 
-function checaSelecionados(itens) {
+function checaSelecionados(remover) {
   let corDeFundo;
-  for (let index = 0; index < itens.length; index += 1){
-    corDeFundo = window.getComputedStyle(itens[index], null).getPropertyValue('background-color');
-    if (corDeFundo = 'rgb(128, 128, 128)'){
-      itens[index].style.backgroundColor = '';
+  const itensLista = document.getElementById('lista-tarefas').children;
+  for (let index = 0; index < itensLista.length; index += 1) {
+    corDeFundo = window.getComputedStyle(itensLista[index], null).getPropertyValue('background-color');
+    if (corDeFundo = 'rgb(128, 128, 128)') {
+      if (remover === true) {
+        itensLista[index].style.backgroundColor = '';
+      } else if (remover === false) {
+          return itensLista[index];
+      }
     }
   }
 }
 
+
 function selecionaItem(event) {
-  if (event.target.className === 'item') {
-    const itensLista = document.getElementById('lista-tarefas').children;
-    checaSelecionados(itensLista);
+  if (event.target.classList.contains('item')) {
+    checaSelecionados(true);
     const itemSelecionado = event.target;
     itemSelecionado.style.backgroundColor = 'rgb(128, 128, 128)';
   }
 }
 
+function completaTarefa(event) {
+  if (event.target.classList.contains('item')) {
+    if (event.target.classList.contains('completed')){
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }
+}
+
+function limpaLista() {
+  const listaTarefas = document.getElementById('lista-tarefas');
+  ultimoItem = listaTarefas.lastElementChild;
+  while (ultimoItem) {
+    listaTarefas.removeChild(ultimoItem);
+    ultimoItem = listaTarefas.lastElementChild;
+  } 
+}
+
 window.onload = function () {
   document.getElementById('criar-tarefa').addEventListener('click', adicionaTarefa);
   document.addEventListener('click', selecionaItem);
+  document.addEventListener('dblclick', completaTarefa);
+  document.querySelector('.box').addEventListener('click', function() {checaSelecionados(true);});
+  document.getElementById('apaga-tudo').addEventListener('click', limpaLista);
 }
