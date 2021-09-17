6e7b5f31651330b873f357322b8a8bb0caa037c5
aparecidag365 <mgegoulart365@gmail.com>
2021-09-16 23:47:12 -0300

    Adicionando item 9

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ btnCriarTarefa.addEventListener('click', function () {
   let texto = document.createTextNode(textoTarefa);
   li.appendChild(texto);
   li.innerText = textoTarefa.value;
-  console.log("li "+li)
   document.getElementById("lista-tarefas").appendChild(li);
   listaTarefas = document.querySelector("#lista-tarefas")
   //alterar a cor da linha
-  listaTarefas.addEventListener('click', newColor) 
+  listaTarefas.addEventListener('click', newColor)
+  listaTarefas.addEventListener('dblclick', riskTasK)
   textoTarefa.value =''
   //
 })
 
 function newColor(color){
-  lineTarefas = document.getElementById("lista-tarefas").children
+  let lineTarefas = document.getElementById("lista-tarefas").children
   for (let index = 0; index < lineTarefas.length; index += 1) {
     lineTarefas[index].style.backgroundColor = 'rgba(100, 148, 237, 0.11)';
   }
     color.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
+
+function riskTasK(risk){
+    risk.target.classList.toggle('completed');
+}
+
