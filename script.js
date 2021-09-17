34dec2cac4511ffc7f725513113bf27f879d96fa
Lucas (Junqueira) Lima <lucascavalcantelima21@gmail.com>
2021-09-16 21:01:47 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 // --------------------------------------------------------------------------------------------
-function addNewTask() {
-
-  const button = document.querySelector('#criar-tarefa');
-  const task = document.querySelector('#texto-tarefa');
-  
-  button.addEventListener('click', () => {
-    const taskName = task.value;
-    const list = document.querySelector('#lista-tarefas');
-    const newTask = document.createElement('li');
-    newTask.classList.add('item');
-    list.appendChild(newTask);
-    newTask.innerHTML = taskName;
-    document.querySelector('#texto-tarefa').value = '';
-    clickToChange();  
-  }); 
-}
-
-addNewTask();
+const list = document.querySelector('#lista-tarefas');
 // --------------------------------------------------------------------------------------------
 function changeBGColor(event) {
-  const selectItem = event.target;
   const bgColor = document.getElementsByTagName('li');
   for (let i = 0; i < bgColor.length; i += 1) {
     bgColor[i].style.background = '';
@@ -27,8 +9,27 @@ function changeBGColor(event) {
   event.target.style.background = 'rgb(128, 128, 128)';
 }
 function clickToChange() {
-  const btn = document.querySelectorAll('#lista-tarefas li');
+  const btn = document.querySelectorAll('#lista-tarefas');
   for (let i = 0; i < btn.length; i += 1) {
     btn[i].addEventListener('click', changeBGColor);
   }
 }
+// --------------------------------------------------------------------------------------------
+list.addEventListener('dblclick', (event) => {
+  event.target.classList.toggle('completed');
+});
+// --------------------------------------------------------------------------------------------
+function addNewTask() {
+  const button = document.querySelector('#criar-tarefa');
+  const task = document.querySelector('#texto-tarefa');
+  button.addEventListener('click', () => {
+    const taskName = task.value;
+    const newTask = document.createElement('li');
+    newTask.classList.add('item');
+    list.appendChild(newTask);
+    newTask.innerHTML = taskName;
+    document.querySelector('#texto-tarefa').value = '';
+    clickToChange();
+  });
+}
+addNewTask();
