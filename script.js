93ba496d5898852860a2fe49b34a1b21c51fbb76
Veidz <joaovictorveidz@gmail.com>
2021-09-18 01:06:07 -0300

    14 - Fazendo com que seja possivel excluir somente o item selecionado, e arrumando alguns problemas do lint

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ createParagraphOperation();
 
 // 3 - Adicionando input
 function createTaskInput() {
-  let taskInputSection = document.createElement('section');
-  let taskInput = document.createElement('input');
+  const taskInputSection = document.createElement('section');
+  const taskInput = document.createElement('input');
   taskInputSection.id = 'input-section';
   taskInput.id = 'texto-tarefa';
   document.body.append(taskInputSection);
@@ -32,8 +32,8 @@ createTaskInput();
 
 // 4 - Adicionando uma lista ordenada
 function createOrderedList() {
-  let listSection = document.createElement('section');
-  let orderedList = document.createElement('ol');
+  const listSection = document.createElement('section');
+  const orderedList = document.createElement('ol');
   listSection.id = 'list-section';
   orderedList.id = 'lista-tarefas';
   document.body.append(listSection);
@@ -42,9 +42,9 @@ function createOrderedList() {
 createOrderedList();
 
 // 5 - Criando botao para adicionar tarefas
+const taskInputSection = document.getElementById('input-section');
 function createTaskButton() {
-  let taskInputSection = document.getElementById('input-section');
-  let taskButton = document.createElement('button');
+  const taskButton = document.createElement('button');
   taskButton.id = 'criar-tarefa';
   taskButton.innerText = 'Criar Tarefa';
   taskInputSection.appendChild(taskButton);
@@ -53,14 +53,14 @@ createTaskButton();
 
 // 5 - Fazendo com que as tarefas seja adicionadas
 // 6 - Ordenando por ordem de criacao
-let taskInput = document.getElementById('texto-tarefa');
-let taskButton = document.getElementById('criar-tarefa');
-let orderedList = document.getElementById('lista-tarefas');
+const taskInput = document.getElementById('texto-tarefa');
+const taskButton = document.getElementById('criar-tarefa');
+const orderedList = document.getElementById('lista-tarefas');
 function handleTaskButton() {
   if (taskInput.value === '') {
     alert('Preencha o campo!');
   } else {
-    let listItem = document.createElement('li');
+    const listItem = document.createElement('li');
     listItem.className = 'item-task';
     listItem.innerText = taskInput.value;
     orderedList.appendChild(listItem);
@@ -72,7 +72,7 @@ taskButton.addEventListener('click', handleTaskButton);
 // 7 - Alterando a cor do fundo do item selecionado
 // 8 - Fazendo com que somente um item fique selecionado
 function changeBgColorItemTask(event) {
-  let listItem = document.getElementsByClassName('item-task');
+  const listItem = document.getElementsByClassName('item-task');
 
   for (let i = 0; i < listItem.length; i += 1) {
     listItem[i].classList.remove('selected');
@@ -94,8 +94,7 @@ orderedList.addEventListener('dblclick', completedTask);
 
 // 10 - Criando o botao que sera usado na proxima funcao para apagar todos os itens
 function createDeleteTasksButton() {
-  let taskInputSection = document.getElementById('input-section');
-  let deleteTasksButton = document.createElement('button');
+  const deleteTasksButton = document.createElement('button');
 
   deleteTasksButton.id = 'apaga-tudo';
   deleteTasksButton.innerText = 'Apagar Tudo';
@@ -104,8 +103,8 @@ function createDeleteTasksButton() {
 createDeleteTasksButton();
 
 // 10 - Fazendo com o que o botao apague todos os itens da lista
-let deleteTasksButton = document.getElementById('apaga-tudo');
-function deleteAllTasks() {  
+const deleteTasksButton = document.getElementById('apaga-tudo');
+function deleteAllTasks() {
   // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
   // Utilizando while para verificar enquanto a lista estiver um elemento, ele ira remover (ultimo), ate a lista ficar vazia
   while (orderedList.lastElementChild) {
@@ -116,8 +115,7 @@ deleteTasksButton.addEventListener('click', deleteAllTasks);
 
 // 11 - Criando o botao que sera usado na proxima funcao para apagar todos os itens completados
 function createDeleteTasksComplete() {
-  let taskInputSection = document.getElementById('input-section');
-  let deleteCompletedTasksButton = document.createElement('button');
+  const deleteCompletedTasksButton = document.createElement('button');
   deleteCompletedTasksButton.id = 'remover-finalizados';
   deleteCompletedTasksButton.innerText = 'Apagar Completados';
 
@@ -126,9 +124,9 @@ function createDeleteTasksComplete() {
 createDeleteTasksComplete();
 
 // 11 - Apagando os itens marcados como completo
-let deleteCompletedTasksButton = document.getElementById('remover-finalizados');
+const deleteCompletedTasksButton = document.getElementById('remover-finalizados');
 function deleteCompletedTasks() {
-  let completedTasks = orderedList.querySelectorAll('.completed')
+  const completedTasks = orderedList.querySelectorAll('.completed');
 
   for (let i = 0; i < completedTasks.length; i += 1) {
     completedTasks[i].remove();
@@ -139,8 +137,7 @@ deleteCompletedTasksButton.addEventListener('click', deleteCompletedTasks);
 // BÔNUS
 // 11 - Criando botao que sera usado para salvar
 function createSaveTasksButton() {
-  let taskInputSection = document.getElementById('input-section');
-  let saveTasksButton = document.createElement('button');
+  const saveTasksButton = document.createElement('button');
   saveTasksButton.id = 'salvar-tarefas';
   saveTasksButton.innerText = 'Salvar';
 
@@ -149,46 +146,32 @@ function createSaveTasksButton() {
 createSaveTasksButton();
 
 // 11 - Salvando o conteudo da litsta (inner html das 'li') no local storage
-let saveTasksButton = document.getElementById('salvar-tarefas');
+const saveTasksButton = document.getElementById('salvar-tarefas');
 function saveTasks() {
-  let listItem = document.querySelectorAll('li');
+  const listItem = document.querySelectorAll('li');
   const tasks = [];
-
-  listItem.forEach(listItem => {
+  listItem.forEach((item) => {
     tasks.push({
-      text: listItem.innerText,
-      completed: listItem.classList.contains('completed')
-    })
-  })
-
+      text: item.innerText,
+      completed: item.classList.contains('completed'),
+    });
+  });
   localStorage.setItem('Task', JSON.stringify(tasks));
 }
 saveTasksButton.addEventListener('click', saveTasks);
 
 // 11 - Carregando o conteudo do localstorage
 const tasks = JSON.parse(localStorage.getItem('Task'));
-let taskKeys = [];
-let stringTaskKeys = '';
-function handleLoadTasks() {
-  if (tasks === null) return;
-
-  for (let i = 0; i < tasks.length; i += 1) {
-    taskKeys.push(tasks[i]);
-  }
-  stringTaskKeys = JSON.stringify(taskKeys);
-
-  loadTask();
-}
-
+const taskKeys = [];
 function loadTask() {
   for (let i = 0; i < taskKeys.length; i += 1) {
     if (taskKeys[i].completed === true) {
-      let listItem = document.createElement('li');
+      const listItem = document.createElement('li');
       listItem.className = 'item-task completed';
       listItem.innerText = taskKeys[i].text;
       orderedList.appendChild(listItem);
     } else {
-      let listItem = document.createElement('li');
+      const listItem = document.createElement('li');
       listItem.className = 'item-task';
       listItem.innerText = taskKeys[i].text;
       orderedList.appendChild(listItem);
@@ -196,36 +179,40 @@ function loadTask() {
   }
 }
 
+function handleLoadTasks() {
+  if (tasks === null) return;
+  for (let i = 0; i < tasks.length; i += 1) {
+    taskKeys.push(tasks[i]);
+  }
+  loadTask();
+}
+
 // 13 - Criando botoes para mover para cima e baixo
 function createMovesButton() {
-  let taskInputSection = document.getElementById('input-section');
-  let moveUpButton = document.createElement('button');
-  let moveDownButton = document.createElement('button');
-
+  const moveUpButton = document.createElement('button');
+  const moveDownButton = document.createElement('button');
   moveUpButton.id = 'mover-cima';
   moveUpButton.innerText = 'Mover Para Cima';
-
   moveDownButton.id = 'mover-baixo';
   moveDownButton.innerText = 'Mover Para Baixo';
-
   taskInputSection.appendChild(moveUpButton);
-  taskInputSection.appendChild(moveDownButton); 
+  taskInputSection.appendChild(moveDownButton);
 }
 createMovesButton();
 
 // 13 - Movimentando o item selecionado para cima ou para baixo
-let moveUpButton = document.getElementById('mover-cima');
-let moveDownButton = document.getElementById('mover-baixo');
+const moveUpButton = document.getElementById('mover-cima');
+const moveDownButton = document.getElementById('mover-baixo');
 function moveTaskUp() {
-  let listItem = document.querySelectorAll('li');
-  let taskSelected = document.querySelector('.selected');
+  const listItem = document.querySelectorAll('li');
+  const taskSelected = document.querySelector('.selected');
 
-  if (listItem[0] === taskSelected || listItem[length - 1] === taskSelected) return;
+  if (orderedList.firstChild === taskSelected) return;
 
   for (let i = 0; i < listItem.length; i += 1) {
     if (listItem[i] === taskSelected) {
-      let itemTask = listItem[i].innerText;
-      let auxItemTask = listItem[i - 1].innerText;
+      const itemTask = listItem[i].innerText;
+      const auxItemTask = listItem[i - 1].innerText;
 
       listItem[i - 1].innerText = itemTask;
       listItem[i].innerText = auxItemTask;
@@ -237,15 +224,15 @@ function moveTaskUp() {
 }
 
 function moveTaskDown() {
-  let listItem = document.querySelectorAll('li');
-  let taskSelected = document.querySelector('.selected');
+  const listItem = document.querySelectorAll('li');
+  const taskSelected = document.querySelector('.selected');
 
-  if (listItem[length + 1] === taskSelected) return;
+  if (orderedList.lastChild === taskSelected) return;
 
   for (let i = 0; i < listItem.length; i += 1) {
     if (listItem[i] === taskSelected) {
-      let itemTask = listItem[i].innerText;
-      let auxItemTask = listItem[i + 1].innerText;
+      const itemTask = listItem[i].innerText;
+      const auxItemTask = listItem[i + 1].innerText;
 
       listItem[i + 1].innerText = itemTask;
       listItem[i].innerText = auxItemTask;
@@ -258,6 +245,25 @@ function moveTaskDown() {
 
 moveUpButton.addEventListener('click', moveTaskUp);
 moveDownButton.addEventListener('click', moveTaskDown);
+
+// 14 - Criando os botoes para a proxima funcao
+function createDeleteSelectedTaskButton() {
+  const deleteSelectedTaskButton = document.createElement('button');
+
+  deleteSelectedTaskButton.id = 'remover-selecionado';
+  deleteSelectedTaskButton.innerText = 'Apagar Selecionado';
+
+  taskInputSection.appendChild(deleteSelectedTaskButton);
+}
+createDeleteSelectedTaskButton();
+
+// 14 - Apagando somente o item selecionado
+const deleteSelectedTaskButton = document.getElementById('remover-selecionado');
+function deleteSelectedTask() {
+  const selectedTask = orderedList.querySelector('.selected');
+  selectedTask.remove();
+}
+deleteSelectedTaskButton.addEventListener('click', deleteSelectedTask);
 
 window.onload = () => {
   handleLoadTasks();
