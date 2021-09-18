d0fda7a34e3bbe3c4b6ac031c8c6acfb400ba84d
Angelica <angelicapedroso05@gmail.com>
2021-09-18 15:46:29 -0300

    add null no lugar de white

--- script.js
+++ script.js
@@ -12,11 +12,9 @@ button.addEventListener('click', addTask);
 
 function colorItemList(event) {
   const item = document.querySelectorAll('li')
-
   for (let index = 0; index < item.length; index += 1) {
-    item[index].style.backgroundColor = 'white';
+    item[index].style.backgroundColor = null;
   }
-
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 document.getElementById('lista-tarefas').addEventListener('click', colorItemList);
