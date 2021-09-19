25c9093f4c6997721affea1231e731c3ac0631bd
jonmanhaes <jonatasjonasjonat@gmail.com>
2021-09-19 18:28:28 -0300

    Corrige desafio 1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// commit inicial
+let getButton = document.getElementById('criar-tarefa')
+let getField = document.getElementById('texo-tarefa')
+let getList = document.getElementById('lista-tarefas')
+
+
+function createTask() {
+  const orderList = document.querySelector('#lista-tarefas');
+  const getInput = document.querySelector('#texto-tarefa');
+  const itemList = document.createElement('li');
+  itemList.innerText = getInput.value;
+  itemList.className = 'task';
+  orderList.appendChild(itemList);
+  getInput.value = '';
+}
