1b843102dfff45225761861ad6ec7ed313c192b6
gemaquejr <gemaquejr@hotmail.com>
2021-09-19 00:14:13 -0300

    Adiciona requisito 8 com ajuda do Brunao e Emerson Moreira

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const theButton = document.querySelector('.theButton');
 const tasklist = document.querySelector('.lista-tarefas');
 const text = document.querySelector('.insert');
+
 function formulary() {
   const list = document.createElement('li');
   list.innerHTML = text.value;
+  list.classList.add('selected');
   tasklist.appendChild(list);
   text.value = '';
 }
 
 theButton.addEventListener('click', formulary);
 
-function bgList(Event) {
-  Event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+function bgList(event) {
+  const e = event.target;
+  e.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 tasklist.addEventListener('click', bgList);
+
+function selectedTask(event) {
+  const eventTargetSelect = event.target;
+
+  const checkStyle = document.querySelectorAll('li');
+  for (let i = 0; i < checkStyle.length; i += 1) {
+    checkStyle[i].style.backgroundColor = '';
+  }
+  eventTargetSelect.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+tasklist.addEventListener('click', selectedTask);
