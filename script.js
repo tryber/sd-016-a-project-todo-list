571fb3f813a10f67ca6f50f060e5cb202022c569
Meiryland Melo <meirylandmelo@gmail.com>
2021-09-19 23:40:27 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const buttonCreateTask = document.getElementById('criar-tarefa');
 const inputItemList = document.getElementById('texto-tarefa');
 const addList = document.getElementById('lista-tarefas');
 
-function criateTask() {
+function itemSelected(e) {
+  const selected = document.querySelector('.selected');
+  if (selected) {
+    selected.classList.remove('selected');
+  }
+  e.currentTarget.classList.add('selected');
+}
+
+function createTask() {
   const itemList = document.createElement('li');
   itemList.innerText = inputItemList.value;
   itemList.className = 'task';
+  itemList.addEventListener('click', itemSelected);
   addList.appendChild(itemList);
   inputItemList.value = '';
 }
 
-buttonCreateTask.addEventListener('click', criateTask);
+buttonCreateTask.addEventListener('click', createTask);
