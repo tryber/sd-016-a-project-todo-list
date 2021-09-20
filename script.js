24ba02d0bf8001626d68238e12462acc85b741b7
Leonardo_Nunes <leonardokarate95@hotmail.com>
2021-09-20 09:20:18 -0300

    adciona função apagaItem

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let button = document.getElementsByTagName("button")[0];
 let input = document.getElementById("texto-tarefa");
 let tabela = document.getElementById("lista-tarefas");
-let listaDeTarefas = document.getElementsByTagName("li");
+let listaDeTarefas = document.getElementsByClassName("lista");
+let apagar = document.getElementById("apaga-tudo");
 
-console.log(button);
+apagar.addEventListener("click", apagarLista);
 button.addEventListener("click", adicionaItem);
 function adicionaItem(event) {
   let texto = input.value;
   itemDaLista = document.createElement("li");
   itemDaLista.innerText = texto;
+  itemDaLista.classList.add("lista")
   tabela.appendChild(itemDaLista);
   input.value = "";
   itemDaLista.addEventListener("click", selecionaItem);
@@ -19,4 +21,14 @@ function selecionaItem(event) {
     listaDeTarefas[i].classList.remove("selected");
   }
   event.target.classList.add("selected")
+  console.log(listaDeTarefas.length);
 }
+function apagarLista(event) {
+  for (let i = 0; i < listaDeTarefas.length; i += 0) {
+    let element = listaDeTarefas[i];
+    tabela.removeChild(element);
+    if (listaDeTarefas.length === 0){
+      i+=1;
+    }
+  }
+} 
