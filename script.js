723bdaf2a1d4a4e92fddd30c86bc0daf477f4c69
Eric <ericvinicius81@yahoo.com.br>
2021-09-20 09:58:10 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const button = document.querySelector('#criar-tarefa');
 button.addEventListener('click', () => {
   const input = document.querySelector('#texto-tarefa');
-  const list = document.querySelector('#lista-tarefas');
   const task = document.createElement('li');
-  task.innerHTML = input.value;
-  list.appendChild(task);
+  // https://www.w3schools.com/jsref/met_document_createtextnode.asp
+  const texto = document.createTextNode(input);
+  task.appendChild(texto);
+  task.innerText = input.value;
+  document.querySelector('#lista-tarefas').appendChild(task);
+  const list = document.querySelector('#lista-tarefas');
+  list.addEventListener('click', changeBg);
   input.value = '';
-  task.addEventListener('click', (event) => {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
 });
+
+function changeBg(color) {
+  const line = document.querySelector('#lista-tarefas').children;
+  for (let index = 0; index < line.length; index += 1) {
+    line[index].style.backgroundColor = 'white';
+  }
+  color.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
