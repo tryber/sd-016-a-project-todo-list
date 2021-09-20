684862ebda52bfe2c0cf6ab1159014b29b88e691
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2021-09-19 21:17:36 -0300

    Requisito 10 finalizado.

--- script.js
+++ script.js
@@ -9,22 +9,41 @@ function changeText2() {
     entry.appendChild(document.createTextNode(input));
     list.appendChild(entry);
     document.getElementById('texto-tarefa').value = "" ;
-  
+    
     addBackgroundColor();
+    completed();
 }
 
 let selecionado = document.getElementById("lista-tarefas");
 
+
 function addBackgroundColor () {
-    
     for (let i = 0; i < list.childNodes.length; i += 1) {
-        list.childNodes[i].addEventListener('click', function (e) {
+        list.childNodes[i].addEventListener('click', function () {
             selecionado.classList.remove('color');
-            list.childNodes[i].className = 'color';
+            list.childNodes[i].classList.add ('color');
             selecionado = list.childNodes[i]
         });
-} }
+    }
+}
 
 
+function completed (){
+    for (let i = 0; i < list.childNodes.length; i += 1) {
+    list.childNodes[i].addEventListener('dblclick', function () {
+        list.childNodes[i].classList.toggle('completed');
+        console.log(list.childNodes[i].classList)     
+    });   
+}
+}
+
+function clearButton (){
+
+while (list.firstChild){
+    list.removeChild(list.lastChild);
+}
+}
+
+const clearBtn = document.querySelector('#apaga-tudo');
 
 
