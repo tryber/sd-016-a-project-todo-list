57a1df3413f721db3534f42aafd3d1c40b09c56f
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-09-20 10:32:36 -0400

    add #7

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
+function UpdateBackgroundColor(event) {
+  console.log(event.target);
+  event.target.classList.toggle('selected');
+}
+
 function AddTask() {
   const task = document.createElement('li');
   task.innerText = document.querySelector('#texto-tarefa').value;
-  document.querySelector('#texto-tarefa').value = '';
+  task.addEventListener('click', UpdateBackgroundColor);
   list.appendChild(task);
+
+  document.querySelector('#texto-tarefa').value = '';
 }
 
 button.addEventListener('click', AddTask);
