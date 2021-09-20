791480a999de5ef8f05eec35fdf2a0a1c4219ff2
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-09-20 10:47:00 -0400

    add #9

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+/* eslint-disable prefer-destructuring */
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
 function UpdateBackgroundColor(event) {
   const targetClassList = event.target.classList;
-  const previousSelected = document.querySelector('.selected');
   if (targetClassList.contains('selected')) return;
+
+  const previousSelected = document.querySelector('.selected');
   if (previousSelected !== null) previousSelected.classList.toggle('selected');
+
   targetClassList.toggle('selected');
+}
+
+function UpdateBackgroundStatus(event) {
+  event.target.classList.toggle('completed');
 }
 
 function AddTask() {
   const task = document.createElement('li');
+
   task.innerText = document.querySelector('#texto-tarefa').value;
   task.addEventListener('click', UpdateBackgroundColor);
+  task.addEventListener('dblclick', UpdateBackgroundStatus);
   list.appendChild(task);
 
   document.querySelector('#texto-tarefa').value = '';
