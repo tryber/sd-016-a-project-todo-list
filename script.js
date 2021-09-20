ad9f0d25dfd4483e1c0c45630240e60bee7b443d
Eduardo <eddpmstudio@gmail.com>
2021-09-20 19:20:48 -0300

    completa o exercicio 5

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const teste = "hello"
-console.log('teste');
+
+const button = document.querySelector('#criar-tarefa')
+button.addEventListener('click', (event) => {
+  const tarefa = document.getElementById('texto-tarefa').value;
+  let lista = document.getElementById('lista-tarefa')
+  let item = document.createElement('li')
+  item.innerHTML = tarefa
+  lista.appendChild(item)
+});
