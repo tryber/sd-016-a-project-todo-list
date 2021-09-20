ea3cb1e35f29657236459cacebc1548df27f46bd
Leonardo_Nunes <leonardokarate95@hotmail.com>
2021-09-20 08:32:24 -0300

    adiciona funação selecionaItem

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 let button = document.getElementsByTagName("button")[0];
 let input = document.getElementById("texto-tarefa");
-let tabela = document.getElementById("lista-tarefas")
+let tabela = document.getElementById("lista-tarefas");
+let listaDeTarefas = document.getElementsByTagName("li");
+
 console.log(button);
 button.addEventListener("click", adicionaItem);
 function adicionaItem(event) {
   let texto = input.value;
-  let listaDeTarefas = document.createElement("li");
-  listaDeTarefas.innerText = texto;
-  tabela.appendChild(listaDeTarefas);
-  listaDeTarefas.addEventListener("click", () => {
-    if (listaDeTarefas.style.backgroundColor == 'rgb(0, 0, 0)') {
-      listaDeTarefas.style.backgroundColor = 'rgb(128, 128, 128)';
-    }
-    else {
-      listaDeTarefas.style.backgroundColor == 'rgb(0, 0, 0)'
-    }
-  })
+  itemDaLista = document.createElement("li");
+  itemDaLista.innerText = texto;
+  tabela.appendChild(itemDaLista);
   input.value = "";
+  itemDaLista.addEventListener("click", selecionaItem);
+
+}
+function selecionaItem(event) {
+  for (let i = 0; i < listaDeTarefas.length; i +=1){
+    listaDeTarefas[i].classList.remove("selected");
+  }
+  event.target.classList.add("selected")
 }
