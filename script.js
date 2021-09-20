8444868ff114447ab570f3bf77633d08bf682b05
Laura <lahfumagalli@gmail.com>
2021-09-20 12:38:53 -0300

    requisito 12

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ newButon.id = 'apaga-tudo'
 document.body.appendChild(newButon)
 document.querySelector('#apaga-tudo').addEventListener('click', clearTasks);
 
-function clearTasks () {
+function clearTasks() {
   const listItems = document.querySelectorAll('li');
-  for (let index = 0; index < listItems.length; index += 1){
+  for (let index = 0; index < listItems.length; index += 1) {
     document.querySelector('#lista-tarefas').removeChild(listItems[index])
   }
 }
@@ -53,19 +53,79 @@ buttonNew.innerText = 'remove finalizados';
 document.body.appendChild(buttonNew)
 
 
-document.getElementById('remover-finalizados').addEventListener('click', function (event){
+document.getElementById('remover-finalizados').addEventListener('click', removeCompletedTasks)
+
+function removeCompletedTasks (event) {
   const listItems = document.querySelectorAll('li');
-  for (let index = 0; index < listItems.length; index += 1){
+  for (let index = 0; index < listItems.length; index += 1) {
     if (listItems[index].className === "completed") {
       listItems[index].remove(); // Danielle Silva tribo B e Julia Barcelos
     }
   }
-})
+}
 
-// - Adicione um botão com id="remover-finalizados" que quando clicado remove somente os elementos finalizados da sua lista
+// function reloadPag() {
+//   let reloadList = localStorage.getItem('newList');
+//   let newOl = document.querySelector('ol');
+//   let splitList = reloadList.split('\n');
+//   for (let key in splitList) {
+//     let newItemReload = document.createElement('li');
+//     newItemReload.innerText = splitList[key];
+//     newOl.appendChild(newItemReload);
+//   }
+// }
+
+// ajudada por JULIA BARCELOS <3 <3 <3 E João Veidz
+let inputList = document.getElementById('lista-tarefas')
+
+function reloadPag () {
+  const arrayLi = localStorage.getItem('newList');
+  inputList.innerHTML =  JSON.parse(localStorage.getItem('newList')) // transforma string para objeto
+  removeCompletedTasks()
+}
+
+function saveTasks() {
+  
+  ;
+  localStorage.setItem('newList', JSON.stringify(inputList.innerHTML)); // transforma de objeto para string
+  // reloadPag()
+  // let wtf = JSON.stringify(inputList.innerHTML)
+  // console.log(JSON.parse(wtf))
+  // console.log(document.getElementById('lista-tarefas'))
+}
+
+function createButtonSaveTasks (){
+let buttonSaveTasks = document.createElement('button')
+buttonSaveTasks.id = 'salvar-tarefas';
+buttonSaveTasks.innerText = 'salvar tarefas';
+document.body.appendChild(buttonSaveTasks)
+}
+createButtonSaveTasks()
+
+document.querySelector('#salvar-tarefas').addEventListener('click', saveTasks)
+window.onload = function(){
+  reloadPag()
+}
+
+
+
+// - Adicione um botão com id="salvar-tarefas" que salve o conteúdo da lista. Se você fechar e reabrir a página, a lista deve continuar no estado em que estava
 // O que será verificado:
 
-// Será verificado que existe um elemento button com o id remover-finalizados
+// Será verificado que existe um elemento button com o id salvar-tarefas
 
-// Será verificado que, ao clicar no botão, todos os elementos marcados como feitos são removidos da lista
+// Será verificado que, quando a lista tiver vários elementos, alguns dos quais marcados como finalizados, um recarregamento da página mantém a lista exatamente como está.
 
+// function saveTasks() {
+//   let newOl = document.querySelector('#lista-tarefas')
+//   localStorage.setItem('taskListStorage', JSON.stringify(newOl.innerHTML));
+// }
+// let saveTask = document.querySelector('#salvar-tarefas');
+// saveTask.addEventListener('click', saveTasks);
+
+// window.onload = () => {
+//   // if (localStorage !== null) {
+//     const taskHTML = JSON.parse(localStorage.getItem('taskListStorage'));
+//     taskList.innerHTML = taskHTML;
+//   // }
+// };
