765649239d42d3e159052fa6a60daada6929f59b
Zaqueu <zaqueufernandes@gmail.com>
2021-09-20 11:53:20 -0300

    Adiciona os requisitos do 1 ao 9

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function addItemList()
+function selectItemLista(event) { /* requisito 7 e 8*/
+  /* requisito 7 */
+  let selectItemList = event.target
+  let itemSelecionado = document.querySelector('.Selecionado');
+  if (itemSelecionado !== null) {
+    itemSelecionado.classList.remove('Selecionado');
+  } 
+  selectItemList.classList.add('Selecionado');
+  
+}
+
+function completedTarefa(event) {  /* requisito 9*/
+let completedItem = event.target
+if (completedItem.className.match('completed') === null) {
+completedItem.classList.add('completed');
+} else {
+  completedItem.classList.remove('completed');
+}
+}
+
+function addItemList() {
+  let imputValue = document.querySelector('#texto-tarefa').value;
+  let itemDaLista = document.createElement('li');
+  itemDaLista.innerText = imputValue;
+  let lista = document.querySelector('#lista-tarefas');
+  lista.appendChild(itemDaLista);
+  document.querySelector('#texto-tarefa').value = "";
+  itemDaLista.addEventListener('click', selectItemLista); /* requisito 7 e 8*/
+
+  itemDaLista.addEventListener('dblclick', completedTarefa);/* requisito 9*/
+}
 
 let addButton = document.querySelector("#criar-tarefa"); // Seleciona o Botão
 
-addButton.addEventListener('click', function(){
-let addInput = document.querySelector('#texto-tarefa').value; // Acessa o valor digitado no input
-// console.log(addInput);
-
-})
+addButton.addEventListener('click', addItemList);
