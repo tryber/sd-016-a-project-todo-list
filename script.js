044ec6a8216a53c75c7d051f1224aaf08703da9c
Eduardo <eddpmstudio@gmail.com>
2021-09-20 23:08:22 -0300

    melhora a avaliaçao do lint

--- script.js
+++ script.js
@@ -3,6 +3,41 @@ const button = d.querySelector('#criar-tarefa');
 const lista = d.getElementById('lista-tarefas');
 let itens = null
 
+function disselectLi() {
+  for (let index = 0; index < itens.length; index += 1) {
+    itens[index].classList.remove('tarefa');
+  }
+};
+
+function selectLi() {
+  for (let index = 0; index < itens.length; index += 1) {
+    itens[index].addEventListener('click', (event) => {
+      const evento = event.target
+      disselectLi();
+      evento.classList.add('tarefa');
+    });
+  }
+}
+
+function unmarkLi() {
+  for (let index = 0; index < itens.length; index += 1) {
+    itens[index].addEventListener('dblclick', (event) => {
+      const evento = event.target;
+      evento.classList.remove('completed');
+    });
+  }
+}
+
+function markLi() {
+  for (let index = 0; index < itens.length; index += 1) {
+    itens[index].addEventListener('dblclick', (event) => {
+      const evento = event.target;
+      evento.classList.add('completed');
+      unmarkLi();
+    });
+  }
+}
+
 button.addEventListener('click', () => {
   const tarefa = d.getElementById('texto-tarefa').value;
   const item = d.createElement('li');
@@ -13,38 +48,3 @@ button.addEventListener('click', () => {
   selectLi();
   markLi();
 });
-
-function selectLi() {
-  for (let index = 0; index < itens.length; index += 1) {
-    itens[index].addEventListener('click', (event) => {
-    const evento = event.target
-    disselectLi();
-    evento.classList.add('tarefa');
-    })
-  }
-};
-
-function disselectLi() {
-  for (let index = 0; index < itens.length; index += 1) {
-    itens[index].classList.remove('tarefa');
-  }
-};
-
-function markLi() {
-  for (let index = 0; index < itens.length; index += 1) {
-    itens[index].addEventListener('dblclick', (event) => {
-    const evento = event.target
-    evento.classList.add('completed');
-    unmarkLi();
-    })
-  }
-};
-
-function unmarkLi() {
-  for (let index = 0; index < itens.length; index += 1) {
-    itens[index].addEventListener('dblclick', (event) => {
-    const evento = event.target
-    evento.classList.remove('completed');
-    })
-  }
-};
