6803bf4ab707b3f17c02c7494a579daccafe54e4
cristianoterra <cristianoterr@gmail.com>
2021-09-21 12:19:52 -0300

    add botao id='remover-finalizados'

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ document.querySelector('#apaga-tudo').addEventListener('click', () => {
   const deleteOl = document.querySelector('#lista-tarefas');
   deleteOl.innerHTML = '';
 });
+
+document.querySelector('#remover-finalizados').addEventListener('click', () => {
+  const deleteLi = document.querySelector('#lista-tarefas');
+  const completedLi = document.querySelectorAll('.completed');
+  for (let i = 0; i < completedLi.length; i += 1) {
+    deleteLi.removeChild(completedLi[i]);
+  }
+});
