ae71722f460e84fe233f2b1ca22c55aed794a427
Christian Lessa <christianclessa@gmail.com>
2021-09-21 13:01:11 -0300

    Requisito 13 depois do 14, concluido

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const buttonCreatTarefa = document.querySelector('#criar-tarefa');
 const listOrder = document.querySelector('#lista-tarefas');
 const buttonRemoveSelectedList = document.getElementById('remover-selecionado');
-// const buttonUp = document.getElementById('mover-cima');
-// const buttonDown = document.getElementById('mover-baixo');
+const buttonUp = document.getElementById('mover-cima');
+const buttonDown = document.getElementById('mover-baixo');
 // const selectLi = document.querySelectorAll('li');
 
 function inputOfButton() {
@@ -57,15 +57,6 @@ function clearListRiscados() {
 
 buttonClearListRiscados.addEventListener('click', clearListRiscados);
 
-// function buttonUpList(event) {
-//   const currentEvent = event.target;
-//   const selectLi = document.querySelectorAll('li');
-//   for (let index = 0; index < selectLi.length; index += 1) {
-//     listOrder.insertBefore(currentEvent, selectLi[index]);
-//   }
-// }
-// listOrder.addEventListener('click', buttonUpList);
-
 // Remove e cria class 'selected' no item da lista selecionado.
 function selectedList(event) {
   const selectLi = document.querySelectorAll('li');
@@ -79,8 +70,30 @@ listOrder.addEventListener('click', selectedList);
 
 // Função ao botão 'Apagar Selecionado' de remover o item selecionado da lista.
 function removeSelectList() {
-  const itemSelectedList = document.querySelector('.selected');
-  listOrder.removeChild(itemSelectedList);
+  const selected = document.querySelector('.selected');
+  listOrder.removeChild(selected);
 }
 
 buttonRemoveSelectedList.addEventListener('click', removeSelectList);
+
+// Cria evento ao botão Cima, para subir item da lista.
+// Concluido com auxilio de Fernando Mós.
+function buttonUpList() {
+  const selected = document.querySelector('.selected');
+  if (selected !== null && selected !== listOrder.firstChild) {
+    listOrder.insertBefore(selected, selected.previousSibling);
+  }
+}
+
+buttonUp.addEventListener('click', buttonUpList);
+
+// Cria evento ao botão Baixo, para descer item da lista.
+// Concluido com auxilio de Fernando Mós.
+function buttonDownList() {
+  const selected = document.querySelector('.selected');
+  if (selected !== null && selected !== listOrder.lastChild) {
+    listOrder.insertBefore(selected.nextSibling, selected);
+  }
+}
+
+buttonDown.addEventListener('click', buttonDownList);
