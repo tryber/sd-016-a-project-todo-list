4e1bde841b1240df414d2c629885270c10032eaf
Eduardo <eddpmstudio@gmail.com>
2021-09-20 22:14:11 -0300

    cumpre o requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const d = document;
+const button = d.querySelector('#criar-tarefa');
+const lista = d.getElementById('lista-tarefas');
+let itens = null
 
-const button = document.querySelector('#criar-tarefa')
-button.addEventListener('click', (event) => {
-  let tarefa = document.getElementById('texto-tarefa').value;
-  let lista = document.getElementById('lista-tarefas')
-  let item = document.createElement('li')
-  item.innerHTML = tarefa
-  lista.appendChild(item)
-  document.getElementById('texto-tarefa').value = ""
+button.addEventListener('click', () => {
+  const tarefa = d.getElementById('texto-tarefa').value;
+  const item = d.createElement('li');
+  item.innerHTML = tarefa;
+  lista.appendChild(item);
+  d.getElementById('texto-tarefa').value = '';
+  itens = d.getElementsByTagName('li');
+  teste()
 });
+
+function teste() {
+for (let index = 0; index < itens.length; index += 1) {
+  itens[index].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    })
+  }
+}; 
