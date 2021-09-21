5c6ba55b6ad9ffba4e36ca3ef0693673bb2dc7a6
Edgar Amaral <edgar@Eddiexx86.com>
2021-09-21 13:28:17 -0300

    adiciona os requisitos 8, 9, 10 e 11

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+/* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable eqeqeq */
 /* eslint-disable max-lines-per-function */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
 /* eslint-disable no-unused-vars */
+
 const toDoList = document.querySelector('#lista-tarefas');
 const btnCreateItem = document.querySelector('#criar-tarefa');
 const textTask = document.querySelector('#texto-tarefa');
@@ -39,3 +43,108 @@ btnClearCompleted.addEventListener('click', () => {
     }
   }
 });
+
+// Ao clicar botão remove todos os itens da lista
+btnClearAll.addEventListener('click', () => {
+  const listItem = document.querySelectorAll('.list-item');
+
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItem[index].remove();
+  }
+});
+
+btnSaveTasks.addEventListener('click', () => {
+  const taskContents = [];
+  const taskClasses = [];
+  const tasks = document.querySelectorAll('#lista-tarefas li');
+
+  for (let index = 0; index < tasks.length; index += 1) {
+    taskContents.push(tasks[index].textContent);
+    taskClasses.push(tasks[index].className);
+  }
+
+  localStorage.setItem('taskContents', JSON.stringify(taskContents));
+  localStorage.setItem('taskClasses', JSON.stringify(taskClasses));
+});
+
+btnMoveUp.addEventListener('click', () => {
+  const listItem = document.querySelectorAll('.list-item');
+
+  for (let index = 0; index < listItem.length; index += 1) {
+    const isSelectedItem = listItem[index].classList.contains('selected');
+    const isValidMovement = (index - 1 >= 0);
+
+    if (isSelectedItem && isValidMovement) {
+      const selectedItem = listItem[index];
+      const previousElement = listItem[index - 1];
+      let auxiliar;
+
+      auxiliar = previousElement.textContent;
+      previousElement.innerHTML = selectedItem.textContent;
+      selectedItem.innerHTML = auxiliar;
+
+      auxiliar = previousElement.className;
+      previousElement.className = selectedItem.className;
+      selectedItem.className = auxiliar;
+    }
+  }
+});
+
+btnMoveDown.addEventListener('click', () => {
+  const listItem = document.querySelectorAll('.list-item');
+
+  for (let index = listItem.length - 1; index >= 0; index -= 1) {
+    const isSelectedItem = listItem[index].classList.contains('selected');
+    const isValidMovement = (index + 1 < listItem.length);
+
+    if (isSelectedItem && isValidMovement) {
+      const selectedItem = listItem[index];
+      const nextElement = listItem[index + 1];
+      let auxiliar;
+
+      auxiliar = nextElement.textContent;
+      nextElement.innerHTML = selectedItem.textContent;
+      selectedItem.innerHTML = auxiliar;
+
+      auxiliar = nextElement.className;
+      nextElement.className = selectedItem.className;
+      selectedItem.className = auxiliar;
+    }
+  }
+});
+
+btnRemoveSelected.addEventListener('click', () => {
+  const listItems = document.querySelectorAll('.list-item');
+
+  for (let index = 0; index < listItems.length; index += 1) {
+    if (listItems[index].classList.contains('selected')) {
+      listItems[index].remove();
+    }
+  }
+});
+
+// Listener que seleciona o item da lista
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('list-item')) {
+    console.log('item da lista foi clicado.');
+    clearLastSelected();
+    event.target.classList.add('selected');
+  }
+});
+
+// Listener que ao ter double-check marca como completa a tarefa
+document.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('list-item')) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+      event.target.classList.remove('selected');
+    } else {
+      event.target.classList.add('completed');
+      event.target.classList.remove('selected');
+    }
+  }
+});
+
+window.onload = function () {
+  loadTasks();
+};
