067be1dd0733b0bdd66df83d4b9a5e9c077be28a
mtbxp <marcellotb2@gmail.com>
2021-09-21 04:53:19 -0300

    Adiciona a funçao de colorir o item da lista ao clicar e riscar ao dar double click

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Declaração de variáveis e constantes
-const input = document.getElementById('texto-tarefa')
-const olList = document.getElementById('lista-tarefas')
-const button = document.getElementById('criar-tarefa')
-const clearAllButton = document.querySelector('#apaga-tudo')
+const input = document.getElementById('texto-tarefa');
+const olList = document.getElementById('lista-tarefas');
+const button = document.getElementById('criar-tarefa');
+const clearAllButton = document.querySelector('#apaga-tudo');
 
 // Função que adiciona elementos do Input do usuário à lista
 function createItens() {
@@ -21,8 +21,32 @@ olList.innerHTML = '';
 // https://www.codegrepper.com/code-examples/javascript/javascript+remove+all+children
 
 // Click Listener (Escutador de click)
-button.addEventListener('click', createItens);
-clearAllButton.addEventListener('click', ClearAll);
- 
+  // Escutador do Botao de Criar Itens
+  button.addEventListener('click', createItens);
+  // Escutador do Botao de Limpar Tudo
+  clearAllButton.addEventListener('click', ClearAll);
+  // Escutador de click, seleciona o item da lista
+  olList.addEventListener('click', SelectAndColor);
+  // Escutador de double click
+  olList.addEventListener('dblclick', crossListItem);
+
+// Função de Selecionar itens e Colorir
+function SelectAndColor (event) {
+  let selected = document.querySelector(".selected");
+  if (selected) {
+    selected.classList.remove("selected");
+  }
+  const itemSelected = event.target;
+  itemSelected.classList.add('selected');
+}
+// Função de Riscar item ao clicar 2x
+function crossListItem(event) {
+  event.target.classList.toggle('completed');
+}
+// consultei o slack e ja havia uma resposta para este desafio
+// https://trybecourse.slack.com/archives/C02A8CKT31U/p1631750861472900
+// não entendi o porque usar toggle e não classList.add então pesquisei e encontrei isso
+// https://pt.stackoverflow.com/questions/492295/qual-a-diferen%C3%A7a-entre-usar-classname-classlist-toggle-e-classlist-add
+
     
 
