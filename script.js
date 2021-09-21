97b061c549eee5a23bb93d78426e90642f43b116
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-09-21 03:45:00 -0300

    Limpando codigo javaScript

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const selectbutton = document.querySelector('#criar-tarefa');
 const inputFrase = document.getElementById('texto-tarefa');
 // Agora quando clicar no botão adicionar o que foi escrito na ol
 selectbutton.addEventListener('click', () => {
-  if(inputFrase.value === ''){
+  if (inputFrase.value === '') {
     return alert('Nenhum intem encontrado');
   }
   const createli = document.createElement('li');
@@ -16,10 +16,10 @@ selectbutton.addEventListener('click', () => {
 
 function MudaBackground() {
   const selectorColor = document.querySelectorAll('.listAdd');
-  for(let index = 0; index < selectorColor.length; index += 1){
+  for (let index = 0; index < selectorColor.length; index += 1) {
     selectorColor[index].addEventListener('click', () => {
-      for(let index = 0; index < selectorColor.length; index += 1){
-        selectorColor[index].style.backgroundColor = '';
+      for (let cont = 0; cont < selectorColor.length; cont += 1) {
+        selectorColor[cont].style.backgroundColor = '';
       }
       selectorColor[index].style.backgroundColor = 'rgb(128, 128, 128)';
     });
@@ -28,7 +28,7 @@ function MudaBackground() {
 
 function ClickComplete() {
   const selectCom = document.querySelectorAll('.listAdd');
-  for(let index = 0; index < selectCom.length; index += 1){
+  for (let index = 0; index < selectCom.length; index += 1) {
     selectCom[index].ondblclick = () => {
       selectCom[index].classList.toggle('completed');
     };
@@ -44,19 +44,19 @@ const buttonClearlist = document.querySelector('#apaga-tudo');
 buttonClearlist.addEventListener('click', () => {
   // Quantos li existe no momento, primeiro eu tenho que capturar class dos lis
   const list = document.querySelectorAll('.listAdd');
-  for(let i = 0; i < list.length; i += 1){
+  for (let i = 0; i < list.length; i += 1) {
     list[i].remove('li');
   }
 });
 
-//Capturar o butão
+// Capturar o butão
 const buttonDel = document.querySelector('#remover-finalizados');
-//Adicionar escutador
+// Adicionar escutador
 buttonDel.addEventListener('click', () => {
   // Capturar o li com completed
   const liCompleted = document.querySelectorAll('.completed');
   // Verificar se tem mais de um
-  for(let index = 0; index < liCompleted.length; index += 1){
+  for (let index = 0; index < liCompleted.length; index += 1) {
     liCompleted[index].remove('li');
   }
 });
