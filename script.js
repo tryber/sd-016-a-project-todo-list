0d3a4fee11027f99981994ea6cd357da82824430
mtbxp <marcellotb2@gmail.com>
2021-09-21 03:42:51 -0300

    Adiciona Botao de apagar toda lista e outros requisitos html

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-let input = document.getElementById('texto-tarefa')
-let olList = document.getElementById('lista-tarefas')
+// Declaração de variáveis e constantes
+const input = document.getElementById('texto-tarefa')
+const olList = document.getElementById('lista-tarefas')
 const button = document.getElementById('criar-tarefa')
+const clearAllButton = document.querySelector('#apaga-tudo')
 
+// Função que adiciona elementos do Input do usuário à lista
 function createItens() {
   let createItem = document.createElement('li');
-  createItem.innerText = input.value;
+  const createdInput = input.value;
+  createItem.classList.add('item');
+  createItem.innerText = createdInput;
   olList.appendChild(createItem);
+  input.value = '';
 }
 
-button.addEventListener('click', createItens)
+// Função para o Botao "Apagar Tudo" apagar a Lista toda
+function ClearAll() {
+olList.innerHTML = '';
+}
+// https://www.codegrepper.com/code-examples/javascript/javascript+remove+all+children
+
+// Click Listener (Escutador de click)
+button.addEventListener('click', createItens);
+clearAllButton.addEventListener('click', ClearAll);
+ 
+    
+
