2bb69a9803e3d178cd3bc3d7b0fa774e380288b2
baky2708 <flavioisaque123@gmail.com>
2021-09-29 18:55:01 -0300

    Complete requeriments

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const btnCreateTask = document.querySelector('#criar-tarefa');
 const btnDeleteAll = document.querySelector('#apaga-tudo');
 const btnDeleteFinished = document.querySelector('#remover-finalizados');
 const btnSaveList = document.querySelector('#salvar-tarefas');
+const btnUp = document.querySelector('#mover-cima');
+const btnDown = document.querySelector('#mover-baixo');
+const btnDeleteSelected = document.querySelector('#remover-selecionado');
 const list = document.querySelector('#lista-tarefas');
 
 function markTaskItem(event) {
   const taskList = document.querySelectorAll('.listItem');
   for (let i = 0; i < taskList.length; i += 1) {
     taskList[i].style.backgroundColor = '';
+    taskList[i].classList.remove('selected');
   }
-  const backgroundColor = 'rgb(128, 128, 128)';
   const taskItem = event;
-  taskItem.target.style.backgroundColor = backgroundColor;
+  taskItem.target.classList.add('selected');
 }
 
 function finishTaskItem(event) {
@@ -77,10 +80,63 @@ function startList() {
   }
 }
 
+function moveUp() {
+  const taskList = document.querySelectorAll('.listItem');
+  let position = '';
+  for (let i = 0, len = taskList.length; i < len; i += 1) {
+    if (taskList[i].className.includes('selected')) {
+      position = i;
+    }
+  }
+  if (taskList[position - 1] !== undefined) {
+    const swap = taskList[position - 1].textContent;
+    const swapClass = taskList[position - 1].className;
+    taskList[position - 1].innerHTML = taskList[position].textContent;
+    taskList[position - 1].className = '';
+    taskList[position - 1].className = taskList[position].className;
+    taskList[position].innerHTML = swap;
+    taskList[position].className = '';
+    taskList[position].className = swapClass;
+  }
+}
+
+function moveDown() {
+  const taskList = document.querySelectorAll('.listItem');
+  let position = '';
+  for (let i = 0, len = taskList.length; i < len; i += 1) {
+    if (taskList[i].className.includes('selected')) {
+      position = i;
+    }
+  }
+  if (position === '') return true;
+  if (taskList[position + 1] !== undefined) {
+    const swap = taskList[position + 1].textContent;
+    const swapClass = taskList[position + 1].className;
+    taskList[position + 1].innerHTML = taskList[position].textContent;
+    taskList[position + 1].className = '';
+    taskList[position + 1].className = taskList[position].className;
+    taskList[position].innerHTML = swap;
+    taskList[position].className = '';
+    taskList[position].className = swapClass;
+  }
+}
+
+function deleteSelected() {
+  const taskList = document.querySelectorAll('.listItem');
+  for (let i = 0, len = taskList.length; i < len; i += 1) {
+    if (taskList[i].className.includes('selected')) {
+      taskList[i].remove('li');
+    }
+  }
+}
+
 btnCreateTask.addEventListener('click', createTask);
 btnDeleteAll.addEventListener('click', deleteAllTasks);
 btnDeleteFinished.addEventListener('click', deleteFinished);
 btnSaveList.addEventListener('click', addLocalStorage);
+btnUp.addEventListener('click', moveUp);
+btnDown.addEventListener('click', moveDown);
+btnDeleteSelected.addEventListener('click', deleteSelected);
 
 window.onload = () => {
   startList();
