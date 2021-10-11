a6f17188ef5f6483826cca4c6d3485f74c60088a
Adolligit <adelson.onod@gmail.com>
2021-10-11 00:59:28 -0300

    js: r10, finish

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 let listItems = document.getElementById('lista-tarefas');
 
 document.getElementById('criar-tarefa').addEventListener('click', addItem);
+
 document.getElementById('texto-tarefa').addEventListener('keypress', (x) => (x.key === 'Enter') ? addItem() : null);
 
-listItems.addEventListener('click', (action) => 
-{
+document.getElementById('apaga-tudo').addEventListener('click', () => {
+  listItems.innerHTML = '';
 });
 
-function addItem(){
+function addItem()
+{
   let li = document.createElement('li');
   let input = document.getElementById('texto-tarefa');
   
   li.addEventListener('click', (pointer) => {
     let her = pointer.target;
-    for(i of listItems.children){
-      i.classList.remove('selected'); 
-    }
+
+    for(i of listItems.children) i.classList.remove('selected'); 
+
     if (!her.classList.contains('completed')) her.classList += ' selected';
   });
 
   li.addEventListener('dblclick', (pointer) => {
     let her = pointer.target;
+
     her.classList = her.classList.contains('completed') ? '' : 'completed';
   });
 
