cb13cd8fdb6956cf662bfda6dad6a79b18f30887
Adolligit <adelson.onod@gmail.com>
2021-10-11 00:48:39 -0300

    js:r9 finalizado

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let listItems = document.getElementById('lista-tarefas');
-let items = listItems.children;
-let i = document.getElementsByTagName('li');
 
-document.getElementById('criar-tarefa').addEventListener('click', () => {
+document.getElementById('criar-tarefa').addEventListener('click', addItem);
+document.getElementById('texto-tarefa').addEventListener('keypress', (x) => (x.key === 'Enter') ? addItem() : null);
+
+listItems.addEventListener('click', (action) => 
+{
+});
+
+function addItem(){
   let li = document.createElement('li');
   let input = document.getElementById('texto-tarefa');
   
-  listItems.appendChild(li).innerText = input.value;
-  input.value = '';
-});
+  li.addEventListener('click', (pointer) => {
+    let her = pointer.target;
+    for(i of listItems.children){
+      i.classList.remove('selected'); 
+    }
+    if (!her.classList.contains('completed')) her.classList += ' selected';
+  });
 
-listItems.addEventListener('click', (action) => {
-  for(i of items){
-    i.style.backgroundColor = 'white';
-  }    
-  action.target.style =  'background-color: rgb(128,128,128);';
-});
+  li.addEventListener('dblclick', (pointer) => {
+    let her = pointer.target;
+    her.classList = her.classList.contains('completed') ? '' : 'completed';
+  });
+
+  if (input.value != '') listItems.appendChild(li).innerText = input.value;
+
+  input.value = '';
+}
