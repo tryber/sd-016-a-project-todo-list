acf1493c3397984d42951d9f85b791c8424ce5a4
Lucas de Oliveira Barboza <lob619@gmail.com>
2021-11-25 12:58:06 -0300

    Entregando 100%

--- script.js
+++ script.js
@@ -29,3 +29,61 @@ function apagarLista() {
   adicionandoItem.innerHTML = '';
 }
 botaoApagar.addEventListener('click', apagarLista);
+
+function apagaSelecionados() {
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    if (itens[i].classList.contains('completed')) {
+      itens[i].remove();
+    }
+  }
+}
+const botaoFinalizados = document.getElementById('remover-finalizados');
+botaoFinalizados.addEventListener('click', apagaSelecionados);
+
+function salvaTarefa() {
+  localStorage.setItem('novaLista', JSON.stringify(adicionandoItem.innerHTML));
+}
+
+window.onload = function storage() {
+  adicionandoItem.innerHTML = JSON.parse(localStorage.getItem('novaLista'));
+};
+
+const botaoSalvar = document.getElementById('salvar-tarefas');
+botaoSalvar.addEventListener('click', salvaTarefa);
+
+function moverCima() {
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    if (itens[i].classList.contains('selected') && itens[i].previousElementSibling) {
+      itens[i].parentNode.insertBefore(itens[i], itens[i].previousElementSibling);
+    }
+  }
+}
+
+const botaoCima = document.getElementById('mover-cima');
+botaoCima.addEventListener('click', moverCima);
+
+function moverBaixo() {
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    if (itens[i].classList.contains('selected') && itens[i].nextElementSibling) {
+      itens[i].parentNode.insertBefore(itens[i].nextElementSibling, itens[i]);
+    }
+  }
+}
+
+const botaoBaixo = document.getElementById('mover-baixo');
+botaoBaixo.addEventListener('click', moverBaixo);
+
+function deleteSelected() {
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    if (itens[i].classList.contains('selected')) {
+      itens[i].remove();
+    }
+  }
+}
+
+const removerSelecionado = document.getElementById('remover-selecionado');
+removerSelecionado.addEventListener('click', deleteSelected);
