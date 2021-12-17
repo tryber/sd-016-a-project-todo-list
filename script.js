fc5f13f5848e462673d581338714e5cb1ae3b1a5
Seu nome <mgabiss10@gmail.com>
2021-12-16 22:40:18 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function createTask() {
+const listaTarefas = document.getElementById('lista-tarefas');
+
+function criarTarefa() {
   const inputTextoTarefa = document.getElementById('texto-tarefa');
   const btnCriarTarefa = document.getElementById('criar-tarefa');
-  const listaTarefas = document.getElementById('lista-tarefas');
   btnCriarTarefa.addEventListener('click', () => {
-    const createLi = document.createElement('li');
-    createLi.className = 'task';
-    createLi.innerText = inputTextoTarefa.value;
-    listaTarefas.appendChild(createLi);
+    const criarLi = document.createElement('li');
+    criarLi.className = 'tarefa';
+    criarLi.innerText = inputTextoTarefa.value;
+    listaTarefas.appendChild(criarLi);
     inputTextoTarefa.value = '';
   });
 }
-createTask();
+criarTarefa();
+
+function backgroundColorTarefa() {
+  listaTarefas.addEventListener('click', (event) => {
+    // eslint-disable-next-line no-param-reassign
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+}
+backgroundColorTarefa();
