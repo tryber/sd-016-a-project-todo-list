4b87d17eb45ea388a5b7381f184e34f1b2069505
Seu nome <mgabiss10@gmail.com>
2021-12-21 00:23:49 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-const listaTarefas = document.getElementById('lista-tarefas');
+// eslint-disable-next-line max-lines-per-function
+window.onload = () => {
+  const listaTarefas = document.getElementById('lista-tarefas');
 
-function criarTarefa() {
-  const inputTextoTarefa = document.getElementById('texto-tarefa');
-  const btnCriarTarefa = document.getElementById('criar-tarefa');
-  btnCriarTarefa.addEventListener('click', () => {
-    const criarLi = document.createElement('li');
-    criarLi.className = 'tarefa';
-    criarLi.innerText = inputTextoTarefa.value;
-    listaTarefas.appendChild(criarLi);
-    inputTextoTarefa.value = '';
-  });
-}
-criarTarefa();
-
-function backgroundColorTarefa() {
-  listaTarefas.addEventListener('click', (event) => {
+  function backgroundColorTarefa(event) {
     const tarefas = document.querySelectorAll('.tarefa');
     for (let i = 0; i < tarefas.length; i += 1) {
       tarefas[i].style.backgroundColor = 'white';
     }
     // eslint-disable-next-line no-param-reassign
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
-}
-backgroundColorTarefa();
+  }
+
+  function tarefaCompleta(event) {
+    const { classList } = event.currentTarget;
+    if (classList.contains('completed') === false) {
+      classList.add('completed');
+    } else if (classList.contains('completed')) {
+      classList.remove('completed');
+    }
+  }
+
+  function criarTarefa() {
+    const inputTextoTarefa = document.getElementById('texto-tarefa');
+    const btnCriarTarefa = document.getElementById('criar-tarefa');
+    btnCriarTarefa.addEventListener('click', () => {
+      const criarLi = document.createElement('li');
+      criarLi.className = 'tarefa';
+      criarLi.innerText = inputTextoTarefa.value;
+      criarLi.addEventListener('click', backgroundColorTarefa);
+      criarLi.addEventListener('dblclick', tarefaCompleta);
+      listaTarefas.appendChild(criarLi);
+      inputTextoTarefa.value = '';
+    });
+  }
+  criarTarefa();
+};
