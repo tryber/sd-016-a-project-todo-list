f233eee3180f95f1436b992227796fc7b08851b7
Seu nome <mgabiss10@gmail.com>
2021-12-21 18:20:55 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
 // eslint-disable-next-line max-lines-per-function
-window.onload = () => {
-  const listaTarefas = document.getElementById('lista-tarefas');
+const listaTarefas = document.getElementById('lista-tarefas');
 
-  function backgroundColorTarefa(event) {
+function backgroundColorTarefa(event) {
+  const tarefas = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    tarefas[i].style.backgroundColor = 'white';
+  }
+  // eslint-disable-next-line no-param-reassign
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function tarefaCompleta(event) {
+  const { classList } = event.currentTarget;
+  if (classList.contains('completed') === false) {
+    classList.add('completed');
+  } else if (classList.contains('completed')) {
+    classList.remove('completed');
+  }
+}
+
+function criarTarefa() {
+  const inputTextoTarefa = document.getElementById('texto-tarefa');
+  const btnCriarTarefa = document.getElementById('criar-tarefa');
+  btnCriarTarefa.addEventListener('click', () => {
+    const criarLi = document.createElement('li');
+    criarLi.className = 'tarefa';
+    criarLi.innerText = inputTextoTarefa.value;
+    criarLi.addEventListener('click', backgroundColorTarefa);
+    criarLi.addEventListener('dblclick', tarefaCompleta);
+    listaTarefas.appendChild(criarLi);
+    inputTextoTarefa.value = '';
+  });
+}
+criarTarefa();
+
+function apagarTarefas() {
+  const btnApagar = document.getElementById('apaga-tudo');
+  btnApagar.addEventListener('click', () => {
+    listaTarefas.innerText = '';
+  });
+}
+apagarTarefas();
+
+function apagarTarefasFinalizadas() {
+  const btnApagar = document.getElementById('remover-finalizados');
+  btnApagar.addEventListener('click', () => {
     const tarefas = document.querySelectorAll('.tarefa');
     for (let i = 0; i < tarefas.length; i += 1) {
-      tarefas[i].style.backgroundColor = 'white';
-    }
-    // eslint-disable-next-line no-param-reassign
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  }
-
-  function tarefaCompleta(event) {
-    const { classList } = event.currentTarget;
-    if (classList.contains('completed') === false) {
-      classList.add('completed');
-    } else if (classList.contains('completed')) {
-      classList.remove('completed');
-    }
-  }
-
-  function criarTarefa() {
-    const inputTextoTarefa = document.getElementById('texto-tarefa');
-    const btnCriarTarefa = document.getElementById('criar-tarefa');
-    btnCriarTarefa.addEventListener('click', () => {
-      const criarLi = document.createElement('li');
-      criarLi.className = 'tarefa';
-      criarLi.innerText = inputTextoTarefa.value;
-      criarLi.addEventListener('click', backgroundColorTarefa);
-      criarLi.addEventListener('dblclick', tarefaCompleta);
-      listaTarefas.appendChild(criarLi);
-      inputTextoTarefa.value = '';
-    });
-  }
-  criarTarefa();
-
-  function apagarTarefas() {
-    const btnApagar = document.getElementById('apaga-tudo');
-    btnApagar.addEventListener('click', () => {
-      listaTarefas.innerText = '';
-    });
-  }
-  apagarTarefas();
-
-  function apagarTarefasFinalizadas() {
-    const btnApagar = document.getElementById('remover-finalizados');
-    btnApagar.addEventListener('click', () => {
-      const tarefas = document.querySelectorAll('.tarefa');
-      for (let i = 0; i < tarefas.length; i += 1) {
-        if (tarefas[i].classList.contains('completed')) {
-          tarefas[i].remove();
-        }
+      if (tarefas[i].classList.contains('completed')) {
+        tarefas[i].remove();
       }
-    });
-  }
-  apagarTarefasFinalizadas();
+    }
+  });
+}
+apagarTarefasFinalizadas();
+
+function salvarTarefas() {
+  const btnSalvar = document.getElementById('salvar-tarefas');
+  btnSalvar.addEventListener('click', () => {
+    localStorage.setItem('Lista', listaTarefas.innerHTML);
+  });
+}
+salvarTarefas();
+
+window.onload = function storage() {
+  listaTarefas.innerHTML = localStorage.getItem('Lista');
 };
