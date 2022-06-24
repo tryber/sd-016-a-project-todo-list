70c97eff7a5cd9af88ad366401adeae9c7fd01b9
aparecidag365 <mgegoulart365@gmail.com>
2022-06-24 16:15:14 -0300

    Modifica Script

--- script.js
+++ script.js
@@ -13,49 +13,49 @@ btnCriarTarefa.addEventListener('click', function () {
   listaTarefas.addEventListener('click', newColor);
   //riscar a tarefa
   listaTarefas.addEventListener('dblclick', riskTasK);
-  textoTarefa.value =''
+  textoTarefa.value = ''
 })
 
 //Item 10 - clicar no botão `apaga-tudo` para deixar lista de tarefas vazia
 let btnLimparTarefas = document.getElementById('apaga-tudo');
-btnLimparTarefas.addEventListener('click', function() {
+btnLimparTarefas.addEventListener('click', function () {
   let lineTarefas = document.querySelectorAll("li")
   for (let index = 0; index < lineTarefas.length; index += 1) {
     document.querySelector("#lista-tarefas").removeChild(lineTarefas[index])
   }
-  })
+})
 
 //Item 11 - clicar no botão `remover-finalizados` 
-function removeFinish(){
+function removeFinish() {
   let lineTarefas = document.querySelectorAll(".completed")
   for (let index = 0; index < lineTarefas.length; index += 1) {
-    document.querySelector("#lista-tarefas").removeChild(lineTarefas[index])  
+    document.querySelector("#lista-tarefas").removeChild(lineTarefas[index])
   }
 }
 btnLimparFinish.addEventListener('click', removeFinish)
 
 //Item 14 - clicar no botão `remover-selecionado` 
-function removeSelect(){
+function removeSelect() {
   let lineTarefas = document.querySelector("#lista-tarefas").children
   for (let index = 0; index < lineTarefas.length; index += 1) {
-    if (lineTarefas[index].style.backgroundColor != 'rgba(100, 148, 237, 0.11)'){
+    if (lineTarefas[index].style.backgroundColor != 'rgba(100, 148, 237, 0.11)') {
       document.querySelector("#lista-tarefas").removeChild(lineTarefas[index])
-    }  
+    }
   }
 }
 btnRemoveSelect.addEventListener('click', removeSelect);
 
-function newColor(color){
+function newColor(color) {
   let lineTarefas = document.getElementById("lista-tarefas").children
   for (let index = 0; index < lineTarefas.length; index += 1) {
     lineTarefas[index].style.backgroundColor = 'rgba(100, 148, 237, 0.11)';
   }
-    color.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  color.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
-function riskTasK(risk){
-    risk.target.classList.toggle('completed');
-    //risk.target.classList.add('completed')
+function riskTasK(risk) {
+  risk.target.classList.toggle('completed');
+  //risk.target.classList.add('completed')
 }
 
 
